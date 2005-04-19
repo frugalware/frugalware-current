da49b4c27643b796e97591be460b77e53a81fd25
VMiklos <mamajom@axelero.hu>
2005-04-19 20:04:23 +0000

    changed banner's size to 80x15 also changed netcraft's banner to a 'powered by frugalware' banner as: - netcraft doesn't show our uptime - msd created that nice powered-by logo

--- docs/homepage/frugalware/pageinfo.php
+++ docs/homepage/frugalware/pageinfo.php
@@ -1,16 +1,14 @@
 <?php
 
 fwopenbox("$fwstrpageinfo", 100, false);
-print("<a href=\"http://uptime.netcraft.com/up/graph?host=frugalware.org\" " .
-	"title=\"View Netcraft statistics\">" .
-	"<img border=0 src=\"images/netcraft.gif\" alt=\"Netcraft\"></a>");
+print("<img border=0 src=\"images/frugalware80x15.png\" alt=\"Powered by Frugalware\" title=\"Powered by Frugalware\">");
 print("<p><a href=\"http://validator.w3.org/check?uri=referer\"" .
 	"title=\"Valid HTML 4.01 Transitional\">" .
-	"<img border=0 src=\"http://www.w3.org/Icons/valid-html401\"" .
+	"<img border=0 src=\"images/html401.png\"" .
 	"alt=\"Valid HTML 4.01!\"></a>");
 print("<p><a href=\"http://jigsaw.w3.org/css-validator/validator?uri=" .
 	"http://frugalware.org$PHP_SELF\" title=\"Valid CSS\">" .
-	"<img border=0 src=\"http://jigsaw.w3.org/css-validator/images/vcss\"" .
+	"<img border=0 src=\"images/css.png\"" .
 	"alt=\"Valid CSS!\"></a>");
 fwclosebox(false);
 ?>

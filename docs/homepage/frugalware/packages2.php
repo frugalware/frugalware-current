9e8a4bf82341f7029ba1a8793edaf5d26eac63b1
VMiklos <mamajom@axelero.hu>
2005-05-09 12:42:54 +0000

    homepage/packages2.php: added link to the pkg's changelog

--- docs/homepage/frugalware/packages2.php
+++ docs/homepage/frugalware/packages2.php
@@ -196,6 +196,12 @@ function pkg_from_id($id) {
 	print "<table border=0 width=100%>\n";
 	print "<tr><td>Name:</td><td><a href=\"".$resz."?id=".$id."&s=f\">".$arr['pkgname']."</a></td></tr>\n";
 	print "<tr><td>Version:</td><td>".$arr['pkgver']."-".$arr['pkgrel']."</td></tr>\n";
+	if ($arr['repo']=="extra")
+	{
+		$repodir="/" . $arr['repo'];
+		$groupdir=preg_replace("/-extra/", "", $arr['groups']);
+	}
+	print "<tr><td>Changelog:</td><td><a href=\"http://ftp.frugalware.org/pub/frugalware/frugalware-" . $arr['fwver'] . "$repodir/source/" . preg_replace("/^([^ ]*) .*/", "$1", $groupdir) . "/" . $arr['pkgname'] . "/Changelog\">Changelog</a></td></tr>\n";
 	if ($arr['groups'] != 'NULL') print "<tr><td>Groups:</td><td>".$arr['groups']."</td></tr>\n";
 	if ($arr['provides'] != 'NULL') print "<tr><td>Provides:</td><td>".$arr['provides']."</td></tr>\n";
 	if ($arr['depends'] != 'NULL')

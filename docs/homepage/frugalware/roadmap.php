57bd33f1650b54013bad2ce266f74f781000e6d3
VMiklos <mamajom@axelero.hu>
2005-05-11 09:11:30 +0000

    made roadmap.php valid html

--- docs/homepage/frugalware/roadmap.php
+++ docs/homepage/frugalware/roadmap.php
@@ -4,37 +4,37 @@ include("header.php");
 
 fwopenbox("0.3 (<a class=menu href=\"http://www.geocities.com/Area51/Dimension/1136/galactography/worlds8.html\">Trantor</a>)");
 print("
-<table width=100%>
-<tr><td>Oct 13, 2005<td>0.3<td><i>pending
-<tr><td>Sep 29, 2005<td>0.3rc2<td><i>pending
-<tr><td>Sep 15, 2005<td>0.3rc1<td><i>pending
-<tr><td>Aug 18, 2005<td>0.3pre2<td><i>pending
-<tr><td width=30%>Jun 23, 2005<td width=30%>0.3pre1<td width=30%><i>pending
+<table width=\"100%\">
+<tr><td>Oct 13, 2005<td>0.3<td><i>pending</i>
+<tr><td>Sep 29, 2005<td>0.3rc2<td><i>pending</i>
+<tr><td>Sep 15, 2005<td>0.3rc1<td><i>pending</i>
+<tr><td>Aug 18, 2005<td>0.3pre2<td><i>pending</i>
+<tr><td width=\"30%\">Jun 23, 2005<td width=\"30%\">0.3pre1<td width=\"30%\"><i>pending</i>
 </table>
 ");
-fwclosebox();
+fwclosebox(false);
 print("<p>");
 fwopenbox("0.2 (Aurora)");
 print("
-<table width=100%>
-<tr><td>Apr 28, 2005<td>0.2<td><i>done
-<tr><td>Apr 18, 2005<td>0.2rc2<td><i>done
-<tr><td>Apr 7, 2005<td>0.2rc1<td><i>done
-<tr><td>Feb 24, 2005<td>0.2pre2<td><i>done
-<tr><td width=30%>Jan 10, 2005<td width=30%>0.2pre1<td width=30%><i>done
+<table width=\"100%\">
+<tr><td>Apr 28, 2005<td>0.2<td><i>done</i>
+<tr><td>Apr 18, 2005<td>0.2rc2<td><i>done</i>
+<tr><td>Apr 7, 2005<td>0.2rc1<td><i>done</i>
+<tr><td>Feb 24, 2005<td>0.2pre2<td><i>done</i>
+<tr><td width=\"30%\">Jan 10, 2005<td width=\"30%\">0.2pre1<td width=\"30%\"><i>done</i>
 </table>
 ");
-fwclosebox();
+fwclosebox(false);
 print("<p>");
 fwopenbox("0.1 (Genesis)");
 print("
-<table width=100%>
-<tr><td>Nov  2, 2004<td>0.1<td><i>done
-<tr><td>Oct 11, 2004<td>0.1rc2<td><i>done
-<tr><td width=30%>Sep 29, 2004<td width=30%>0.1rc1<td width=30%><i>done
+<table width=\"100%\">
+<tr><td>Nov  2, 2004<td>0.1<td><i>done</i>
+<tr><td>Oct 11, 2004<td>0.1rc2<td><i>done</i>
+<tr><td width=\"30%\">Sep 29, 2004<td width=\"30%\">0.1rc1<td width=\"30%\"><i>done</i>
 </table>
 ");
-fwclosebox();
+fwclosebox(false);
 
 include("footer.php");
 ?>

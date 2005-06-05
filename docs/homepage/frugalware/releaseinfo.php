75087f2957d9f020786876e802919e651e1f421c
VMiklos <mamajom@axelero.hu>
2005-06-05 20:50:55 +0000

    homepage: update releaseinfo.php for the new mkiso script ugly hack, this whole stuff should be moved to mysql soon

--- docs/homepage/frugalware/releaseinfo.php
+++ docs/homepage/frugalware/releaseinfo.php
@@ -6,11 +6,11 @@ foreach($fwreleases as $i)
 {
 	foreach($fwisos as $j)
 	{
-		if (preg_match ("/^l.*$i-cd1.iso$/i", $j))
+		if (preg_match ("/^l.*$i-i686-cd1.iso$/i", $j))
 			break;
 	}
-	$fwisover=preg_replace("|.*e-(.*)-cd1.iso ->.*|", "$1", $j);
-	$isoname="$fwtopsrcdir/frugalware-current-iso/frugalware-$i-cd1.iso";
+	$fwisover=preg_replace("|.*e-(.*)-i686-cd1.iso ->.*|", "$1", $j);
+	$isoname="$fwtopsrcdir/frugalware-current-iso/frugalware-$i-i686-cd1.iso";
 	print("<div align=\"left\">$arrow-$i<br>\n");
 	print("&nbsp;&nbsp;&nbsp;" .
 		date("M d H:i:s", filemtime($isoname)) . "<br>");

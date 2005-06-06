1534388dc0b70ba4ca42ec03ecf2f1fb531c41e8
VMiklos <mamajom@axelero.hu>
2005-06-06 14:40:13 +0000

    homepage/rss.php: initial release as requested so many times...

---
+++ docs/homepage/frugalware/rss.php
@@ -0,0 +1,35 @@
+<?
+	switch($_GET['type'])
+	{
+		case "stable":
+			include("/etc/todo.conf");
+			$conn = mysql_connect(DBHOST, DBUSER, DBPASS);
+			mysql_select_db(DBNAME, $conn);
+			$query="select version, `desc` from releases where type='stable' order by date desc";
+			$result = mysql_query($query) or die();
+			while ($i = mysql_fetch_array($result, MYSQL_ASSOC))
+				$items[] = $i;
+			mysql_free_result($result);
+			break;
+		default:
+			die();
+	}
+	
+	header('Content-Type: application/xml; charset=utf-8');
+	print('<?xml version="1.0"  encoding="utf-8"?>
+<rss version="2.0">
+<channel>
+	<title>Frugalware Linux</title>
+	<description>Frugalware Linux is general purpose Linux distribution designed for intermediate users. Some of its elements were borrowed from Slackware Linux and Arch Linux.</description>
+	<link>http://frugalware.org/</link>');
+	foreach($items as $i)
+	{
+		print("<item>
+<title>frugalware-" . $i['version'] . "</title>
+<description>" . $i['desc'] . "</description>
+<link>http://frugalware.org/download.php?url=frugalware-" . $i['version'] . "-iso/frugalware-" . $i['version'] . "-dvd.iso</link>
+</item>");
+	}
+print('</channel>
+</rss>');
+?>

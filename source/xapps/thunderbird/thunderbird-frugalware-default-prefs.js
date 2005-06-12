5f235a38636d72cfa0ad8387b0ce042050fce11f
VMiklos <mamajom@axelero.hu>
2005-06-12 12:34:51 +0000

    thunderbird-1.0.2-2-i686 From Laszlo Dvornik <dvornik@gnome.hu> Compiled with freetype2 support. Added language autodetection support. Added GNOME and KDE menu file. Now using the GTK+2 file chooser. Now using OpenOffice.org's MySpell dictionaries for spell checking. Compiled with MathML support. Fixed JS vulnerability. Removed /usr/bin/defaults directory during upgrade. Updated pkgdesc and url.

---
+++ source/xapps/thunderbird/thunderbird-frugalware-default-prefs.js
@@ -0,0 +1,7 @@
+pref("general.useragent.vendor", "Frugalware");
+pref("general.useragent.vendorSub", "FW_VR");
+
+# Make hyperlinks work
+pref("network.protocol-handler.app.http", "/usr/lib/thunderbird/open-browser.sh");
+pref("network.protocol-handler.app.https", "/usr/lib/thunderbird/open-browser.sh");
+pref("network.protocol-handler.app.ftp", "/usr/lib/thunderbird/open-browser.sh");

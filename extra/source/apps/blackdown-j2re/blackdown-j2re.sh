ffbe3acbcd55b058e9a595568f150f3240b59db6
Christian Hamar alias krix <krics@linuxforum.hu>
2005-06-30 02:52:57 +0000

    blackdown-j2re-1.4.2_02-1-x86_64   * Fixed a path problem on blackdown.sh

--- extra/source/apps/blackdown-j2re/blackdown-j2re.sh
+++ extra/source/apps/blackdown-j2re/blackdown-j2re.sh
@@ -1,4 +1,4 @@
-export J2REDIR=/usr/lib/j2re1.4.2
+export J2REDIR=/usr/lib/blackdown-j2re
 export PATH=$PATH:$J2REDIR/bin
 export MANPATH=$MANPATH:$J2REDIR/man
 export CLASSPATH=$CLASSPATH:$J2REDIR/lib

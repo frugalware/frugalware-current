64fc3de6de4a95332ca160b5c0e3226b2eef89ae
VMiklos <mamajom@axelero.hu>
2005-07-24 22:34:24 +0000

    include/skel.sh: initial import added a skeleton for further .shs to /include (used by Finclude)

---
+++ source/include/skel.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# (c) 2005 Miklos Vajna <vmiklos@frugalware.org>
+# skel.sh for Frugalware
+# distributed under GPL License
+
+# one line to give the brief idea of what this .sh does
+
+echo foo

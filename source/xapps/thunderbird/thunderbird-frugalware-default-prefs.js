f061c404057acca1799f7487670910116f76c82f
Laszlo Dvornik <dvornik@gnome.hu>
2005-07-30 11:49:23 +0000

    thunderbird-1.0.6-2-i686   - Fixed #20.   - Removed open-browser.sh.

--- source/xapps/thunderbird/thunderbird-frugalware-default-prefs.js
+++ source/xapps/thunderbird/thunderbird-frugalware-default-prefs.js
@@ -1,7 +1,8 @@
 pref("general.useragent.vendor", "Frugalware");
 pref("general.useragent.vendorSub", "FW_VR");
 
-# Make hyperlinks work
-pref("network.protocol-handler.app.http", "/usr/lib/thunderbird/open-browser.sh");
-pref("network.protocol-handler.app.https", "/usr/lib/thunderbird/open-browser.sh");
-pref("network.protocol-handler.app.ftp", "/usr/lib/thunderbird/open-browser.sh");
+# Thunderbird default uses Firefox to open links, to use your favourite
+# web browser / ftp client, modify the lines below.
+#pref("network.protocol-handler.app.http", "/full_path/my_favourite_web_browser");
+#pref("network.protocol-handler.app.https", "/full_path/my_favourite_web_browser");
+#pref("network.protocol-handler.app.ftp", "/full_path/my_favourite_web_browser");

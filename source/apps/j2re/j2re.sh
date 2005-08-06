842288bf405711eeba8a97a04bd68057d635a742
BMH1980 <bmh1980de@yahoo.de>
2005-08-06 18:41:22 +0000

    j2re-5.0_04-1-i686 Version bump Changing compiling time to SBU Changing $pkgurl Some cosmetics on differences between i686 and x86_64 md5sum -> sha1sums Fixing up2date because new $pkgurl Cleaning build() Fixing $JAVA_HOME in /etc/profile.d/j2re.sh

--- source/apps/j2re/j2re.sh
+++ source/apps/j2re/j2re.sh
@@ -2,3 +2,6 @@ export J2REDIR=/usr/lib/j2re
 export PATH=$PATH:$J2REDIR/bin
 export MANPATH=$MANPATH:$J2REDIR/man
 export CLASSPATH=$CLASSPATH:$J2REDIR/lib
+if [ ! -f /etc/profile.d/j2sdk.sh ]; then
+        export JAVA_HOME=$J2REDIR
+fi

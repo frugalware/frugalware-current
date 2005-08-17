a808097f7f476fbc64a6dc917768f94d689d055e
VMiklos <vmiklos@frugalware.org>
2005-08-17 20:31:58 +0000

    bash-completion-20050721-2-i686 disable bash_completion.sh in zsh fix and update bash-completion file for pacman

--- source/apps/bash-completion/bash_completion.sh
+++ source/apps/bash-completion/bash_completion.sh
@@ -1,9 +1,10 @@
 # bash-completion
-
-bash=${BASH_VERSION%.*}; bmajor=${bash%.*}; bminor=${bash#*.}
-if [ "$PS1" ] && [ \( $bmajor -eq 2 -a $bminor '>' 04 \) -o $bmajor -ge 3 ] \
-  && [ -f /etc/bash_completion ]; then # interactive shell
-  # Source completion code
-  . /etc/bash_completion
+if [ ! -z "${BASH_VERSION}" ]; then
+  bash=${BASH_VERSION%.*}; bmajor=${bash%.*}; bminor=${bash#*.}
+  if [ "$PS1" ] && [ \( $bmajor -eq 2 -a $bminor -ge 04 \) -o $bmajor -ge 3 ] \
+    && [ -f /etc/bash_completion ]; then # interactive shell
+    # Source completion code
+    . /etc/bash_completion
+  fi
+  unset bash bmajor bminor
 fi
-unset bash bmajor bminor

46b0ca9803a9103e1fbe6291610a194378e784e1
VMiklos <vmiklos@frugalware.org>
2005-08-22 15:32:17 +0000

    added linuxmedia.hu's banner

--- docs/homepage/frugalware/sidebar.php
+++ docs/homepage/frugalware/sidebar.php
@@ -3,5 +3,9 @@ fwopenbox("$fwstrbrick", 100, false);
 print("<div align=\"center\">
 <A HREF=\"http://www.linuxlinks.com\"><IMG SRC=\"http://www.linuxlinks.com/portal/content/logos/newlogo5.gif\" BORDER=0 width=\"86\" height=\"24\" alt=\"Linux Links\"></A>
 </div>\n");
+if ($_SERVER['PHP_SELF'] == "/download.php" and $lang=="hu")
+	print("<div align=\"center\">
+	<A HREF=\"http://www.linuxmedia.hu\"><IMG SRC=\"/images/linuxmedia.jpg\" BORDER=0 width=\"120\" height=\"240\" alt=\"Linuxmedia.hu\"></A>
+	</div>\n");
 fwclosebox(false);
 ?>

b7f8e2a5d9e61ea3fc276822eb99c8d3eb032d60
VMiklos <vmiklos@frugalware.org>
2005-08-25 19:38:22 +0000

    homepage/donations.php: new page

---
+++ docs/homepage/frugalware/donations.php
@@ -0,0 +1,24 @@
+<?
+$fwtitle="Donations";
+include("header.php");
+
+fwopenbox("Donations", 80, false);
+print("<div align=left>Donations are a great way to show your appreciation
+and support for Frugalware Linux. On this page we list the donations we have
+received so far and those that would help us in our work on Frugalware Linux.
+If you have a piece of hardware or something whose Frugalware support could be
+improved, a good way to achieve this is to donate it to a developer.</div>");
+fwclosebox(false);
+print("<p>");
+
+fwopenbox("The Frugalware Team", 80, false);
+print("<div align=left>Wishes:<ul>
+<li>a socket 939 type motherboard + AMD Athlon64 3000+ or better (or X2 DualCore 4400+) CPU Socket 939 version + DDR RAM's for the motherboard for x86_64 package building</li>
+</ul>Received:<ul>
+<li>two 40GB IDE HDD (Ervin Szalai)</li>
+<li>40GB IDE HDD (Janos Kovacs)</li>
+</ul></div>");
+fwclosebox(false);
+
+include("footer.php");
+?>

3ebb19b8caa6d63b0723126b3bb5b36e7d81db89
Tuxbubling <tuxbubling@tiscali.fr>
2005-08-29 22:23:19 +0000

    homepage/todolist.fr.php - firt french file First (the smaller one :P) french translated page

---
+++ docs/homepage/frugalware/lang/todolist.fr.php
@@ -0,0 +1,13 @@
+<?php
+
+#todolist.fr.php
+
+$fwtodoprior = array(
+	"Peu important, seulement fonctionnel", 
+	"Peu important, mais semble intéressant",
+	"Peu être important, doit être vérifié", 
+	"Trés important, doit être fait avant distribution",
+	"Critique, doit être réparé en un à deux jours"
+);
+
+?>

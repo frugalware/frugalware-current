a0999ce5d7d52b7cf75e39921912622391f20b5c
VMiklos <vmiklos@frugalware.org>
2005-09-08 19:16:15 +0000

    homepage/releaseinfo.php small changes: display all stable release make the "frugalware-version" string a link to the dvd iso added a link to the rss feed

--- docs/homepage/frugalware/releaseinfo.php
+++ docs/homepage/frugalware/releaseinfo.php
@@ -8,16 +8,20 @@ $fwisos=split("\n", `ls -l $fwtopsrcdir/frugalware-current-iso/`);
 fwopenbox("$fwstrreleaseinfo", 100, false);
 foreach($fwreleases as $i)
 {
-	$query = "select version, unix_timestamp(date) from releases where type = '$i' order by date desc limit 0,1";
+	$query = "select version, unix_timestamp(date) from releases where type = '$i' order by date desc";
 	$result = mysql_query($query) or die('Error in query: ' . mysql_error());
-	$release = mysql_fetch_array($result, MYSQL_ASSOC);
+	while($j = mysql_fetch_array($result, MYSQL_ASSOC))
+		$releases[] = $j;
 	mysql_free_result($result);
 	print("<div align=\"left\">$arrow-$i<br>\n");
-	print("&nbsp;&nbsp;&nbsp;" .
-		date("M d H:i:s", $release['unix_timestamp(date)']) . "<br>");
-	print("&nbsp;&nbsp;&nbsp;frugalware-" . $release['version']);
+	foreach($releases as $j)
+	{
+		print("<p>&nbsp;&nbsp;&nbsp;<a href=\"http://frugalware.org/download.php?url=frugalware-" . $j['version'] . "-iso/frugalware-" . $j['version'] . "-dvd.iso\">frugalware-" . $j['version'] . "</a><br>");
+		print("&nbsp;&nbsp;&nbsp;" .
+			date("M d H:i:s", $j['unix_timestamp(date)']) . "<br></p>");
+	}
 	print("</div><p>");
 }
-print("<a href=\"download.php\">$fwstrdl</a>");
+print("<a href=\"download.php\">$fwstrdl</a> &middot; <a href=\"/rss.php?type=stable\">RSS</a>");
 fwclosebox(false);
 ?>

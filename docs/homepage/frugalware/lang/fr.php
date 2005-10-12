17767f1401fa1d954890c77e4ad9ba59bc348713
Krisztian VASAS <iron@frugalware.org>
2005-10-12 08:25:40 +0000

    homepage: correction of fr.php, again

--- docs/homepage/frugalware/lang/fr.php
+++ docs/homepage/frugalware/lang/fr.php
@@ -1,214 +1,214 @@
- 1 <?
-2 // changelog.php
-3 $fwstrprelog="lire le changelog ";
-4 $fwstrmidlog="ici";
-5 $fwstrpostlog=".";
-6 //  download.php
-8 $fwstrdlfw="Télécharger Frugalware Linux (via ftp)";
-9 $fwstrdlfile="Télécharger fichier (via ftp)";
-10 $fwstrdlhttp="Via http";
-11 $fwstrdlrsync="Via rsync";
-12 $fwstrdlrsynct="Via rsync (mini-howto)";
-13 $fwstrdlrsync1="mirroir entier (~27GB):";
-14 $fwstrdlrsync2="seulement une section (~5GB, for example -current):";
-15 $fwstrcheapisot="Obtenir un CD";
-16 $fwstrcheapisod="Vous pouvez acheter des CD Frugalware sur les sites suivants:";
-17
-18 // footer.php
-19 $fwstrgentime="Generation time";
-20 $fwstrlastmod="dernière modif";
-21 $fwstrsecs="seconds";
-22
-23 // header.php
-24 $fwstrsearch="recherche";
-25
-26 //menu.php
-27 $fwstrnews="nouveautés";
-28 $fwstrabout="a propos";
-29 $fwstrchl="changelog";
-30 $fwstrdr="darcs";
-31 $fwstrpac="paquets";
-32 $fwstrdl="telechargements";
-33 $fwstrlist="listes";
-34 $fwstrirc="irc";
-35 $fwstrroadmap="feuille de route";
-36 $fwstrmwishlist="wishlist";
-37 $fwstrmbugs="bugs";
-38 $fwstrmblogs="blogs";
-39 $fwstrauthors="people";
-40 $fwstrforum="forum";
-41 $fwstrwiki="wiki";
-42 $fwstrdocsmenu="documentation";
-43 $fwstrdonations="donations";
-44
-45 // news.php
-46 $fwstrpby="posté par";
-47 $fwstrprenews="lire les dernières nouveautés ";
-48 $fwstrmidnews="ici";
-49 $fwstrpostnews=".";
-50
-51 //banner.php
-52 $fwstrbrick="Sponsors";
-53
-54 // packages.php
-55 $fwstrpacsearch="Recherché un paquet";
-56 $fwstrpacver="Version";
-57 $fwstrpacrepo="Repo";
-58 $fwstrpaccat="Categories";
+<?php
+// changelog.php
+$fwstrprelog="lire le changelog ";
+$fwstrmidlog="ici";
+$fwstrpostlog=".";
+// download.php
+$fwstrdlfw="Télécharger Frugalware Linux (via ftp)";
+$fwstrdlfile="Télécharger fichier (via ftp)";
+$fwstrdlhttp="Via http";
+$fwstrdlrsync="Via rsync";
+$fwstrdlrsynct="Via rsync (mini-howto)";
+$fwstrdlrsync1="mirroir entier (~27GB):";
+$fwstrdlrsync2="seulement une section (~5GB, for example -current):";
+$fwstrcheapisot="Obtenir un CD";
+$fwstrcheapisod="Vous pouvez acheter des CD Frugalware sur les sites suivants:";
 
-60 $fwstrpacinfo="information du paquet";
-61 $fwstrpacname="Nom";
-62 $fwstrpacrel="Release";
-63 $fwstrpacdesc="Description";
-64 $fwstrpacmd5="MD5 Sum";
-65 $fwstrpacsha1="SHA1 Sum";
-66 $fwstrpacdeps="Dépendances";
-67 $fwstrpacfwver="Frugalware version";
-68 $fwstrpacnopac="paquet non disponible.";
-69 $fwstrpacdl="Télécharger";
-70
-71 // swpat.php
-72 $fwstrsayno="Dites non";
-73
-74 // serverinfo.php
-75 $fwstrday="jour";
-76 $fwstrhour="h";
-77 $fwstrmin="m";
-78 $fwstrsec="s";
-79 $fwstrserverinfo="Server infos";
-80
-81 // releaseinfo.php
-82 $fwstrreleaseinfo="Publication";
-83
-84 // pkginfo.php
-85 $fwstrpkginfo="mise à jour récentes";
-86
-87 // lib.php
-88 $fwcharset="utf-8";
-89 $fwstrarrow="flèche";
-90
-91 // about.php
-92 $fwstraboutshort="Court";
-93 $fwstraboutlong="Long";
-94 $fwstrquestion="Question";
-95 $fwstranswer="Réponses";
-96
-97 // pageinfo.php
-98 $fwstrpageinfo="Informations";
-99
-100 // flag.php
-101 $fwstrchlang="Changer de langue";
-102
-103 // wishlist.php
-104 $fwstrwishlist="Wish list";
-105 $fwstrwprog="Programmes";
-106 $fwstrwnick="Email";
-107 $fwstrwtime="Heure";
-108 $fwstrwcomment="Commentaire";
-109 $fwstrwnosuch="Il y a actuellement 0 programmes dans la wishlist...";
-110 $fwstrwask="Demander un programme...";
-111 $fwstrerrnoprog="ERREUR!!! vous n'avez pas écris le nom du programme que vous demandez!!!";
-112 $fwstrerrnonick="ERREUR!!! vous n'avez pas enregistré votre nickname!!!";
-113 $fwstrinpgo=" allé ";
-114 $fwstrinpdel="éffacer";
-115 $fwstrgotoshow="Aller à la wishlist...";
-116
-117 // todolist.php
-118 $fwstrtodolist="Todo list";
-119 $fwstrtprog="Programme";
-120 $fwstrtnick="Email";
-121 $fwstrtaddnick="ajouté par";
-122 $fwstrtprior="Priorité";
-123 $fwstrttime="heure";
-124 $fwstrtcomment="Commentaire";
-125 $fwstrtready="Prêt";
-126 $fwstrtnosuch="Il y a actuellement 0 programme dans la todolist...";
-127 $fwstrerrnoprior="ERREUR!!! la Priorité n'est pas donnée!!!";
-128
-129 // login.php
-130 $fwerrbadpass="Mauvais mot de passe!!!";
-131 $fwerrhavetologin="Vous avez besoisn de vous loger pour voir cette page!!!";
-132 $fwstruname="Nom d'utilisateur:";
-133 $fwstrpass="mot de passe:";
-134 $fwstrlogin="Login";
-135
-136 // admin.php
-137 $fwstradminbox=" menu administrateu";
-138 $fwadmwshow="voir la wishlist";
-139 $fwadmtshow="voir la todolist";
-140 $fwadmbackadm="revenir a l'acceuil administrateur";
-141 $fwadmlogout="se déconnecter"
-142 $fwadmwsett="Ajouter";
-143 $fwadmwdel="Effacer";
-144 $fwadmtodoready="Prêt à écrire dans la Todo list";
-145 $fwadmbackwish="Revenir à la Wish list";
-146 $fwadmsurewdel="êtes vous sur de vouloir éffacer ?";
-147 $fwadmyes="oui";59 $fwstrpacpac="Paquets";
-148 $fwadmno="Non";
-149 $fwadmdelready="Prêt à éffacer depuis la Wish list";
-150 $fwadmtodonotready="Todo list pas encore faite";
-151 $fwadmtododone="Todo list déja éffectuée";
-152 $fwadmtnosuch="Pas de tel programme...";
-153 $fwadmbacktodo="Revenir à la Todo list";
-154 $fwadmpriorready="Prêt a changer de priorité";
-155 $fwadmsameprior="ERREUR!!! Priorité non changée!!!";
-156 $fwadmback="Revenir";
-157 $fwadmreadydochange="Êtes vous sure que cet article est prêt?";
-158 $fwadmreadynochange="ATTENTION!!! cet article n'a pas été vérifié!<br>\revenez en arrière et corrigez le!";
-159 $fwadmmadeready="l'article est prêt.";
-160
-161 // authors.php
-162 $fwstraudev="Developpeurs";
-163 $fwstraucontrib="Contributeurs";
-164 $fwstrauthx="Remerciements (Sponsors)";
-165
-166 // irc.php
-167 $fwstrircgeninfo="Informations générales";
-168 $fwstrircgeninfod="notre canal irc est sur le réseau (server: irc.freenode.net), at #frugalware. Ceci est le site d'aide  officielle Frugalware Linux irc.";
-169 $fwstrirclogint="se loguer via web";
-170 $fwstrircvalidnick="Entrez un nickname valable";
-171 $fwstrircnick="Nickname";
-172 $fwstrircserver="Server";
-173 $fwstrircchan="Channel";
-174 $fwstrircadv="Advanced...";
-175 $fwstrircloginb="Login";
-176 $fwstrircsocdiag="Social network diagram for #frugalware";
-177 $fwstrirclogt="Log of the #frugalware channel";
-178 $fwstrirclogd="Today's log can be read <a href=\"%s\">here</a>.";
-179
-180 // screenshot.php
-181 $fwstrscreenshot="captures";
-182 $shot_titles["sksetup.png"]="Localized setup";
-183 $shot_titles["bootsplash.png"]="Bootsplash";
-184 $shot_titles["kde.png"]="KDE";
-185 $shot_titles["gnome.png"]="Frugalware 0.2, GNOME 2.10.1";
-186 $shot_titles["firefox.png"]="Localisation du paquet firefox";
-187 $shot_titles["xfce4.jpg"]="Xfce";
-188 $shot_titles["blackbox.png"]="Blackbox";
-189 $shot_titles["blackbox2.png"]="Blackbox en action";
-190 $shot_titles["fluxbox.jpg"]="Fluxbox";
-191 $shot_titles["fluxbox2.jpg"]="Fluxbox en action";
-192 $shot_titles["gnome-x86_64.png"]="GNOME on x86_64";
-193
-194 // docs.php
-195 $fwstrdocs="Documentation";
-196 $fwstrdocsabout="<p align=\"left\">la plupart des questions couramment posées sont à <a href=\"%s\">about</a> page.</p>";
-197 $fwstrdocsdarcs="<p align=\"left\">cette documentation est gènéré à partie d'un texte dans le <a href=\"%s\">-current darcs tree</a>.</p>";
-198 $fwstrdocsq="<p align=\"left\">j'aimerais traduire la documentation dans ma propre langue, comment faire pour commençer?<br>";
-199 $fwstrdocsa="Evoyez un mail à  <a href=\"%s\">frugalware-devel</a> mailing list, et nous vous répondrons.</p>";
-200 $fwstrdocsonline="vue en ligne";
-201 $fwstrdocsen="Anglais";
-202 $fwstrdocshu="Hongrois";
-203 $fwstrdocssk="S";lovak";
-204 $fwstrdocshtml="HTML";
-205 $fwstrdocshtmlsingle="HTML simple fichier";
-206
-207 # donations.php
-208 $fwdonatestring="Donations";
-209 $fwdonatewelcome="<div align=left>les Donations sont un bon moyen de nous aider ";
-210 $fwdonatewelcome.="et de supporter Frugalware Linux. sur cette page nous listons nos donations ";
-211 $fwdonatewelcome.="reçu jusqu'ici et ceux qui nous aideraient dans notre travail sur Frugalware Linux ";
-212 $fwdonatewelcome.="Si vous avez du matériel qui pourrait être utile a Frugalware";
-213 $fwdonatewelcome.="une bonne manière de nous supporter est de le donner à un développeur.</div>";
-214
-215 ?>
+// footer.php
+$fwstrgentime="Generation time";
+$fwstrlastmod="dernière modif";
+$fwstrsecs="seconds";
+
+// header.php
+$fwstrsearch="recherche";
+
+// menu.php
+$fwstrnews="nouveautés";
+$fwstrabout="a propos";
+$fwstrchl="changelog";
+$fwstrdr="darcs";
+$fwstrpac="paquets";
+$fwstrdl="telechargements";
+$fwstrlist="listes";
+$fwstrirc="irc";
+$fwstrroadmap="feuille de route";
+$fwstrmwishlist="wishlist";
+$fwstrmbugs="bugs";
+$fwstrmblogs="blogs";
+$fwstrauthors="people";
+$fwstrforum="forum";
+$fwstrwiki="wiki";
+$fwstrdocsmenu="documentation";
+$fwstrdonations="donations";
+
+// news.php
+$fwstrpby="posté par";
+$fwstrprenews="lire les dernières nouveautés ";
+$fwstrmidnews="ici";
+$fwstrpostnews=".";
+
+// banner.php
+$fwstrbrick="Sponsors";
+
+// packages.php
+$fwstrpacsearch="Recherché un paquet";
+$fwstrpacver="Version";
+$fwstrpacrepo="Repo";
+$fwstrpaccat="Categories";
+
+$fwstrpacinfo="information du paquet";
+$fwstrpacname="Nom";
+$fwstrpacrel="Release";
+$fwstrpacdesc="Description";
+$fwstrpacmd5="MD5 Sum";
+$fwstrpacsha1="SHA1 Sum";
+$fwstrpacdeps="Dépendances";
+$fwstrpacfwver="Frugalware version";
+$fwstrpacnopac="paquet non disponible.";
+$fwstrpacdl="Télécharger";
+
+// swpat.php
+$fwstrsayno="Dites non";
+
+// serverinfo.php
+$fwstrday="jour";
+$fwstrhour="h";
+$fwstrmin="m";
+$fwstrsec="s";
+$fwstrserverinfo="Server infos";
+
+// releaseinfo.php
+$fwstrreleaseinfo="Publication";
+
+// pkginfo.php
+$fwstrpkginfo="mise à jour récentes";
+
+// lib.php
+$fwcharset="utf-8";
+$fwstrarrow="flèche";
+
+// about.php
+$fwstraboutshort="Court";
+$fwstraboutlong="Long";
+$fwstrquestion="Question";
+$fwstranswer="Réponses";
+
+// pageinfo.php
+$fwstrpageinfo="Informations";
+
+// flag.php
+$fwstrchlang="Changer de langue";
+
+// wishlist.php
+$fwstrwishlist="Wish list";
+$fwstrwprog="Programmes";
+$fwstrwnick="Email";
+$fwstrwtime="Heure";
+$fwstrwcomment="Commentaire";
+$fwstrwnosuch="Il y a actuellement 0 programmes dans la wishlist...";
+$fwstrwask="Demander un programme...";
+$fwstrerrnoprog="ERREUR!!! vous n'avez pas écris le nom du programme que vous demandez!!!";
+$fwstrerrnonick="ERREUR!!! vous n'avez pas enregistré votre nickname!!!";
+$fwstrinpgo=" allé ";
+$fwstrinpdel="éffacer";
+$fwstrgotoshow="Aller à la wishlist...";
+
+// todolist.php
+$fwstrtodolist="Todo list";
+$fwstrtprog="Programme";
+$fwstrtnick="Email";
+$fwstrtaddnick="ajouté par";
+$fwstrtprior="Priorité";
+$fwstrttime="heure";
+$fwstrtcomment="Commentaire";
+$fwstrtready="Prêt";
+$fwstrtnosuch="Il y a actuellement 0 programme dans la todolist...";
+$fwstrerrnoprior="ERREUR!!! la Priorité n'est pas donnée!!!";
+
+// login.php
+$fwerrbadpass="Mauvais mot de passe!!!";
+$fwerrhavetologin="Vous avez besoisn de vous loger pour voir cette page!!!";
+$fwstruname="Nom d'utilisateur:";
+$fwstrpass="mot de passe:";
+$fwstrlogin="Login";
+
+// admin.php
+$fwstradminbox=" menu administrateu";
+$fwadmwshow="voir la wishlist";
+$fwadmtshow="voir la todolist";
+$fwadmbackadm="revenir a l'acceuil administrateur";
+$fwadmlogout="se déconnecter"
+$fwadmwsett="Ajouter";
+$fwadmwdel="Effacer";
+$fwadmtodoready="Prêt à écrire dans la Todo list";
+$fwadmbackwish="Revenir à la Wish list";
+$fwadmsurewdel="êtes vous sur de vouloir éffacer ?";
+$fwadmyes="oui";59 $fwstrpacpac="Paquets";
+$fwadmno="Non";
+$fwadmdelready="Prêt à éffacer depuis la Wish list";
+$fwadmtodonotready="Todo list pas encore faite";
+$fwadmtododone="Todo list déja éffectuée";
+$fwadmtnosuch="Pas de tel programme...";
+$fwadmbacktodo="Revenir à la Todo list";
+$fwadmpriorready="Prêt a changer de priorité";
+$fwadmsameprior="ERREUR!!! Priorité non changée!!!";
+$fwadmback="Revenir";
+$fwadmreadydochange="Êtes vous sure que cet article est prêt?";
+$fwadmreadynochange="ATTENTION!!! cet article n'a pas été vérifié!<br>\revenez en arrière et corrigez le!";
+$fwadmmadeready="l'article est prêt.";
+
+// authors.php
+$fwstraudev="Developpeurs";
+$fwstraucontrib="Contributeurs";
+$fwstrauthx="Remerciements (Sponsors)";
+
+// irc.php
+$fwstrircgeninfo="Informations générales";
+$fwstrircgeninfod="notre canal irc est sur le réseau (server: irc.freenode.net), at #frugalware. Ceci est le site d'aide  officielle Frugalware Linux irc.";
+$fwstrirclogint="se loguer via web";
+$fwstrircvalidnick="Entrez un nickname valable";
+$fwstrircnick="Nickname";
+$fwstrircserver="Server";
+$fwstrircchan="Channel";
+$fwstrircadv="Advanced...";
+$fwstrircloginb="Login";
+$fwstrircsocdiag="Social network diagram for #frugalware";
+$fwstrirclogt="Log of the #frugalware channel";
+$fwstrirclogd="Today's log can be read <a href=\"%s\">here</a>.";
+
+// screenshot.php
+$fwstrscreenshot="captures";
+$shot_titles["sksetup.png"]="Localized setup";
+$shot_titles["bootsplash.png"]="Bootsplash";
+$shot_titles["kde.png"]="KDE";
+$shot_titles["gnome.png"]="Frugalware 0.2, GNOME 2.10.1";
+$shot_titles["firefox.png"]="Localisation du paquet firefox";
+$shot_titles["xfce4.jpg"]="Xfce";
+$shot_titles["blackbox.png"]="Blackbox";
+$shot_titles["blackbox2.png"]="Blackbox en action";
+$shot_titles["fluxbox.jpg"]="Fluxbox";
+$shot_titles["fluxbox2.jpg"]="Fluxbox en action";
+$shot_titles["gnome-x86_64.png"]="GNOME on x86_64";
+
+// docs.php
+$fwstrdocs="Documentation";
+$fwstrdocsabout="<p align=\"left\">la plupart des questions couramment posées sont à <a href=\"%s\">about</a> page.</p>";
+$fwstrdocsdarcs="<p align=\"left\">cette documentation est gènéré à partie d'un texte dans le <a href=\"%s\">-current darcs tree</a>.</p>";
+$fwstrdocsq="<p align=\"left\">j'aimerais traduire la documentation dans ma propre langue, comment faire pour commençer?<br>";
+$fwstrdocsa="Evoyez un mail à  <a href=\"%s\">frugalware-devel</a> mailing list, et nous vous répondrons.</p>";
+$fwstrdocsonline="vue en ligne";
+$fwstrdocsen="Anglais";
+$fwstrdocshu="Hongrois";
+$fwstrdocssk="S";lovak";
+$fwstrdocshtml="HTML";
+$fwstrdocshtmlsingle="HTML simple fichier";
+
+// donations.php
+$fwdonatestring="Donations";
+$fwdonatewelcome="<div align=left>les Donations sont un bon moyen de nous aider ";
+$fwdonatewelcome.="et de supporter Frugalware Linux. sur cette page nous listons nos donations ";
+$fwdonatewelcome.="reçu jusqu'ici et ceux qui nous aideraient dans notre travail sur Frugalware Linux ";
+$fwdonatewelcome.="Si vous avez du matériel qui pourrait être utile a Frugalware";
+$fwdonatewelcome.="une bonne manière de nous supporter est de le donner à un développeur.</div>";
+
+?>

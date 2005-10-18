19349c58230d421b67fd069385b8583638ebae37
Krisztian VASAS <iron@frugalware.org>
2005-10-18 17:36:17 +0000

    homepage: donations.php changins - we've got some donations, i've added them

--- docs/homepage/frugalware/donations.php
+++ docs/homepage/frugalware/donations.php
@@ -9,16 +9,17 @@ print("<p>");
 
 fwopenbox("The Frugalware Team", 80, false);
 print("<div align=left>Wishes:<ul>
-<li>Socket939 Motherboard + AMD Athlon64 3000+ or better (or X2 DualCore 4400+) CPU Socket939 version + DDR RAM's for the motherboard for x86_64 package building</li>
 <li>American mirror</li>
 <li>Asian mirror</li>
 </ul>Received:<ul>
+<li>Socket939 Motherboard + AMD Athlon64 3000+ CPU Socket939 version + 512MB DDR400 RAM (this will be our x86_64 buildserver)</li>
+<li>Codegen case for the new x86_64 buildserver (Krisztian VASAS)</li>
 <li>i586 Server: Pentium MMX 200 Mhz CPU, 64 Mb memory, 2.8Gb + 37Gb HDD (Botond Balazs, Miklos Vajna)</li>
 <li>Main server hosting (Sandor Szentirmay)</li>
 <li>Hungarian mirrors: inflame.hu, linuxforum.hu, FSN.hu</li>
 <li>European mirrors: belnet.be</li>
 <li>i686 Server: Pentium III (Coppermine) 600 Mhz, 256 Mb memory (the developer team)</li>
-<li>i686 build server: Pentium II (Deschutes) 300 Mhz, 384 Mb memory (University of Debrecen)</li>
+<li>i686 build server: Pentium II (Deschutes) 300 Mhz, 384 Mb memory</li>
 <li>i686 build server hosting (University of Debrecen)</li>
 <li>Advertising: linuxuser.hu, linuxlinks.com</li>
 <li>2x40GB IDE HDD (Szalai, Ervin)</li>

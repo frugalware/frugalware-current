9332075196b36b8f3c379b7f03b692c7ea64e91c
VMiklos <vmiklos@frugalware.org>
2005-10-25 22:58:15 +0000

    source/include/emul.sh strip the -emul suffix

--- source/include/emul.sh
+++ source/include/emul.sh
@@ -6,15 +6,16 @@
 
 # common source() and build() for emulation of i686 on x86_64
 
+ename=`echo $pkgname|sed 's/-emul//'`
 earch=i686
 
-source=(http://ftp.frugalware.org/pub/frugalware/frugalware-current/frugalware-$earch/$pkgname-$pkgver-$pkgrel-$earch.fpm)
+source=(http://ftp.frugalware.org/pub/frugalware/frugalware-current/frugalware-$earch/$ename-$pkgver-$pkgrel-$earch.fpm)
 
 build()
 {
         mkdir $Fsrcdir/tmp || return 1
-	file $pkgname-$pkgver-$pkgrel-$earch.fpm |grep -q bzip2 && f=j || f=z
-        tar x${f}f $pkgname-$pkgver-$pkgrel-$earch.fpm -C tmp || return 1
+	file $ename-$pkgver-$pkgrel-$earch.fpm |grep -q bzip2 && f=j || f=z
+        tar x${f}f $ename-$pkgver-$pkgrel-$earch.fpm -C tmp || return 1
         Fmkdir /usr/lib/chroot32
         cp -av tmp/* $Fdestdir/usr/lib/chroot32 || return 1
 }

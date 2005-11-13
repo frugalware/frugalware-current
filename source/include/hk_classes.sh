5a22029d7b3e87cda26bbf94ea9ba1b9f51f189d
BMH1980 <bmh1980de@yahoo.de>
2005-11-13 15:57:27 +0000

    hk_classes.sh Adding Finclude script for hk_classes-*

---
+++ source/include/hk_classes.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# (c) 2005 Marcus Habermehl <bmh1980de@yahoo.de>
+# hk_classes.sh for Frugalware
+# distributed under GPL License
+
+realname=hk_classes
+pkgver=0.8
+license="LGPL2"
+groups=('lib-extra')
+url="http://hk-classes.sourceforge.net/"
+up2date="lynx -dump 'http://knoda.sourceforge.net/tiki-page.php?pageName=Download'|grep 'stable.*hk_classes'|cut -d - -f 2"
+source=(http://dl.sourceforge.net/sourceforge/hk-classes/$realname-$pkgver.tar.bz2)
+sha1sums=('a6cfec587b046db244268546bb71a24afea21396')

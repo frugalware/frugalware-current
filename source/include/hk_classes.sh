aca784f0b56f2e5498fa2751fc50c65174f6f290
BMH1980 <bmh1980de@yahoo.de>
2005-11-13 16:11:12 +0000

    hk_classes.sh Removing some not needed vars

--- source/include/hk_classes.sh
+++ source/include/hk_classes.sh
@@ -5,10 +5,6 @@
 # distributed under GPL License
 
 realname=hk_classes
-pkgver=0.8
-license="LGPL2"
-groups=('lib-extra')
-url="http://hk-classes.sourceforge.net/"
 up2date="lynx -dump 'http://knoda.sourceforge.net/tiki-page.php?pageName=Download'|grep 'stable.*hk_classes'|cut -d - -f 2"
 source=(http://dl.sourceforge.net/sourceforge/hk-classes/$realname-$pkgver.tar.bz2)
 sha1sums=('a6cfec587b046db244268546bb71a24afea21396')

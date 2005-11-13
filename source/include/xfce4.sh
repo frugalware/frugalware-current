d18a49173d20635d9ef4cd85b920a988fa9e8104
VMiklos <vmiklos@frugalware.org>
2005-11-13 11:21:22 +0000

    xfce4.sh some cleanup requested by bmh i hope i didn't broke anything :)

--- source/include/xfce4.sh
+++ source/include/xfce4.sh
@@ -5,7 +5,6 @@
 # distributed under GPL License
 
 hpurl="http://www.xfce.org/"
-preup2date=`lynx -dump $hpurl|grep -m 1 -o '[0-9\.]\+ released'|sed 's| released||'`
 
 if [ -z $realname ] ; then
 	name=$pkgname
@@ -15,8 +14,9 @@ fi
 
 if echo ${groups[*]} | grep -q goodies ; then
 	dlurl="http://download.berlios.de/xfce-goodies/"
-	up2date="lynx -dump 'http://developer.berlios.de/project/showfiles.php?group_id=910'|grep -m 1 \"$name-[0-9\.]\+.tar.\(gz\|bz2\)\"|grep -o '[0-9]\.[0-9\.]\+[0-9]'"
+	up2date="lynx -dump http://developer.berlios.de/project/showfiles.php?group_id=910|grep \"$name-.*tar.\(gz\|bz2\)$\"|sed 's/.*-\(.*\)\.t.*/\1/;q'"
 else
+	preup2date=`lynx -dump $hpurl|grep released|sed 's/.*e \(.*\) r.*/\1/;q'`
 	dlurl="$hpurl/archive/xfce-$preup2date/src/"
-	up2date="lynx -dump $dlurl|grep -m 1 \"http.*$name-\"|grep -o \"$name-[0-9\.]\+[0-9]\"|sed \"s|$name-||\""
+	up2date="lynx -dump $hpurl/archive/xfce-$preup2date/src/|grep $name-|Flasttar"
 fi

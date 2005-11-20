e406dd9f59c421c545af9bef3e5005ada5189b5e
voroskoi <voroskoi@gmail.com>
2005-11-20 16:26:43 +0000

    Finclude for e17

---
+++ source/include/e17.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# (c) 2005 Andras Voroskoi <voroskoi@frugalware.org>
+# e17.sh for Frugalware
+# distributed under GPL License
+
+# up2date macro for e17 packages
+up2date="lynx -dump http://frugalware.org/~voroskoi/e17.up2date |sed -n '1 p'"

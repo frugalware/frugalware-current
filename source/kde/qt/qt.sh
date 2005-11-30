e5f75b02b1e07c8abaf0bf382d2800fb00c1a5f8
VMiklos <vmiklos@frugalware.org>
2005-11-30 19:58:53 +0000

    qt-3.3.5-1-i686 version bump

--- source/kde/qt/qt.sh
+++ source/kde/qt/qt.sh
@@ -2,9 +2,12 @@
 
 export QTDIR=/usr/lib/qt
 export PATH=$PATH:$QTDIR/bin
+export UIC_PATH=$QTDIR/bin/uic
+export QT_XFT=true
 export MANPATH=$MANPATH:$QTDIR/man
 if [ -z "$PKG_CONFIG_PATH" ]; then
 	export PKG_CONFIG_PATH=$QTDIR/lib/pkgconfig
 else
 	export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$QTDIR/lib/pkgconfig
 fi
+

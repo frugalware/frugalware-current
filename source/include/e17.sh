36e1239271b750a757bb0f6069ccbe470123157c
voroskoi <voroskoi@gmail.com>
2006-01-04 00:19:19 +0000

    e17.sh added $pkgver, but that doesn't work (yet) added source() and build()

--- source/include/e17.sh
+++ source/include/e17.sh
@@ -5,4 +5,13 @@
 # distributed under GPL License
 
 # up2date macro for e17 packages
+#pkgver="20060103"
+source=($pkgname-$pkgver.tar.gz)
 up2date="lynx -dump http://frugalware.org/~voroskoi/e17.up2date |sed -n '1 p'"
+
+build() {
+	Fcd $pkgname
+	export NOCONFIGURE="yes"
+	./autogen.sh || Fdie
+	Fbuild
+}

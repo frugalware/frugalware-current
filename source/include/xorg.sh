2f7125fc4b8a55909b3a797fd6cc60bebeb83317
VMiklos <vmiklos@frugalware.org>
2006-01-27 14:31:20 +0000

    xorg-7.0-1-i686 merged the xorg7 repo

--- source/include/xorg.sh
+++ source/include/xorg.sh
@@ -6,9 +6,40 @@
 
 # common url, up2date, source() and license for xorg packages
 
-xorgver="7.0-RC2"
+if ! [ -z "$realname" ]; then
+    name=$realname
+else
+    name=$pkgname
+fi
+
+xorgver="7.0"
 url="http://xorg.freedesktop.org"
-dlurl="http://xorg.freedesktop.org/releases/X11R$xorgver/everything/"
-up2date="lynx -dump $dlurl | grep ]$pkgname|sed -n -e 's/.*$pkgname-\(.*\)\.tar\.bz2.*/\1/' -e '1 p'"
-source=($dlurl/$pkgname-$pkgver.tar.bz2)
+dlurl="http://xorg.freedesktop.org/releases/X11R$xorgver/src/everything/"
+if [ "$xorgtag" == "1" ]; then
+	up2date="lynx -dump $dlurl | grep ]$name|sed -n -e 's/.*$name-X11R$xorgver-\(.*\)\.tar\.bz2.*/\1/' -e '1 p'"
+else
+	up2date="lynx -dump $dlurl | grep ]$name|sed -n -e 's/.*$name-\(.*\)\.tar\.bz2.*/\1/' -e '1 p'"
+fi
+if [ "$xorgtag" == "1" ]; then
+	source=($dlurl/$name-X11R$xorgver-$pkgver.tar.bz2)
+else
+	source=($dlurl/$name-$pkgver.tar.bz2)
+fi
 license="GPL2"
+unset realname
+
+Fcd() {
+	if [ "$Fsrcdir" = `pwd` ]; then
+		if [ "$#" -eq 1 ]; then
+			Fmessage "Going to the source directory..."
+			cd "$Fsrcdir/$1" || Fdie
+		elif [ "$#" -eq 0 ]; then
+			if [ "$xorgtag" == "1" ]; then
+				Fcd "$name-X11R$xorgver-$pkgver$pkgextraver"
+			else
+				Fcd "$name-$pkgver$pkgextraver"
+			fi
+		fi
+	fi
+}
+unset xorgtag

08a738478ccc710a3639f0ba4963838d3e97d4a9
VMiklos <vmiklos@frugalware.org>
2006-01-27 12:01:45 +0000

    include/kde.sh updates from crazy

--- source/include/kde.sh
+++ source/include/kde.sh
@@ -11,15 +11,24 @@ pkgurl="ftp://ftp.solnet.ch/mirror/KDE/stable/$kdever/src"
 up2date="lynx -dump http://www.kde.org/download/|grep $pkgname|sed -n '1 p'|sed 's/.*-\([^ ]*\) .*/\1/'"
 source=($pkgurl/$pkgname-$pkgver.tar.bz2)
 
-if [ "`cat /proc/meminfo |grep MemTotal|sed 's/.* \(.*\) kB/\1/'`" -ge 500000 ]; then
-	Fconfopts="$Fconfopts --enable-final"
-fi
+# crazy says this is still experimental to enable by default for all pkgs
+#if [ "`cat /proc/meminfo |grep MemTotal|sed 's/.* \(.*\) kB/\1/'`" -ge 500000 ]; then
+#	Fconfopts="$Fconfopts --enable-final"
+#fi
 
 build() 
 {
+	# we need that because KDE add some -O2' twice we already have it in our {$CXX,$C}{FLAGS}
+	Fcd
+	for i in `find . -iname configure`
+	do
+		Fsed '-O2' '' $i
+	done
+	
 	Fbuild CXXFLAGS="$CXXFLAGS -Wno-deprecated" \
 		--disable-dependency-tracking \
 		--disable-debug \
 		--with-gnu-ld \
+		--enable-gcc-hidden-visibility \
 		DO_NOT_COMPILE="doc"
 }

73765e8d5cd2ce8415be14d275efca97d4d2aa8b
VMiklos <vmiklos@frugalware.org>
2006-01-31 02:12:08 +0000

    homepage: updated sidebar now linuxlinks.com's banner is an internal link removed distrotalk.net's banner

--- docs/homepage/frugalware/sidebar.php
+++ docs/homepage/frugalware/sidebar.php
@@ -1,10 +1,7 @@
 <?php
 fwopenbox("$fwstrbrick", 100, false);
 print("<div align=\"center\">
-<A HREF=\"http://www.linuxlinks.com\"><IMG SRC=\"http://www.linuxlinks.com/portal/content/logos/newlogo5.gif\" BORDER=0 width=\"86\" height=\"24\" alt=\"Linux Links\"></A>
-</div>\n");
-print("<div align=\"center\">
-<A HREF=\"http://www.distrotalk.net/\"><IMG SRC=\"http://www.distrotalk.net/images/dtbanner-small.png\" BORDER=0 width=\"86\" height=\"24\" alt=\"DistroTalk\"></A>
+<A HREF=\"http://www.linuxlinks.com\"><IMG SRC=\"/images/linuxlinks.gif\" BORDER=0 width=\"86\" height=\"24\" alt=\"Linux Links\"></A>
 </div>\n");
 if ($_SERVER['PHP_SELF'] == "/download.php" and $lang=="hu")
 	print("<div align=\"center\"><br>

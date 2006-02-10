b8d92f5592afc260be7af60ea00e7d426875c0a2
VMiklos <vmiklos@frugalware.org>
2006-02-10 15:22:42 +0000

    include/kernel-module.sh new scheme for kernel modules
    how to use it:
    1) remove (if any) the .install script
    2) remove makedepends() (if contained only kernel-source)
    3) Finclude kernel-module
    4) add Fcheckkernel to the top of build()

---
+++ source/include/kernel-module.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# (c) 2006 Miklos Vajna <vmiklos@frugalware.org>
+# kernel-module.sh for Frugalware
+# distributed under GPL License
+
+# a common function and a scriptlet for externel kernel module packages
+
+kernelver=2.6.15-1
+makedepends=("kernel-source=$kernelver")
+install=$Fincdir/kernel-module.install
+
+Fcheckkernel()
+{
+	if [ "`uname -r|sed 's/fw//'`" != "$kernelver" ]; then
+		error "You might installed the required kernel package, but"
+		plain "you're not running that kernel. Please reboot your system."
+		Fdie
+	fi
+}

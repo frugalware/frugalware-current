acc032ee715f171db3c27a2fd4be4346917b7f3f
voroskoi <voroskoi@gmail.com>
2006-02-10 12:20:59 +0000

    e17.sh using tarballs from enlightenment.freedesktop.org created e17-cvs.sh for cvs builds

--- source/include/e17.sh
+++ source/include/e17.sh
@@ -1,17 +1,9 @@
 #!/bin/sh
 
-# (c) 2005 Andras Voroskoi <voroskoi@frugalware.org>
+# (c) 2006 Andras Voroskoi <voroskoi@frugalware.org>
 # e17.sh for Frugalware
 # distributed under GPL License
 
-# up2date macro for e17 packages
-#pkgver="20060103"
-source=($pkgname-$pkgver.tar.gz)
-up2date="lynx -dump http://frugalware.org/~voroskoi/e17.up2date |sed -n '1 p'"
-
-build() {
-	Fcd $pkgname
-	export NOCONFIGURE="yes"
-	./autogen.sh || Fdie
-	Fbuild
-}
+url="http://enlightenment.freedesktop.org/"
+source=($url/files/$pkgname-$pkgver.tar.gz)
+up2date="lynx -dump $url |grep =$pkgname |sed 's/.*-\(.*\).tar.*/\1/'"

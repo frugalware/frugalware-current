7fcf7d178d62016ebeb5568dff1c0559755946a1
VMiklos <vmiklos@frugalware.org>
2006-02-18 22:28:50 +0000

    include/gnome.sh generate preup2date from $pkgver, so that simply source'ing the a FrugalBuild with Fincude gnome will not generate an lynx call this simply speeds up gensync/updatesync/other scripts

--- source/include/gnome.sh
+++ source/include/gnome.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-# (c) 2003-2005 Miklos Vajna <vmiklos@frugalware.org>
+# (c) 2003-2006 Miklos Vajna <vmiklos@frugalware.org>
 # gnome.sh for Frugalware
 # distributed under GPL License
 
-# up2date macro for gnome packages
+# up2date and source() macro for gnome packages
 
 if ! [ -z "$realname" ]; then
 	name=$realname
@@ -12,11 +12,17 @@ else
 	name=$pkgname
 fi
 
+preup2date=`echo $pkgver|sed 's/\([0-9]\+\.[0-9]\+\).*$/\1/'`
+
+pkgurl="http://ftp.gnome.org/pub/GNOME/sources"
+
 if [ "$nondevel" == "1" ]; then
-	preup2date=`lynx -dump http://ftp.gnome.org/pub/GNOME/sources/$name/?C=N\;O=D|grep '/'|sed -ne 's|.*]\(.*\)/.*|\1|' -e '1 p'`
+	up2date="lynx -dump $pkgurl/$name/\$(lynx -dump $pkgurl/$name/?C=N\;O=D|grep '/'|sed -ne 's|.*]\(.*\)/.*|\1|' -e '1 p')/|grep ]LA|sed 's/.*S-\([0-9\.]*\).*/\1/'"
 else
-	preup2date=`lynx -dump http://ftp.gnome.org/pub/GNOME/sources/$name/?C=N\;O=D|grep '[0-9]\.[0-9]*[02468]/'|sed -ne 's|.*]\(.*\)/.*|\1|' -e '1 p'`
+	up2date="lynx -dump $pkgurl/$name/\$(lynx -dump $pkgurl/$name/?C=N\;O=D|grep '[0-9]\.[0-9]*[02468]/'|sed -ne 's|.*]\(.*\)/.*|\1|' -e '1 p')/|grep ]LA|sed 's/.*S-\([0-9\.]*\).*/\1/'"
 fi
-up2date="lynx -dump http://ftp.gnome.org/pub/GNOME/sources/$name/$preup2date/|grep ]LA|sed 's/.*S-\([0-9\.]*\).*/\1/'"
+
+source=(ftp://ftp.gnome.org/pub/gnome/sources/$pkgname/$preup2date/$name-$pkgver.tar.bz2)
+
 unset realname
 unset nondevel

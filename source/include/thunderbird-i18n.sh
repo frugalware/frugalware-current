69dfadef8421bad3cd073fe94d75751ccfee5699
Krisztian VASAS <iron@frugalware.org>
2006-02-24 17:47:46 +0000

    thunderbird-i18n.sh correction - added the same pkg-creation as firefox

--- source/include/thunderbird-i18n.sh
+++ source/include/thunderbird-i18n.sh
@@ -8,15 +8,20 @@
 
 up2date="1.5"
 
-source=(http://ftp.mozilla.org/pub/mozilla.org/thunderbird/releases/$pkgver/linux-i686$junk/xpi/$llang.xpi \
+source=(http://ftp.mozilla.org/pub/mozilla.org/thunderbird/releases/$pkgver/linux-i686/xpi/$llang.xpi \
 	lang-$pkgname.txt)
 
 build()
 {
-	Fmkdir /usr/lib/thunderbird
-	cd $Fdestdir/usr/lib/thunderbird
-	unzip -o $Fsrcdir/$llang.xpi
-	rm -rf defaults install.rdf chrome/chromelist.txt
-	mv chrome.manifest $lang.manifest
-	Ffile /usr/lib/thunderbird/chrome/lang/lang-$pkgname.txt
+	unzip -qqo $llang.xpi
+	sed -i 's|chrome/||' chrome.manifest
+	Ffilerel chrome.manifest /usr/lib/thunderbird/chrome/$llang.manifest
+	Ffilerel chrome/$llang.jar /usr/lib/thunderbird/chrome/$llang.jar
+
+#	Fmkdir /usr/lib/thunderbird
+#	cd $Fdestdir/usr/lib/thunderbird
+#	unzip -o $Fsrcdir/$llang.xpi
+#	rm -rf defaults install.rdf chrome/chromelist.txt
+#	mv chrome.manifest $lang.manifest
+	Ffile /usr/lib/thunderbird/chrome/lang-$pkgname.txt
 }

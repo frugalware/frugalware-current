9fca38540c06c6c4cd8672dcc7eb432a65321198
VMiklos <vmiklos@frugalware.org>
2006-03-03 15:14:45 +0000

    include/sourceforge.sh added _F_sourceforge_prefix and _F_sourceforge_ext variables

--- source/include/sourceforge.sh
+++ source/include/sourceforge.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# (c) 2005 Miklos Vajna <vmiklos@frugalware.org>
+# (c) 2005-2006 Miklos Vajna <vmiklos@frugalware.org>
 # sourceforge.sh for Frugalware
 # distributed under GPL License
 
@@ -12,6 +12,14 @@ else
 	name=$pkgname
 fi
 url="http://sourceforge.net/projects/$name"
-up2date="lynx -dump http://sourceforge.net/project/showfiles.php?group_id=\$(lynx -dump $url|grep showfiles|sed 's/.*=\(.*\)/\1/;q')|grep 'Release Notes'|sed 's/[^]]*][^]]*]\([^ ]*\) .*/\1/;q'"
-source=(http://dl.sourceforge.net/sourceforge/$name/$name-$pkgver.tar.gz)
-unset realname
+if [ -z "$_F_sourceforge_prefix" ]; then
+	up2date="lynx -dump http://sourceforge.net/project/showfiles.php?group_id=\$(lynx -dump $url|grep showfiles|sed 's/.*=\(.*\)/\1/;q')|grep 'Release Notes'|sed 's/[^]]*][^]]*]\([^ ]*\) .*/\1/;q'"
+else
+	up2date="lynx -dump http://sourceforge.net/project/showfiles.php?group_id=\$(lynx -dump $url|grep showfiles|sed 's/.*=\(.*\)/\1/;q')|grep 'Release Notes'|sed 's/[^]]*][^]]*]$_F_sourceforge_prefix\([^ ]*\) .*/\1/;q'"
+fi
+if [ -z "$_F_sourceforge_ext" ]; then
+	source=(http://dl.sourceforge.net/sourceforge/$name/$name-$pkgver.tar.gz)
+else
+	source=(http://dl.sourceforge.net/sourceforge/$name/$name-$pkgver.$_F_sourceforge_ext)
+fi
+unset realname _F_sourceforge_prefix _F_sourceforge_ext

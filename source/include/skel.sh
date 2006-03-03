211839a7ac5660d5a533679389219968977b2999
VMiklos <vmiklos@frugalware.org>
2006-03-03 15:20:19 +0000

    include/skel.sh added some comments about parameters

--- source/include/skel.sh
+++ source/include/skel.sh
@@ -6,4 +6,10 @@
 
 # one line to give the brief idea of what this .sh does
 
+# if you want the FrugalBuild to pass some parameters, then you should name
+# the variables like this: _F_foo_paramname. also don't forget to unset them
+# at the end of the script
+
 echo foo
+
+# unset _F_foo_paramname

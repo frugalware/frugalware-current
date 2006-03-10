4f525e702c94213232f466ff6057c5ef40131e4e
VMiklos <vmiklos@frugalware.org>
2006-03-10 08:16:39 +0000

    gnupg-1.4.2.2-1-i686 version bump moved the install scriptlet to a separate scheme

---
+++ source/include/texinfo.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# (c) 2006 Miklos Vajna <vmiklos@frugalware.org>
+# texinfo.sh for Frugalware
+# distributed under GPL License
+
+# common .install for packages with texinfo files
+
+install=$Fincdir/texinfo.install

9dd60b1d3ef64e4500d9bbbcc536830f3ab46e7a
VMiklos <vmiklos@frugalware.org>
2006-03-30 15:34:07 +0000

    homepage/releaseinfo.php fix download path for versions > 0.2 in the releaseinfo box

--- docs/homepage/frugalware/releaseinfo.php
+++ docs/homepage/frugalware/releaseinfo.php
@@ -14,7 +14,7 @@ foreach($fwreleases as $i)
 	print("<div align=\"left\">$arrow-$i<br>\n");
 	foreach($releases as $j)
 	{
-		print("<p>&nbsp;&nbsp;&nbsp;<a href=\"http://frugalware.org/download.php?url=frugalware-" . $j['version'] . "-iso/frugalware-" . $j['version'] . "-dvd.iso\">frugalware-" . $j['version'] . "</a><br>");
+		print("<p>&nbsp;&nbsp;&nbsp;<a href=\"http://frugalware.org/download.php?url=frugalware-" . $j['version'] . "-iso/frugalware-" . $j['version'] . ($j['version'] > "0.2" ? "-i686" : "") . "-dvd.iso\">frugalware-" . $j['version'] . "</a><br>");
 		print("&nbsp;&nbsp;&nbsp;" .
 			date("M d H:i:s", $j['unix_timestamp(date)']) . "<br></p>");
 	}

ac5f781c1f6d8628247be113db0eff8c072ae32f
VMiklos <vmiklos@frugalware.org>
2006-04-07 23:17:34 +0000

    include/perl.sh added _F_perl_ext support

--- source/include/perl.sh
+++ source/include/perl.sh
@@ -18,12 +18,16 @@
 	Fdie
 }
 
+[ -z "$_F_perl_ext" ] && _F_perl_ext=".tar.gz"
+
 url="http://cpan.org/"
 up2date="pud -p \"http://search.cpan.org/search?query=\$(echo $modname |sed s/-/::/g)&mode=all\" -e '$modname-(.*?) '"
-source=(http://search.cpan.org/CPAN/authors/id/$modauthor/$modname-$pkgver.tar.gz)
+source=(http://search.cpan.org/CPAN/authors/id/$modauthor/$modname-$pkgver$F_perl_ext)
 build()
 {
 	Fcd $modname-$pkgver
 	Fbuild
 	Frm /usr/lib/perl5/current
 }
+
+unset _F_perl_ext

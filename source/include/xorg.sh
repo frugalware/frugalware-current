33dde56453b413cd921c1513a55ba9ab564fd4f4
VMiklos <vmiklos@frugalware.org>
2006-04-08 14:04:44 +0000

    include/xorg.sh check for versions from the individual dir

--- source/include/xorg.sh
+++ source/include/xorg.sh
@@ -1,21 +1,23 @@
 #!/bin/sh
 
-# (c) 2005 Miklos Vajna <vmiklos@frugalware.org>
+# (c) 2005-2006 Miklos Vajna <vmiklos@frugalware.org>
 # xorg.sh for Frugalware
 # distributed under GPL License
 
 # common url, up2date, source() and license for xorg packages
 
 if ! [ -z "$realname" ]; then
-    name=$realname
+	_F_xorg_name=$realname
 else
-    name=$pkgname
+	_F_xorg_name=$pkgname
 fi
 
 xorgver="7.0"
 url="http://xorg.freedesktop.org"
-dlurl="http://xorg.freedesktop.org/releases/X11R$xorgver/src/everything/"
-up2date="lynx -dump $dlurl | grep $name-[0-9].*bz2$|sed 's/.*$name-\(.*\)\.t.*/\1/'"
-source=($dlurl/$name-$pkgver.tar.bz2)
+_F_xorg_dir=`echo ${groups[$((${#groups[@]}-1))]}|sed 's/xorg-\(.*\)/\1/;s/s$//'`
+[ "$pkgname" = "xorg-server" ] && _F_xorg_dir="xserver"
+dlurl="$url/releases/individual/$_F_xorg_dir/"
+up2date="lynx -dump $dlurl | grep $_F_xorg_name-[0-9].*bz2$|sed -n 's/.*$_F_xorg_name-\(.*\)\.t.*/\1/;$ p'"
+source=($dlurl/$_F_xorg_name-$pkgver.tar.bz2)
 license="GPL2"
-unset realname
+unset realname _F_xorg_dir _F_xorg_name

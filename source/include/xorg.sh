4a325e1a13428867c93175032c757bc912feff21
VMiklos <vmiklos@frugalware.org>
2006-04-08 11:34:52 +0000

    include/xorg.sh up2date fix a big cleanup

--- source/include/xorg.sh
+++ source/include/xorg.sh
@@ -15,31 +15,7 @@ fi
 xorgver="7.0"
 url="http://xorg.freedesktop.org"
 dlurl="http://xorg.freedesktop.org/releases/X11R$xorgver/src/everything/"
-if [ "$xorgtag" == "1" ]; then
-	up2date="lynx -dump $dlurl | grep ]$name|sed -n -e 's/.*$name-X11R$xorgver-\(.*\)\.tar\.bz2.*/\1/' -e '1 p'"
-else
-	up2date="lynx -dump $dlurl | grep ]$name|sed -n -e 's/.*$name-\(.*\)\.tar\.bz2.*/\1/' -e '1 p'"
-fi
-if [ "$xorgtag" == "1" ]; then
-	source=($dlurl/$name-X11R$xorgver-$pkgver.tar.bz2)
-else
-	source=($dlurl/$name-$pkgver.tar.bz2)
-fi
+up2date="lynx -dump $dlurl | grep $name-[0-9].*bz2$|sed 's/.*$name-\(.*\)\.t.*/\1/'"
+source=($dlurl/$name-$pkgver.tar.bz2)
 license="GPL2"
 unset realname
-
-Fcd() {
-	if [ "$Fsrcdir" = `pwd` ]; then
-		if [ "$#" -eq 1 ]; then
-			Fmessage "Going to the source directory..."
-			cd "$Fsrcdir/$1" || Fdie
-		elif [ "$#" -eq 0 ]; then
-			if [ "$xorgtag" == "1" ]; then
-				Fcd "$name-X11R$xorgver-$pkgver$pkgextraver"
-			else
-				Fcd "$name-$pkgver$pkgextraver"
-			fi
-		fi
-	fi
-}
-unset xorgtag

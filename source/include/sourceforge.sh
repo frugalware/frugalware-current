2152c3c8cbda8959787ac5f0ca9d9ab8ad8df596
VMiklos <vmiklos@frugalware.org>
2006-04-29 19:18:04 +0000

    include/sourceforge.sh s/-/_/g in $up2date, source()

--- source/include/sourceforge.sh
+++ source/include/sourceforge.sh
@@ -21,13 +21,13 @@ fi
 
 url="http://sourceforge.net/projects/$name"
 if [ -z "$_F_sourceforge_prefix" ]; then
-	up2date="lynx -dump http://sourceforge.net/project/showfiles.php?group_id=\$(lynx -dump $url|grep showfiles|sed 's/.*=\(.*\)/\1/;q')|grep 'Release Notes'|sed 's/[^]]*][^]]*]\([^ ]*\) .*/\1/;q'"
+	up2date="lynx -dump http://sourceforge.net/project/showfiles.php?group_id=\$(lynx -dump $url|grep showfiles|sed 's/.*=\(.*\)/\1/;q')|grep 'Release Notes'|sed 's/[^]]*][^]]*]\([^ ]*\) .*/\1/;s/-/_/g;q'"
 else
-	up2date="lynx -dump http://sourceforge.net/project/showfiles.php?group_id=\$(lynx -dump $url|grep showfiles|sed 's/.*=\(.*\)/\1/;q')|grep 'Release Notes'|sed 's/[^]]*][^]]*]$_F_sourceforge_prefix\([^ ]*\) .*/\1/;q'"
+	up2date="lynx -dump http://sourceforge.net/project/showfiles.php?group_id=\$(lynx -dump $url|grep showfiles|sed 's/.*=\(.*\)/\1/;q')|grep 'Release Notes'|sed 's/[^]]*][^]]*]$_F_sourceforge_prefix\([^ ]*\) .*/\1/;s/-/_/g;q'"
 fi
 if [ -z "$_F_sourceforge_ext" ]; then
-	source=(http://${_F_sourceforge_mirror}dl.sourceforge.net/sourceforge/$name/$name-$pkgver.tar.gz)
+	source=(http://${_F_sourceforge_mirror}dl.sourceforge.net/sourceforge/$name/$name-${pkgver//_/-}.tar.gz)
 else
-	source=(http://${_F_sourceforge_mirror}dl.sourceforge.net/sourceforge/$name/$name-$pkgver$_F_sourceforge_ext)
+	source=(http://${_F_sourceforge_mirror}dl.sourceforge.net/sourceforge/$name/$name-${pkgver//_/-}$_F_sourceforge_ext)
 fi
 unset realname _F_sourceforge_prefix _F_sourceforge_ext _F_sourceforge_mirror

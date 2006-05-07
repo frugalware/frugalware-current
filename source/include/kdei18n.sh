24bfd0c3eba791159d2c3b3ffda9413d03379218
VMiklos <vmiklos@frugalware.org>
2006-05-07 21:18:47 +0000

    include/kdei18n.sh added pkgver, pkgrel, groups(), archs()

--- source/include/kdei18n.sh
+++ source/include/kdei18n.sh
@@ -5,13 +5,16 @@
 # distributed under GPL License
 
 url="http://www.kde.org"
-kdever=3.5.2
-pkgurl="http://ftp.tu-chemnitz.de/pub/X11/kde/stable/$kdever/src/kde-i18n"
+pkgver=3.5.2
+pkgrel=1
+pkgurl="http://ftp.tu-chemnitz.de/pub/X11/kde/stable/$pkgver/src/kde-i18n"
 ## we just need the right version 
 up2date="lynx -dump http://www.kde.org/download/|grep 'kdelibs'|sed -n '1 p'|sed 's/.*-\([^ ]*\) .*/\1/'"
 source=($pkgurl/$pkgname-$pkgver.tar.bz2)
-depends=("kdelibs>=$kdever")
-rodepends=("kdebase>=$kdever")
+depends=("kdelibs>=$pkgver")
+rodepends=("kdebase>=$pkgver")
+groups=('locale-extra')
+archs=('i686' 'x86_64')
 options=(${options[@]} 'scriptlet')
 groups=('locale-extra')
 

52c4906ce446f4277401314ae24f0b286f568c6d
VMiklos <vmiklos@frugalware.org>
2006-05-08 23:04:11 +0000

    homepage/rss.php added author tags for packages

--- docs/homepage/frugalware/rss.php
+++ docs/homepage/frugalware/rss.php
@@ -25,12 +25,13 @@
 			include("/etc/todo.conf");
 			$conn = mysql_connect(DBHOST, DBUSER, DBPASS);
 			mysql_select_db(DBNAME, $conn);
-			$query="select groups, pkgname, id, pkgver, pkgrel, arch, `desc`, unix_timestamp(updated) from packages order by updated desc limit 10";
+			$query="select groups, pkgname, id, pkgver, pkgrel, arch, `desc`, unix_timestamp(updated), uploader from packages order by updated desc limit 10";
 			$result = mysql_query($query) or die();
 			while ($i = mysql_fetch_array($result, MYSQL_ASSOC))
 			{
 				$handle['items'][] = array("title" => preg_replace("/^([^ ]*) .*/", "$1", $i['groups']) . "/${i['pkgname']}-${i['pkgver']}-${i['pkgrel']}-${i['arch']}",
 						"desc" => $i['desc'],
+						"author" => $i['uploader'],
 						"pubDate" => date(DATE_RFC2822, $i['unix_timestamp(updated)']),
 						"link" => "http://frugalware.org/packages.php?id=${i['id']}");
 			}
@@ -74,6 +75,8 @@
 <title>" . $i['title'] . "</title>
 <description>" . htmlspecialchars($i['desc']) . "</description>
 <link>" . $i['link'] . "</link>\n");
+if(isset($i['author']))
+	print("<author>" . $i['author'] . "</author>\n");
 if(isset($i['pubDate']))
 	print("<pubDate>" . $i['pubDate'] . "</pubDate>\n");
 print("</item>");

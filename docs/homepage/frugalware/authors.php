8d0c2dfd8b8e5cf2c4832657c7739477bce5a90e
VMiklos <vmiklos@frugalware.org>
2006-05-13 18:07:08 +0000

    homepage/authors.php: cleanup for the new autogenerated AUTHORS

--- docs/homepage/frugalware/authors.php
+++ docs/homepage/frugalware/authors.php
@@ -4,38 +4,13 @@ include("header.php");
 
 $people=file($fwautorsfile);
 
-// fix mail addresses
-foreach($people as $key => $value)
-{
-	$people[$key] = str_replace(array("<", ">", "@", "á", "Á", "ä", "Ä", "é", "É", "ó", "Ó", "ö", "Ö", "ü", "Ü", "õ"), array("&lt;", "&gt;", "_at_", "&aacute;", "&Aacute;", "&auml;", "&Auml;", "&eacute;", "&Eacute;", "&oacute;", "&Oacute;", "&ouml;", "&Ouml;", "&uuml;", "&Uuml;", "&#337;"), $value);
-}
-
-// skip to list
-$i=0;
-while (!preg_match("/^Maintainers:/", $people[$i]))
-	$i++;
-$i++;
-
 fwopenbox("$fwstraudev", 100, false, $fwwhite);
 print("<div align=left><pre>");
-while (!preg_match("/^$/", $people[$i]))
+foreach($people as $key => $value)
 {
-	print($people[$i]);
-	$i++;
+	// fix mail addresses
+	print(str_replace(array("<", ">", "@"), array("&lt;", "&gt;", " at "), $value));
 }
-$i+=2;
-print("</pre></div>");
-fwclosebox(false);
-print("<p>");
-
-fwopenbox("$fwstraucontrib", 100, false, $fwwhite);
-print("<div align=left><pre>");
-while (!preg_match("/^$/", $people[$i]))
-{
-	print(preg_replace("/^\t/", "", $people[$i]));
-	$i++;
-}
-$i+=2;
 print("</pre></div>");
 fwclosebox(false);
 print("<p>");

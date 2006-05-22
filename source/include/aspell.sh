db6e6d69fa6444325bf0b1b578a2643986973fd8
VMiklos <vmiklos@frugalware.org>
2006-05-22 10:53:21 +0000

    include/aspell.sh: add pkgname, url, depends, groups, archs

--- source/include/aspell.sh
+++ source/include/aspell.sh
@@ -1,17 +1,23 @@
 #!/bin/sh
 
-# (c) 2005 Miklos Vajna <vmiklos@frugalware.org>
+# (c) 2005-2006 Miklos Vajna <vmiklos@frugalware.org>
 # aspell.sh for Frugalware
 # distributed under GPL License
 
-# common up2date, source() and build() for aspell dicts
+# common pkgname, url, depends, groups, archs, up2date, source() and build()
+# for aspell dicts
 
+pkgname=aspell6-$lang
+url="http://aspell.net/"
+depends=('aspell>=0.60')
+groups=('locale-extra')
+archs=('i686' 'x86_64')
 up2date="lynx -dump http://ftp.gnu.org/pub/gnu/aspell/dict/$lang/\?M=D|grep aspell6-$lang|sed -n -e \"s/aspell6-$lang-//\" -e 's/\.tar\..* / /' -e 's/-/r/' -e '1p'|cut -d ] -f 3|cut -d ' ' -f 1"
-source=(ftp://ftp.gnu.org/gnu/aspell/dict/$lang/$pkgname-`echo $pkgver|sed 's/r/-/'`.tar.bz2)
+source=(ftp://ftp.gnu.org/gnu/aspell/dict/$lang/$pkgname-${pkgname/r/-}.tar.bz2)
 
 build()
 {
-	Fcd $pkgname-`echo $pkgver|sed 's/r/-/'`
+	Fcd $pkgname-${pkgname/r/-}
 	./configure
 	
 	make || return 1

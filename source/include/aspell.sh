35537b44a54b6876d92eb7453569d535ff092f0f
VMiklos <vmiklos@frugalware.org>
2006-05-22 10:57:08 +0000

    include/aspell.sh: typo fix (s/pkname/pkgver/ where it was necessary)

--- source/include/aspell.sh
+++ source/include/aspell.sh
@@ -13,11 +13,11 @@ depends=('aspell>=0.60')
 groups=('locale-extra')
 archs=('i686' 'x86_64')
 up2date="lynx -dump http://ftp.gnu.org/pub/gnu/aspell/dict/$lang/\?M=D|grep aspell6-$lang|sed -n -e \"s/aspell6-$lang-//\" -e 's/\.tar\..* / /' -e 's/-/r/' -e '1p'|cut -d ] -f 3|cut -d ' ' -f 1"
-source=(ftp://ftp.gnu.org/gnu/aspell/dict/$lang/$pkgname-${pkgname/r/-}.tar.bz2)
+source=(ftp://ftp.gnu.org/gnu/aspell/dict/$lang/$pkgname-${pkgver/r/-}.tar.bz2)
 
 build()
 {
-	Fcd $pkgname-${pkgname/r/-}
+	Fcd $pkgname-${pkgver/r/-}
 	./configure
 	
 	make || return 1

4cb6ccb07a54af94f56a7c4fefa38bcfe997de59
crazy <crazy@frugalware.org>
2006-06-01 18:57:02 +0000

    dbus-0.61-5-i686 * relbump * don't start dbus-launch for fst and root * back to expat code for now xml is broken

--- source/apps/dbus/dbus.sh
+++ source/apps/dbus/dbus.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-## TODO: kill root
-dbuslaunch="`which dbus-launch 2>/dev/null`"
-if [ -n "$dbuslaunch" ] && [ -x "$dbuslaunch" ] && [ -z "$DBUS_SESSION_BUS_ADDRESS" ]; then
-  	   eval `$dbuslaunch --auto-syntax --exit-with-session`
-fi
 
+dbuslaunch="`which dbus-launch 2>/dev/null`"
+if [ "`whoami`" != "fst" -a "`whoami`" != "root" ]; then
+	if [ -n "$dbuslaunch" ] && [ -x "$dbuslaunch" ] && [ -z "$DBUS_SESSION_BUS_ADDRESS" ]; then
+  	   	eval `$dbuslaunch --auto-syntax --exit-with-session`
+	fi
+fi

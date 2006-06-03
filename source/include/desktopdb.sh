1f316123544c39e7cb52f62372177389a620edf5
VMiklos <vmiklos@frugalware.org>
2006-06-03 10:34:57 +0000

    include/desktopdb.{install,sh} new scheme for the commonly used "updating desktop database" scriptlet

---
+++ source/include/desktopdb.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# (c) 2006 Miklos Vajna <vmiklos@frugalware.org>
+# desktopdb.sh for Frugalware
+# distributed under GPL License
+
+# common scriptlet for updatating the desktop database
+
+install=$Fincdir/desktopdb.install

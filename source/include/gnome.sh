e3bff73649ba56b1b57df8f4350122ddbba0af69
VMiklos <vmiklos@frugalware.org>
2006-06-06 12:04:47 +0000

    include/gnome.sh yet another sed->bash conversion a nice speedup

--- source/include/gnome.sh
+++ source/include/gnome.sh
@@ -12,8 +12,6 @@ else
 	name=$pkgname
 fi
 
-preup2date=`echo $pkgver|sed 's/\([0-9]\+\.[0-9]\+\).*$/\1/'`
-
 pkgurl="http://ftp.gnome.org/pub/GNOME/sources"
 
 if [ "$nondevel" == "1" ]; then
@@ -22,7 +20,7 @@ else
 	up2date="lynx -dump $pkgurl/$name/\$(lynx -dump $pkgurl/$name/?C=N\;O=D|grep '[0-9]\.[0-9]*[02468]/'|sed -ne 's|.*]\(.*\)/.*|\1|' -e '1 p')/|grep ]LA|sed 's/.*S-\([0-9\.]*\).*/\1/'"
 fi
 
-source=(http://ftp.gnome.org/pub/gnome/sources/$pkgname/$preup2date/$name-$pkgver.tar.bz2)
+source=(http://ftp.gnome.org/pub/gnome/sources/$pkgname/${pkgver%.?}/$name-$pkgver.tar.bz2)
 
 options=(${options[@]} 'scriptlet')
 

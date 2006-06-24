74cd315c69ab60e6e3e6fedb1581fed2c077675a
VMiklos <vmiklos@frugalware.org>
2006-06-24 20:52:37 +0000

    include/desktopdb.*: drop replaced by gnome-scriptlet

--- source/include/desktopdb.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# (c) 2006 Miklos Vajna <vmiklos@frugalware.org>
-# desktopdb.sh for Frugalware
-# distributed under GPL License
-
-# common scriptlet for updatating the desktop database
-
-install=$Fincdir/desktopdb.install

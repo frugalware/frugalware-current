bc3c935071e0a3ec22a36e1d542138f385d127c9
VMiklos <vmiklos@frugalware.org>
2006-07-06 21:38:46 +0000

    include/firefox-extension.sh added default rodepends/groups/archs added _F_firefox_name

--- source/include/firefox-extension.sh
+++ source/include/firefox-extension.sh
@@ -6,13 +6,20 @@
 
 # provides an Fxpiinstall() function + overwrites build() to just call it
 
+rodepends=('firefox>=1.5')
+groups=('xapps-extra' 'firefox-extensions')
+archs=('i686' 'x86_64')
+
 Fxpiinstall()
 {
 	[ -z "$extname" ] && extname="$pkgname"
+	[ -n "$_F_firefox_name" ] && mv $_F_firefox_name $extname-$pkgver.xpi
 	Fmkdir /usr/lib/firefox/extensions/\{$extid\}
 	cd $Fdestdir/usr/lib/firefox/extensions/\{$extid\}
 	unzip -qqo $Fsrcdir/$extname-$pkgver.xpi || return 1
 	Ffile /usr/lib/firefox/extensions/\{$extid\}/chrome.manifest
+
+	unset _F_firefox_name
 }
 
 build()

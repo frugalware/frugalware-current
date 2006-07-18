301355d8bf5d52b6fe4d7e906375c8be527d0309
crazy <crazy@frugalware.org>
2006-07-18 13:23:24 +0000

    include/perl.sh * Ok here the 'pud' problem in chroot: ** 'pud' _NEED_ netkit-base to work but we cannot add ** netkit-base as makedepend() to perl.sh now because ** we already copy files from netkit-base in chroot and we ** have conflicts then. So till 'Lord' has some better solution :P ** I've made a up2date() line using lynx.

--- source/include/perl.sh
+++ source/include/perl.sh
@@ -21,7 +21,8 @@
 [ -z "$_F_perl_ext" ] && _F_perl_ext=".tar.gz"
 
 url="http://cpan.org/"
-up2date="pud -p 'http://search.cpan.org/search?query=$(echo $modname |sed s/-/::/g)&mode=all' -e '$modname-(.*?) '"
+up2date="lynx -dump -nolist 'http://search.cpan.org/search?query="$modname"&mode=module'|grep -m1 "$modname-"|sed -e 's/.*"$modname"-\(.*\) .*/\1/' -e 's/ .*//'"
+#up2date="pud -p 'http://search.cpan.org/search?query=$(echo $modname |sed s/-/::/g)&mode=all' -e '$modname-(.*?) '"
 source=(http://search.cpan.org/CPAN/authors/id/$modauthor/$modname-$pkgver$_F_perl_ext)
 build()
 {

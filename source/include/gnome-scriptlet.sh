76f71ee85d3553e04469e1b9be3a08b1a45a4dae
AlexExtreme <alex@alex-smith.me.uk>
2006-08-03 19:11:13 +0000

    gnome-scriptlet: added support for gconf .entries files

--- source/include/gnome-scriptlet.sh
+++ source/include/gnome-scriptlet.sh
@@ -8,6 +8,7 @@
 
 # usage:
 # _F_gnome_schemas() - if declared, gconf will be called to register them
+# _F_gnome_entries() - same as above except for gconf .entries files
 # _F_gnome_desktop - set to "y" if your package provides a .desktop file
 # _F_gnome_scrollkeeper - set to "y" if you want to run scrollkeeper
 # _F_gnome_mime - set to "y" if your package provides a mime type
@@ -56,6 +57,18 @@ Fbuild_gnome_scriptlet()
 		done
 	fi
 	Fsed '$_F_gnome_schemas' "$str" ${Fsrcdir%/src}/$_F_gnome_name
+	str=''
+	if [ -n "$_F_gnome_entries" ]; then
+		for i in "${_F_gnome_entries[@]}"
+		do
+			if [ -z "$str" ]; then
+				str="'$i'\n"
+			else
+				str="$str\t'$i'\n"
+			fi
+		done
+	fi
+	Fsed '$_F_gnome_entries' "$str" ${Fsrcdir%/src}/$_F_gnome_name
 	Fsed '$_F_gnome_desktop' "$_F_gnome_desktop" ${Fsrcdir%/src}/$_F_gnome_name
 	Fsed '$_F_gnome_scrollkeeper' "$_F_gnome_scrollkeeper" ${Fsrcdir%/src}/$_F_gnome_name
 	Fsed '$_F_gnome_mime' "$_F_gnome_mime" ${Fsrcdir%/src}/$_F_gnome_name

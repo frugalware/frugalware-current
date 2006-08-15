7577b3dadc48c92628bb518bda199ecfb071f828
VMiklos <vmiklos@frugalware.org>
2006-08-15 12:05:56 +0000

    kernel-module.sh: bump pkgrel

--- source/include/kernel-module.sh
+++ source/include/kernel-module.sh
@@ -7,7 +7,7 @@
 # a common function and a scriptlet for externel kernel module packages
 
 kernelbasever=2.6.17
-kernelrel=3
+kernelrel=4
 kernelver=$kernelbasever-$kernelrel
 _F_kernel_dir=/lib/modules/$kernelbasever-fw$kernelrel
 depends=("kernel=$kernelver")

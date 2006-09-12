97065ec19b713a46f56ca5eb92971f06018557fd
VMiklos <vmiklos@frugalware.org>
2006-09-12 09:00:50 +0000

    tools/all2db.sh: fix for nobuild pkgs

--- tools/all2db.sh
+++ tools/all2db.sh
@@ -14,7 +14,10 @@ do
 	unset pkgname pkgver pkgrel archs subpkgs groups
 	for j in `set|grep ^_F_|sed 's/=.*//'`; do unset $j; done
 	. FrugalBuild || echo "errors parsing the FrugalBuild"
-	[ -z "$pkgname" -o "$nobuild" -o "`check_option NOBUILD`" ] && continue
+	if [ -z "$pkgname" -o "$nobuild" -o "`check_option NOBUILD`" ]; then
+		cd $CWD
+		continue
+	fi
 	for j in ${archs[@]}
 	do
 		echo ${sqlarchs[@]} |grep -q $j || continue

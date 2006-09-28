889d224ba5416e2c979151aaad1c6d65368d10ab
crazy <crazy@frugalware.org>
2006-09-28 13:49:29 +0000

    dbus-0.93-2-i686 * relbump * removed root from dbus.sh to make the gnome m8r happy * added --enable-{asserts,tests,check}=yes to:   1). solve some hal problems   2). to make some gtk* stuff not segfaults is something wrong there

--- source/apps/dbus/dbus.sh
+++ source/apps/dbus/dbus.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 dbuslaunch="`which dbus-launch 2>/dev/null`"
-if [ "`whoami`" != "fst" -a "`whoami`" != "root" ]; then
+if [ "`whoami`" != "fst"  ]; then
 	if [ -n "$dbuslaunch" ] && [ -x "$dbuslaunch" ] && [ -z "$DBUS_SESSION_BUS_ADDRESS" ]; then
   	   	eval `$dbuslaunch --auto-syntax --exit-with-session`
 	fi

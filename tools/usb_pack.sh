b0b55e0d6d4ef71a9a725337fa5342ce5997cc29
VMiklos <vmiklos@frugalware.org>
2006-10-01 11:14:54 +0000

    tools/usb_pack.sh: crosscompile support

--- tools/usb_pack.sh
+++ tools/usb_pack.sh
@@ -1,12 +1,13 @@
+[ -z "$arch" ] && arch=`uname -m`
 ver=`grep '<version>' ../docs/xml/volumes.xml |sed 's/.*>\(.*\)<.*/\1/'`
 [ -z "$ver" ] && ver=`date +%Y%m%d`
 
-mkdir frugalware-$ver-`uname -m`-usb
-cd frugalware-$ver-`uname -m`-usb
+mkdir frugalware-$ver-$arch-usb
+cd frugalware-$ver-$arch-usb
 cp ../usb_install.sh ./
 chmod +x usb_install.sh
-cp -v ../../boot/initrd-`uname -m`.img.gz ./
-cp -v ../../boot/vmlinuz-*`uname -m` ./
+cp -v ../../boot/initrd-$arch.img.gz ./
+cp -v ../../boot/vmlinuz-*$arch ./
 cp -v ../../boot/grub/message ./
 cp -v ../../docs/xml/volumes.xml ./
 echo "This tarball contains all the necessary tools to install the Frugalware
@@ -14,5 +15,5 @@ echo "This tarball contains all the necessary tools to install the Frugalware
 
 Run ./usb_install.sh as root and follow the instructions." > README
 cd ..
-tar cvzf frugalware-$ver-`uname -m`-usb.tar.gz frugalware-$ver-`uname -m`-usb
-rm -rf frugalware-$ver-`uname -m`-usb
+tar cvzf frugalware-$ver-$arch-usb.tar.gz frugalware-$ver-$arch-usb
+rm -rf frugalware-$ver-$arch-usb

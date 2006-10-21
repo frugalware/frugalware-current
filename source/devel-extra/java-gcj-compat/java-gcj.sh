eae56df96823d51f68ffb0dd6dea9133c55204c8
VMiklos <vmiklos@frugalware.org>
2006-10-21 13:34:24 +0000

    java-gcj-compat-1.0.68-3-i686 java-gcj.sh: disable if requested in /etc/sysconfig/java

--- source/devel-extra/java-gcj-compat/java-gcj.sh
+++ source/devel-extra/java-gcj-compat/java-gcj.sh
@@ -1,1 +1,8 @@
-export JAVA_HOME=/usr
+# by default if both java-gcj-compat and j2re installed, the previous is the
+# preferred one. if you put "java=foo" (where foo != gcj) to /etc/sysconfig/java,
+# then this script won't touch the JAVA_HOME variable
+
+[ -e /etc/sysconfig/java ] && source /etc/sysconfig/java
+if [ -z "$java" -o "$java" == "gcj" ]; then
+	export JAVA_HOME=/usr
+fi

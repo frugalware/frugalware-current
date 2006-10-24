cccd640d2e0c1224f59ea00736205cf3a8cdbea5
VMiklos <vmiklos@frugalware.org>
2006-10-24 20:28:48 +0000

    include/firefox-extension.sh update to firefox-2.0 add support for applying patches

--- source/include/firefox-extension.sh
+++ source/include/firefox-extension.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-# (c) 2005 Miklos Vajna <vmiklos@frugalware.org>
+# (c) 2005-2006 Miklos Vajna <vmiklos@frugalware.org>
 # firefox-extension.sh for Frugalware
 # distributed under GPL License
 
 # provides an Fxpiinstall() function + overwrites build() to just call it
 
-rodepends=('firefox>=1.5')
+rodepends=('firefox>=2.0')
 groups=('xapps-extra' 'firefox-extensions')
 archs=('i686' 'x86_64')
 
@@ -17,6 +17,7 @@ Fxpiinstall()
 	Fmkdir /usr/lib/firefox/extensions/\{$extid\}
 	cd $Fdestdir/usr/lib/firefox/extensions/\{$extid\}
 	unzip -qqo $Fsrcdir/$extname-$pkgver.xpi || return 1
+	Fpatchall
 	Ffile /usr/lib/firefox/extensions/\{$extid\}/chrome.manifest
 
 	unset _F_firefox_name

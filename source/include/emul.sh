e84567d17b4f7a7ffce70fcedce376396d192013
VMiklos <vmiklos@frugalware.org>
2006-10-27 22:12:12 +0000

    include/emul.sh: ename -> _F_emul_name, earch -> _F_emul_arch

--- source/include/emul.sh
+++ source/include/emul.sh
@@ -1,24 +1,23 @@
 #!/bin/sh
 
-# (c) 2005 Miklos Vajna <vmiklos@frugalware.org>
+# (c) 2005-2006 Miklos Vajna <vmiklos@frugalware.org>
 # emul.sh for Frugalware
 # distributed under GPL License
 
 # common source() and build() for emulation of i686 on x86_64
 
-ename=`echo $pkgname|sed 's/-emul//'`
-earch=i686
+_F_emul_name=${pkgname/-emul}
+_F_emul_arch=i686
 
-up2date="lynx -dump http://ftp.frugalware.org/pub/frugalware/frugalware-current/$repo/frugalware-i686/ |grep '$ename-[^-]*-[^-]*-$earch.fpm$'|grep -v 'mingw'|sed -n 's/.*$ename-\(.*\)-\([^-]*\)-$earch.fpm/\1_\2/;$ p'"
+up2date="lynx -dump http://ftp.frugalware.org/pub/frugalware/frugalware-current/frugalware-$_F_emul_arch/ |grep '$_F_emul_name-[^-]*-[^-]*-$_F_emul_arch.fpm$'|grep -v 'mingw'|sed -n 's/.*$_F_emul_name-\(.*\)-\([^-]*\)-$_F_emul_arch.fpm/\1_\2/;$ p'"
 # requested by krix
 up2date="$pkgver"
-source=(http://ftp.frugalware.org/pub/frugalware/frugalware-current/$repo/frugalware-$earch/$ename-$pkgver-$pkgrel-$earch.fpm)
+source=(http://ftp.frugalware.org/pub/frugalware/frugalware-current/frugalware-$_F_emul_arch/$_F_emul_name-$pkgver-$pkgrel-$_F_emul_arch.fpm)
 
 build()
 {
         mkdir $Fsrcdir/tmp || return 1
-	file $ename-$pkgver-$pkgrel-$earch.fpm |grep -q bzip2 && f=j || f=z
-        tar x${f}f $ename-$pkgver-$pkgrel-$earch.fpm -C tmp || return 1
+        tar xf $_F_emul_name-$pkgver-$pkgrel-$_F_emul_arch.fpm -C tmp || return 1
         Fmkdir /usr/lib/chroot32
         cp -av tmp/* $Fdestdir/usr/lib/chroot32 || return 1
 	Frm /usr/share/{doc,man}

da73518c7ce5771a1317d1781c99983a9d3df755
VMiklos <vmiklos@frugalware.org>
2006-10-27 19:31:58 +0000

    include/berlios.sh: realname -> _F_berlios_name no pkg uses it atm, so no FB have been updated

--- source/include/berlios.sh
+++ source/include/berlios.sh
@@ -7,24 +7,20 @@
 
 # sets url, up2date and source() for a typical berlios project
 
-if ! [ -z "$realname" ]; then
-	name=$realname
-else
-	name=$pkgname
+if [ -z "$_F_berlios_name" ]; then
+	_F_berlios_name=$pkgname
 fi
 
-url="http://developer.berlios.de/projects/$name"
+url="http://developer.berlios.de/projects/$_F_berlios_name"
 
 if [ -z "$_F_berlios_prefix" ]; then
-	up2date="lynx -dump http://developer.berlios.de/project/showfiles.php?group_id=\$(lynx -dump $url| grep -m1 showfiles | sed -e 's/.*id=\(.*\)\&.*/\1/;q') | sed -e '1,/$name/d;//{s/ *[^ ]* *\([^ ]*\).*/\1/;q;};d'"
+	up2date="lynx -dump http://developer.berlios.de/project/showfiles.php?group_id=\$(lynx -dump $url| grep -m1 showfiles | sed -e 's/.*id=\(.*\)\&.*/\1/;q') | sed -e '1,/$_F_berlios_name/d;//{s/ *[^ ]* *\([^ ]*\).*/\1/;q;};d'"
 else
-	up2date="lynx -dump http://developer.berlios.de/project/showfiles.php?group_id=\$(lynx -dump $url| grep -m1 showfiles | sed -e 's/.*id=\(.*\)\&.*/\1/;q') | sed -e '1,/$name/d;//{s/ *[^ ]$_F_berlios_prefix\([^ ]*\).*/\1/;q;};d'"
+	up2date="lynx -dump http://developer.berlios.de/project/showfiles.php?group_id=\$(lynx -dump $url| grep -m1 showfiles | sed -e 's/.*id=\(.*\)\&.*/\1/;q') | sed -e '1,/$_F_berlios_name/d;//{s/ *[^ ]$_F_berlios_prefix\([^ ]*\).*/\1/;q;};d'"
 fi
 
 if [ -z "$_F_berlios_ext" ]; then
-	source=(http://download.berlios.de/$name/$name-$pkgver.tar.gz)
+	source=(http://download.berlios.de/$_F_berlios_name/$_F_berlios_name-$pkgver.tar.gz)
 else
-	source=(http://download.berlios.de/$name/$name-$pkgver$_F_berlios_ext)
+	source=(http://download.berlios.de/$_F_berlios_name/$_F_berlios_name-$pkgver$_F_berlios_ext)
 fi
-
-unset realname _F_berlios_prefix _F_berlios_ext

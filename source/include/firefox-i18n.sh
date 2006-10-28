41c2f0732c6a03f26f455517760591bb6f6b154e
VMiklos <vmiklos@frugalware.org>
2006-10-28 23:13:45 +0000

    include/firefox-i18n.sh: lang -> _F_firefox_lang

--- source/include/firefox-i18n.sh
+++ source/include/firefox-i18n.sh
@@ -7,8 +7,8 @@
 # provides a common up2date, source and build for firefox i18n packages
 
 up2date="2.0"
-source=(http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/$pkgver/linux-i686/xpi/$lang.xpi)
-pkgname=firefox-$lang
+source=(http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/$pkgver/linux-i686/xpi/$_F_firefox_lang.xpi)
+pkgname=firefox-$_F_firefox_lang
 url="http://www.mozilla.org/projects/l10n/mlp.html"
 rodepends=('firefox>=2.0')
 makedepends=('unzip')
@@ -17,8 +17,8 @@ archs=('i686' 'x86_64')
 
 build()
 {
-	unzip -qqo $lang.xpi
+	unzip -qqo $_F_firefox_lang.xpi
 	sed -i 's|chrome/||' chrome.manifest
-	Ffilerel chrome.manifest /usr/lib/firefox/chrome/$lang.manifest
-	Ffilerel chrome/$lang.jar /usr/lib/firefox/chrome/$lang.jar
+	Ffilerel chrome.manifest /usr/lib/firefox/chrome/$_F_firefox_lang.manifest
+	Ffilerel chrome/$_F_firefox_lang.jar /usr/lib/firefox/chrome/$_F_firefox_lang.jar
 }

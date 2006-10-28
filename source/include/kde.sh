068a3b0a51c442bc4d63e7c257d6a52843a4e331
VMiklos <vmiklos@frugalware.org>
2006-10-28 23:51:18 +0000

    kde.sh: kdename -> _F_kde_name, kdever -> _F_kde_ver

--- source/include/kde.sh
+++ source/include/kde.sh
@@ -6,16 +6,15 @@
 
 # common url, up2date, and source(), and build() for kde packages
 url="http://www.kde.org"
-kdever=3.5.5
-pkgurl="ftp://ftp.solnet.ch/mirror/KDE/stable/$kdever/src"
-#pkgurl="ftp://ftp.tu-chemnitz.de/pub/X11/kde/stable/$kdever/src"
-#pkgurl="ftp://ftp-stud.fht-esslingen.de/pub/Mirrors/ftp.kde.org/pub/kde/stable/$kdever/src"
+_F_kde_ver=3.5.5
+pkgurl="ftp://ftp.solnet.ch/mirror/KDE/stable/$_F_kde_ver/src"
+#pkgurl="ftp://ftp.tu-chemnitz.de/pub/X11/kde/stable/$_F_kde_ver/src"
+#pkgurl="ftp://ftp-stud.fht-esslingen.de/pub/Mirrors/ftp.kde.org/pub/kde/stable/$_F_kde_ver/src"
 
 # strip down the -docs suffix
-kdename=`echo $pkgname|sed 's/-docs$//'`
-up2date="lynx -dump http://www.kde.org/download/|grep $kdename|sed -n '1 p'|sed 's/.*-\([^ ]*\) .*/\1/'"
-source=($pkgurl/$kdename-$pkgver.tar.bz2)
-unset kdename
+_F_kde_name=`echo $pkgname|sed 's/-docs$//'`
+up2date="lynx -dump http://www.kde.org/download/|grep $_F_kde_name|sed -n '1 p'|sed 's/.*-\([^ ]*\) .*/\1/'"
+source=($pkgurl/$_F_kde_name-$pkgver.tar.bz2)
 # qt's post_install is essential for kde pkgs
 options=(${options[@]} 'scriptlet')
 

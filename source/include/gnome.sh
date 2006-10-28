4c492f99558c042c3f04c29e7672d0241661a5d1
VMiklos <vmiklos@frugalware.org>
2006-10-28 23:34:43 +0000

    include/gnome.sh: nondevel -> _F_gnome_devel, realname -> _F_gnome_name

--- source/include/gnome.sh
+++ source/include/gnome.sh
@@ -6,42 +6,41 @@
 
 # up2date and source() macro for gnome packages
 
-if ! [ -z "$realname" ]; then
-	name=$realname
-else
-	name=$pkgname
+if [ -z "$_F_gnome_name" ]; then
+	_F_gnome_name=$pkgname
 fi
 
-nondevel=1 # temp. hack for bumping to 2.15
+_F_gnome_devel="n" # temp. hack for bumping to 2.15
+
+_F_gnome_getver()
+{
+	local tmpver count
+
+	tmpver=`echo $pkgver | tr -d '[0-9][a-z]'`
+	count=`expr length "$tmpver"`
+
+	case $count in
+	    1)
+	    tmpver="$pkgver"
+	    ;;
+	    2)
+	    tmpver="${pkgver%.*}"
+	    ;;
+	    3)
+	    tmpver="${pkgver%.*.*}"
+	    ;;
+	esac
+	echo $tmpver
+}
 
 pkgurl="http://ftp.gnome.org/pub/GNOME/sources"
 
-if [ "$nondevel" == "1" ]; then
-	up2date="lynx -dump $pkgurl/$name/\$(lynx -dump $pkgurl/$name/?C=N\;O=D|grep '/'|sed -ne 's|.*]\(.*\)/.*|\1|' -e '1 p')/|grep ]LA|sed 's/.*S-\([0-9\.]*\).*/\1/'"
+if [ -n "$_F_gnome_devel" ]; then
+	up2date="lynx -dump $pkgurl/$_F_gnome_name/\$(lynx -dump $pkgurl/$_F_gnome_name/?C=N\;O=D|grep '/'|sed -ne 's|.*]\(.*\)/.*|\1|' -e '1 p')/|grep ]LA|sed 's/.*S-\([0-9\.]*\).*/\1/'"
 else
-	up2date="lynx -dump $pkgurl/$name/\$(lynx -dump $pkgurl/$name/?C=N\;O=D|grep '[0-9]\.[0-9]*[02468]/'|sed -ne 's|.*]\(.*\)/.*|\1|' -e '1 p')/|grep ]LA|sed 's/.*S-\([0-9\.]*\).*/\1/'"
+	up2date="lynx -dump $pkgurl/$_F_gnome_name/\$(lynx -dump $pkgurl/$_F_gnome_name/?C=N\;O=D|grep '[0-9]\.[0-9]*[02468]/'|sed -ne 's|.*]\(.*\)/.*|\1|' -e '1 p')/|grep ]LA|sed 's/.*S-\([0-9\.]*\).*/\1/'"
 fi
 
-tmpver=`echo $pkgver | tr -d '[0-9][a-z]'`
-count=`expr length "$tmpver"`
-
-case $count in
-    1)
-    tmpver="$pkgver"
-    ;;
-    2)
-    tmpver="${pkgver%.*}"
-    ;;
-    3)
-    tmpver="${pkgver%.*.*}"
-    ;;
-esac
-
-source=(http://ftp.gnome.org/pub/gnome/sources/$name/$tmpver/$name-$pkgver.tar.bz2)
+source=(http://ftp.gnome.org/pub/gnome/sources/$_F_gnome_name/`_F_gnome_getver`/$_F_gnome_name-$pkgver.tar.bz2)
 
 options=(${options[@]} 'scriptlet')
-
-unset realname
-unset nondevel
-unset tmpver
-unset count

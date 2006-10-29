461f825cd43d83a7e43683a87e279ce3e909c9c7
VMiklos <vmiklos@frugalware.org>
2006-10-29 00:41:43 +0000

    include/skel.sh: manual unsetting of _F_* is no longer needed

--- source/include/skel.sh
+++ source/include/skel.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
 
-# (c) 2005 Miklos Vajna <vmiklos@frugalware.org>
+# (c) 2006 Miklos Vajna <vmiklos@frugalware.org>
 # skel.sh for Frugalware
 # distributed under GPL License
 
 # one line to give the brief idea of what this .sh does
 
 # if you want the FrugalBuild to pass some parameters, then you should name
-# the variables like this: _F_foo_paramname. also don't forget to unset them
-# at the end of the script
+# the variables like this: _F_foo_paramname
 
 echo foo
-
-# unset _F_foo_paramname

ff1bf30144afe3eee15a41158be603c6e1e30083
VMiklos <vmiklos@frugalware.org>
2006-10-31 02:05:04 +0000

    include/xfce4.sh: realname -> _F_xfce_name

--- source/include/xfce4.sh
+++ source/include/xfce4.sh
@@ -7,10 +7,8 @@
 
 hpurl="http://www.xfce.org"
 
-if [ -z $realname ] ; then
-	name=$pkgname
-else
-	name=$realname
+if [ -z $_F_xfce_name ] ; then
+	_F_xfce_name=$pkgname
 fi
 
 if [ -z $_F_xfce_goodies_ext ] ; then
@@ -18,21 +16,19 @@ if [ -z $_F_xfce_goodies_ext ] ; then
 fi
 
 if [ -z $_F_xfce_goodies_dir ] ; then
-	_F_xfce_goodies_dir=$name
+	_F_xfce_goodies_dir=$_F_xfce_name
 fi
 
 if echo ${groups[*]} | grep -q goodies ; then
-	url="http://goodies.xfce.org/projects/panel-plugins/${name}"
+	url="http://goodies.xfce.org/projects/panel-plugins/${_F_xfce_name}"
 	dlurl="http://goodies.xfce.org/releases/$_F_xfce_goodies_dir/"
-	up2date="lynx -dump $dlurl | grep "$name-.*${_F_xfce_goodies_ext}$" | sed -n 's/.*-\(.*\)\.t.*/\1/;$ p'"
-	source=($dlurl/${name}-${pkgver}${_F_xfce_goodies_ext})
+	up2date="lynx -dump $dlurl | grep "$_F_xfce_name-.*${_F_xfce_goodies_ext}$" | sed -n 's/.*-\(.*\)\.t.*/\1/;$ p'"
+	source=($dlurl/${_F_xfce_name}-${pkgver}${_F_xfce_goodies_ext})
 else
 	preup2date="lynx -dump http://www.xfce.org/archive | grep 'xfce-' | sed -n 's/.*-\(.*\)\.t.*/\1/;$ p' | sed 's/[0-9][0-9]\. http:\/\/www\.xfce\.org\/archive\/xfce-//g' | sed 's/ //g' | sed 's/\///g'"
 	dlurl="$hpurl/archive/xfce-4.3.99.1/src"
-	up2date="lynx -dump $hpurl/archive/xfce-\$($preup2date)/src/ | grep $name | Flasttarbz2"
-	source=($dlurl/$name-$pkgver.tar.bz2)
+	up2date="lynx -dump $hpurl/archive/xfce-\$($preup2date)/src/ | grep $_F_xfce_name | Flasttarbz2"
+	source=($dlurl/$_F_xfce_name-$pkgver.tar.bz2)
 fi
 
 options=(${options[@]} 'scriptlet')
-
-unset realname

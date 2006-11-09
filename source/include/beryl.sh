4529f6df786226741e48d050d71396ba650e4739
AlexExtreme <alex@alex-smith.me.uk>
2006-11-09 16:11:10 +0000

    beryl.sh: version bump to 0.1.2 - finally, there are official release tarballs \o/

--- source/include/beryl.sh
+++ source/include/beryl.sh
@@ -6,26 +6,13 @@
 
 # up2date and source() macro for beryl packages
 
-pkgver=0.1.1
+pkgver=0.1.2
 pkgdesc="Beryl is a compositing window manager which provides lots of fancy effects on your desktop"
-url="http://forum.beryl-project.org/"
-up2date="$pkgver"
-srcurl="http://ftp.frugalware.org/pub/other/sources/beryl"
-source=($srcurl/$pkgname-$pkgver.tar.bz2 $srcurl/beryl-VERSION-$pkgver)
-signatures=($srcurl/$pkgname-$pkgver.tar.bz2.asc $srcurl/beryl-VERSION-$pkgver.asc)
+url="http://www.beryl-project.org/"
+up2date="lynx -dump http://releases.beryl-project.org/current/ | grep $pkgname | Flasttarbz2"
+srcurl="http://releases.beryl-project.org/current"
+source=($srcurl/$pkgname-$pkgver.tar.bz2)
 options=(${options[@]} 'force')
-
-Fbuild_beryl() {
-	cd $Fsrcdir || Fdie
-	cp beryl-VERSION-$pkgver VERSION || Fdie
-	Fcd
-	./autogen.sh || Fdie
-	Fbuild
-}
-
-build() {
-	Fbuild_beryl
-}
 
 unset MAKEFLAGS
 

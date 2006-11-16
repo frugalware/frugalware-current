f4e449cdc4e1e798c91b601b228728393c522668
crazy <crazy@frugalware.org>
2006-11-16 00:04:20 +0000

    sourceforge.sh * added _F_sourceforge_broken_up2date , default is off ( 0 ) ( huch ? :P) * ok let me explain. there are projects on sf.net have : - 1) more files apps in just a dir ( foo-1.gz blubb-2.bz2 etc ) - 2) broken project _dirs_ ( here a normal up2date won't work at all ) - 3) sf.net changed layout again so we have a big mess :| - 4) such */sort_by=? up2date lines for sf.net won't work anymore * so if you get some app does not work with only Finclude sourceforge for up2date try   add _F_sourceforge_broken_up2date=1 which does this :   lynx -dump _the_project_id|grep -m1 '$_F_sourceforge_name-\(.*\)$_F_sourceforge_ext'|sed 's/.*-\(.*\).t.*/\1/' * /me waits for vmiklos slap =)

--- source/include/sourceforge.sh
+++ source/include/sourceforge.sh
@@ -23,7 +23,14 @@ if [ -z "$_F_sourceforge_ext" ]; then
 	_F_sourceforge_ext=".tar.gz"
 fi
 
+if [ -z "$_F_sourceforge_broken_up2date" ]; then
+        _F_sourceforge_broken_up2date=0
+fi
+
 url="http://sourceforge.net/projects/$_F_sourceforge_name"
-#up2date="lynx -dump http://sourceforge.net/project/showfiles.php?group_id=\$(lynx -dump $url|grep showfiles|sed 's/.*=\(.*\)/\1/;q')|grep 'Release Notes'|sed 's/[^]]*][^]]*]$_F_sourceforge_prefix\([^ ]*\) .*/\1/;s/-/_/g;q'"
-up2date="lynx -dump http://sourceforge.net/project/showfiles.php?group_id=\$(lynx -dump $url|grep showfiles|sed 's/.*=\(.*\)/\1/;q')|grep -m1 'Latest \[.*\]'|sed 's/.*]$_F_sourceforge_prefix\(.*\) \[.*\].*/\1/'"
+if [ $_F_sourceforge_broken_up2date -eq 0 ]; then
+	up2date="lynx -dump http://sourceforge.net/project/showfiles.php?group_id=\$(lynx -dump $url|grep showfiles|sed 's/.*=\(.*\)/\1/;q')|grep -m1 'Latest \[.*\]'|sed 's/.*]$_F_sourceforge_prefix\(.*\) \[.*\].*/\1/'"
+else
+ 	up2date="lynx -dump http://sourceforge.net/project/showfiles.php?group_id=\$(lynx -dump $url|grep showfiles|sed 's/.*=\(.*\)/\1/;q')|grep -m1 '$_F_sourceforge_name-\(.*\)$_F_sourceforge_ext'|sed 's/.*-\(.*\).t.*/\1/'"
+fi
 source=(http://${_F_sourceforge_mirror}.dl.sourceforge.net/sourceforge/$_F_sourceforge_dirname/$_F_sourceforge_name-${pkgver//_/-}$_F_sourceforge_ext)

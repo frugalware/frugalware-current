d63e11d561da929c02edd8760b739f48d3787c73
Christian Hamar alias krix <krics@linuxforum.hu>
2006-11-20 21:38:51 +0000

    emul.sh: new versioning scheme for emul packs   * Added support for new versioning scheme for source download too     (ty vmiklos :P )

--- source/include/emul.sh
+++ source/include/emul.sh
@@ -12,7 +12,8 @@ _F_emul_arch=i686
 up2date="lynx -dump http://ftp.frugalware.org/pub/frugalware/frugalware-current/frugalware-$_F_emul_arch/ |grep '$_F_emul_name-[^-]*-[^-]*-$_F_emul_arch.fpm$'|grep -v 'mingw'|sed -n 's/.*$_F_emul_name-\(.*\)-\([^-]*\)-$_F_emul_arch.fpm/\1_\2/;$ p'"
 # requested by krix
 up2date="$pkgver"
-source=(http://ftp.frugalware.org/pub/frugalware/frugalware-current/frugalware-$_F_emul_arch/$_F_emul_name-$pkgver-$pkgrel-$_F_emul_arch.fpm)
+#source=(http://ftp.frugalware.org/pub/frugalware/frugalware-current/frugalware-$_F_emul_arch/$_F_emul_name-$pkgver-$pkgrel-$_F_emul_arch.fpm)
+source=(http://ftp.frugalware.org/pub/frugalware/frugalware-current/frugalware-$_F_emul_arch/$_F_emul_name-${pkgver/_/-}-$_F_emul_arch.fpm)
 
 build()
 {

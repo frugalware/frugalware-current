9ac5f3dbad7eb60f29883d2571637a519a74e8f8
AlexExtreme <alex@alex-smith.me.uk>
2006-12-10 09:55:07 +0000

    beryl.sh: version bump to 0.1.3

--- source/include/beryl.sh
+++ source/include/beryl.sh
@@ -10,11 +10,11 @@ if [ -z "$_F_beryl_name" ]; then
 	_F_beryl_name=$pkgname
 fi
 
-pkgver=0.1.2
+pkgver=0.1.3
 pkgdesc="Beryl is a compositing window manager which provides lots of fancy effects on your desktop"
 url="http://www.beryl-project.org/"
 up2date="lynx -dump http://releases.beryl-project.org/current/ | grep $_F_beryl_name | Flasttarbz2"
-srcurl="http://releases.beryl-project.org/current"
+srcurl="http://releases.beryl-project.org/$pkgver"
 source=($srcurl/$_F_beryl_name-$pkgver.tar.bz2)
 options=(${options[@]} 'force')
 _F_cd_path="$_F_beryl_name-$pkgver"

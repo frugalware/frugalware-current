7155af46735d39053407fd6f2e3a2dcfb30e64f0
VMiklos <vmiklos@frugalware.org>
2006-12-12 20:36:22 +0000

    kernel-2.6.19-2-i686 bump to patchlevel '1' 6e38433357e2381bb278a418fb7e2fd201475101.diff and CVE-2006-6333.diff is now in upstream

--- source/include/kernel-module.sh
+++ source/include/kernel-module.sh
@@ -7,7 +7,7 @@
 # a common function and a scriptlet for externel kernel module packages
 
 _F_kernelmod_ver=2.6.19
-_F_kernelmod_rel=1
+_F_kernelmod_rel=2
 _F_kernelmod_uname=$_F_kernelmod_ver-fw$_F_kernelmod_rel
 _F_kernelmod_pkgver=${_F_kernelmod_uname/fw}
 _F_kernelmod_dir=/lib/modules/$_F_kernelmod_uname

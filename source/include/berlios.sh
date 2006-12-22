fe783d678b70de4f799ec64569894f76044a00ec
crazy <crazy@frugalware.org>
2006-12-22 01:16:46 +0000

    berlios.sh * fixed up2date | _F_berlios_ext * removed _F_berlios_prefix ( nothing use it and the layout they use now does not need it ) * changed source too

--- source/include/berlios.sh
+++ source/include/berlios.sh
@@ -11,16 +11,10 @@ if [ -z "$_F_berlios_name" ]; then
 	_F_berlios_name=$pkgname
 fi
 
-url="http://developer.berlios.de/projects/$_F_berlios_name"
-
-if [ -z "$_F_berlios_prefix" ]; then
-	up2date="lynx -dump http://developer.berlios.de/project/showfiles.php?group_id=\$(lynx -dump $url| grep -m1 showfiles | sed -e 's/.*id=\(.*\)\&.*/\1/;q') | sed -e '1,/$_F_berlios_name/d;//{s/ *[^ ]* *\([^ ]*\).*/\1/;q;};d'"
-else
-	up2date="lynx -dump http://developer.berlios.de/project/showfiles.php?group_id=\$(lynx -dump $url| grep -m1 showfiles | sed -e 's/.*id=\(.*\)\&.*/\1/;q') | sed -e '1,/$_F_berlios_name/d;//{s/ *[^ ]$_F_berlios_prefix\([^ ]*\).*/\1/;q;};d'"
-fi
-
 if [ -z "$_F_berlios_ext" ]; then
-	source=(http://download.berlios.de/$_F_berlios_name/$_F_berlios_name-$pkgver.tar.gz)
-else
-	source=(http://download.berlios.de/$_F_berlios_name/$_F_berlios_name-$pkgver$_F_berlios_ext)
+        _F_berlios_ext=".tar.gz"
 fi
+
+url="http://developer.berlios.de/projects/$_F_berlios_name"
+up2date="lynx -dump http://developer.berlios.de/project/showfiles.php?group_id=\$(lynx -dump $url| grep -m1 showfiles | sed -e 's/.*id=\(.*\)\&.*/\1/;q') | grep -m1 '$_F_berlios_name-\(.*\)$_F_berlios_ext'| sed 's/.*$_F_berlios_name-\(.*\)$_F_berlios_ext.*/\1/;s/-/_/g'"
+source=(http://download.berlios.de/$_F_berlios_name/$_F_berlios_name-${pkgver//_/-}$_F_berlios_ext)

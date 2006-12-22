1a23feff93d6a836e12f6386f8dbc82f03ec78a6
VMiklos <vmiklos@frugalware.org>
2006-12-22 14:45:02 +0000

    j2sdk-6-1-i686 version bump updated j2sdk.sh based on openjdk.sh

--- source/devel-extra/j2sdk/j2sdk.sh
+++ source/devel-extra/j2sdk/j2sdk.sh
@@ -1,5 +1,13 @@
 export J2SDKDIR=/usr/lib/java
-export PATH=$PATH:$J2SDKDIR/bin
 export MANPATH=$MANPATH:$J2SDKDIR/man
-export JAVA_HOME=$J2SDKDIR
 export CLASSPATH=$CLASSPATH:$J2SDKDIR/lib
+
+# if you put "java=foo" (where foo != j2sdk) to /etc/sysconfig/java,
+# then this script won't touch the JAVA_HOME variable
+[ -e /etc/sysconfig/java ] && source /etc/sysconfig/java
+if [ -z "$java" -o "$java" == "j2sdk" ]; then
+	export JAVA_HOME=$J2SDKDIR
+	export PATH=$J2SDKDIR/bin:$PATH
+else
+	export PATH=$PATH:$J2SDKDIR/bin
+fi

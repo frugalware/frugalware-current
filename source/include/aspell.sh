ad707b73adfeb225c7beb279ca9a1dd627c1e5eb
VMiklos <vmiklos@frugalware.org>
2006-12-30 12:35:16 +0000

    include/aspell.sh: added signatures()

--- source/include/aspell.sh
+++ source/include/aspell.sh
@@ -16,6 +16,7 @@ groups=('locale-extra')
 archs=('i686' 'x86_64')
 up2date="lynx -dump http://ftp.gnu.org/pub/gnu/aspell/dict/$_F_aspell_lang/\?M=D|grep aspell$_F_aspell_ver-$_F_aspell_lang|sed -n -e \"s/aspell$_F_aspell_ver-$_F_aspell_lang-//\" -e 's/\.tar\..* / /' -e 's/-/r/' -e '1p'|cut -d ] -f 3|cut -d ' ' -f 1"
 source=(ftp://ftp.gnu.org/gnu/aspell/dict/$_F_aspell_lang/$pkgname-${pkgver/r/-}.tar.bz2)
+signatures=($source.sig)
 
 build()
 {

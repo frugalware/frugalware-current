961d76392a6e96b6cb356fa1a13fefdacb208b69
crazy <crazy@frugalware.org>
2007-01-27 17:13:25 +0000

    kde.sh * changed _F_kde_name , all *-docs are subpkgs now * merged some things from kde-split.sh from my local tree on which I don't work anymore * build with --disable-final by default ( br0ken again in 3.5.6 and no one really cares to fix the code ) * added some 'default' configure options * killed build()

--- source/include/kde.sh
+++ source/include/kde.sh
@@ -5,28 +5,38 @@
 # distributed under GPL License
 
 # common url, up2date, and source(), and build() for kde packages
+
+if [ -z "$_F_kde_name" ]; then
+        _F_kde_name=$pkgname
+fi
+
+if [ -z "$_F_kde_build_debug" ]; then
+        _F_kde_build_debug=0
+fi
+
+if [ -z "$_F_cd_path" ]; then
+        _F_cd_path=$_F_kde_name-$pkgver
+fi
+
 url="http://www.kde.org"
 _F_kde_ver=3.5.5
 pkgurl="ftp://ftp.solnet.ch/mirror/KDE/stable/$_F_kde_ver/src"
-#pkgurl="ftp://ftp.tu-chemnitz.de/pub/X11/kde/stable/$_F_kde_ver/src"
-#pkgurl="ftp://ftp-stud.fht-esslingen.de/pub/Mirrors/ftp.kde.org/pub/kde/stable/$_F_kde_ver/src"
-
-# strip down the -docs suffix
-_F_kde_name=`echo $pkgname|sed 's/-docs$//'`
-up2date="lynx -dump http://www.kde.org/download/|grep $_F_kde_name|sed -n '1 p'|sed 's/.*-\([^ ]*\) .*/\1/'"
+up2date="lynx -dump http://www.kde.org/download/|grep '$_F_kde_name'|sed -n '1 p'|sed 's/.*-\([^ ]*\) .*/\1/'"
 source=($pkgurl/$_F_kde_name-$pkgver.tar.bz2)
 # qt's post_install is essential for kde pkgs
 options=(${options[@]} 'scriptlet')
+## Well this final is almost broken , disabled by default
+Fconfopts="$Fconfopts \
+                --disable-dependency-tracking \
+                --with-gnu-ld \
+                --enable-gcc-hidden-visibility \
+                --enable-new-ldflags \
+		--disable-finale"
 
-#if [ "`cat /proc/meminfo |grep MemTotal|sed 's/.* \(.*\) kB/\1/'`" -ge 500000 ]; then
-#	Fconfopts="$Fconfopts --enable-final"
-#fi
+if [ "$_F_kde_build_debug" -eq 1 ]; then
+        Fconfopts="$Fconfopts --enable-debug --with-debug"
+else
+        Fconfopts="$Fconfopts --disable-debug --without-debug"
+fi
 
-build() 
-{
-     Fbuild CXXFLAGS="$CXXFLAGS -Wno-deprecated" \
-		--disable-dependency-tracking \
-		--disable-debug --without-debug \
-		--with-gnu-ld --enable-gcc-hidden-visibility
-}
 

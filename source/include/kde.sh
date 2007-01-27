9fd30946735f6403d2c28e8afe0c9708a39836e3
crazy <crazy@frugalware.org>
2007-01-27 17:34:02 +0000

    kde.sh * added this /etc config stuff in here _with a BIG FAT WARNING_ before I forget about * of course disabled.

--- source/include/kde.sh
+++ source/include/kde.sh
@@ -25,7 +25,13 @@ up2date="lynx -dump http://www.kde.org/download/|grep '$_F_kde_name'|sed -n '1 p
 source=($pkgurl/$_F_kde_name-$pkgver.tar.bz2)
 # qt's post_install is essential for kde pkgs
 options=(${options[@]} 'scriptlet')
-## Well this final is almost broken , disabled by default
+## If someone want to work on this /etc move here is a way to do it. Just add ${kde_config} to Fconfopts
+## !!!BIG FAT WARNING!!!: THE WHOLE KDE AND _EVERY DAMN KDE_APP NEED BE REBUILD WITH THIS_ 
+## 			  !!! DO NOT EVER REMOVE THIS FROM DEFAULTS ONCE IS ADDED OR KDE BREAKS !!!
+
+#kde_config="kde_confdir=/etc/kde3/config \
+#            kde_kcfgdir=/etc/kde3/config.kcfg"
+
 Fconfopts="$Fconfopts \
                 --disable-dependency-tracking \
                 --with-gnu-ld \
@@ -38,5 +44,6 @@ if [ "$_F_kde_build_debug" -eq 1 ]; then
 else
         Fconfopts="$Fconfopts --disable-debug --without-debug"
 fi
+
 
 

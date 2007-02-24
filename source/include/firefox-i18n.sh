e60e664d5c9a85e198c546ec131a86f26161ddf0
VMiklos <vmiklos@frugalware.org>
2007-02-24 16:16:12 +0000

    include/firefox-i18n.sh: added asciidoc documentation

--- source/include/firefox-i18n.sh
+++ source/include/firefox-i18n.sh
@@ -1,10 +1,35 @@
 #!/bin/sh
 
-# (c) 2006 Miklos Vajna <vmiklos@frugalware.org>
-# firefox-i18n.sh for Frugalware
-# distributed under GPL License
-
-# provides a common up2date, source and build for firefox i18n packages
+###
+# = firefox-i18n.sh(3)
+# Miklos Vajna <vmiklos@frugalware.org>
+#
+# == NAME
+# firefox-i18n.sh - for Frugalware
+#
+# == SYNOPSIS
+# Common schema for Firefox language packages.
+#
+# == EXAMPLE
+# --------------------------------------------------
+# _F_firefox_lang=it
+# pkgver=2.0
+# pkgrel=1
+# pkgdesc="Italian language support for Firefox"
+# Finclude firefox-i18n
+# sha1sums=('0b8a517c87e9e2c0143eb60f10ce0e07316eb733')
+# --------------------------------------------------
+#
+# == OVERWRITTEN VARIABLES
+# * up2date
+# * source()
+# * pkgname
+# * url
+# * rodepends()
+# * makedepends()
+# * groups()
+# * archs()
+###
 
 up2date="2.0"
 source=(http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/$pkgver/linux-i686/xpi/$_F_firefox_lang.xpi)
@@ -15,6 +40,10 @@ makedepends=('unzip')
 groups=('locale-extra')
 archs=('i686' 'x86_64')
 
+###
+# == PROVIDED FUNCTIONS
+# * build()
+###
 build()
 {
 	unzip -qqo $_F_firefox_lang.xpi

afea5fe49397ea5c8ccd2f9143da9fb8e8c92b22
VMiklos <vmiklos@frugalware.org>
2007-02-24 16:34:22 +0000

    include/skel.sh: added asciidoc documentation

--- source/include/skel.sh
+++ source/include/skel.sh
@@ -1,12 +1,57 @@
 #!/bin/sh
 
-# (c) 2006 Miklos Vajna <vmiklos@frugalware.org>
-# skel.sh for Frugalware
-# distributed under GPL License
+###
+# = skel.sh(3)
+# Miklos Vajna <vmiklos@frugalware.org>
+#
+# == NAME
+# skel.sh - for Frugalware
+#
+# == SYNOPSIS
+# Common schema for skeleton packages.
+#
+# == EXAMPLE
+# --------------------------------------------------
+# # Here comes a working example - without the 2-lines-length header and
+# # without any #optimization ok and other footnodes.
+# --------------------------------------------------
+#
+# == OPTIONS
+# * _F_skel_name (defaults to $pkgname): if you want to use a custom package
+# name (for example the upstream name contains uppercase letters) then use this
+# to declare the real name
+###
+if [ -z "$_F_berlios_name" ]; then
+	_F_berlios_name=$pkgname
+fi
 
-# one line to give the brief idea of what this .sh does
+###
+# == OVERWRITTEN VARIABLES
+# * up2date
+# * source()
+###
+up2date="foo"
+source=(bar)
 
-# if you want the FrugalBuild to pass some parameters, then you should name
-# the variables like this: _F_foo_paramname
+###
+# == APPENDED VARIABLES
+# * force to options()
+###
+options=(${options[@]} 'force')
 
-echo foo
+###
+# == PROVIDED FUNCTIONS
+# * Fbuildskel()
+###
+Fbuildskel()
+{
+	true
+}
+
+###
+# * build() just calls Fbuildskel()
+###
+build()
+{
+	Fbuildskel
+}

6682d916a74504762c09da1da8fbff8073c85dd6
VMiklos <vmiklos@frugalware.org>
2007-02-24 14:21:59 +0000

    include/aspell.sh: added asciidoc documentation

--- source/include/aspell.sh
+++ source/include/aspell.sh
@@ -1,14 +1,43 @@
 #!/bin/sh
 
-# (c) 2005-2006 Miklos Vajna <vmiklos@frugalware.org>
-# aspell.sh for Frugalware
-# distributed under GPL License
-
-# common pkgname, url, depends, groups, archs, up2date, source() and build()
-# for aspell dicts
-
+###
+# = aspell.sh(3)
+# Miklos Vajna <vmiklos@frugalware.org>
+#
+# == NAME
+# aspell.sh - for Frugalware
+#
+# == SYNOPSIS
+# Common schema for aspell dictonary packages.
+#
+# == EXAMPLE
+# --------------------------------------------------
+# # Compiling Time:  SBU
+# # Maintainer: VMiklos <vmiklos@frugalware.org>
+#
+# _F_aspell_lang=de
+# pkgver=20030222r1
+# pkgrel=1
+# pkgdesc="GNU Aspell 0.60 German Word List Package"
+# Finclude aspell
+# --------------------------------------------------
+#
+# == OPTIONS
+# * _F_aspell_ver (defaults to 6): the dictionary is made for this version of aspell
+###
 [ -z "$_F_aspell_ver" ] && _F_aspell_ver=6
 
+###
+# == OVERWRITTEN VARIABLES
+# * pkgname
+# * url
+# * depends()
+# * groups()
+# * archs()
+# * up2date
+# * source()
+# * signatures
+###
 pkgname=aspell$_F_aspell_ver-$_F_aspell_lang
 url="http://aspell.net/"
 depends=('aspell>=0.60')
@@ -18,6 +47,10 @@ up2date="lynx -dump http://ftp.gnu.org/pub/gnu/aspell/dict/$_F_aspell_lang/\?M=D
 source=(ftp://ftp.gnu.org/gnu/aspell/dict/$_F_aspell_lang/$pkgname-${pkgver/r/-}.tar.bz2)
 signatures=($source.sig)
 
+###
+# == PROVIDED FUNCTIONS
+# * build()
+###
 build()
 {
 	Fcd $pkgname-${pkgver/r/-}

a8c36e5a86024dee6539f4bcced13a1de1fc970f
VMiklos <vmiklos@frugalware.org>
2007-02-24 14:43:28 +0000

    include/e17.sh: added asciidoc documentation

--- source/include/e17.sh
+++ source/include/e17.sh
@@ -1,9 +1,37 @@
 #!/bin/sh
 
-# (c) 2006 Andras Voroskoi <voroskoi@frugalware.org>
-# e17.sh for Frugalware
-# distributed under GPL License
+###
+# = e17.sh(3)
+# Andras Voroskoi <voroskoi@frugalware.org>
+#
+# == NAME
+# e17.sh - for Frugalware
+#
+# == SYNOPSIS
+# Common schema for e17 packages.
+#
+# == EXAMPLE
+# --------------------------------------------------
+# pkgname=ewl
+# pkgver=0.0.4.007
+# pkgrel=2
+# pkgdesc="EWL is a simple-to-use general purpose widget library based on the \
+# 	Evas canvas for E17."
+# depends=('edje>=0.5.0.033-2' 'evas>=0.9.9.033' 'ecore>=0.9.9.033-2')
+# makedepends=('edb')
+# groups=('e17-extra' 'e17-misc')
+# archs=('i686' 'x86_64')
+# Finclude e17
+# sha1sums=('c003a1b0423a61a28b76aa68bde59e0f333ae6d3')
+# --------------------------------------------------
+###
 
+###
+# == OVERWRITTEN VARIABLES
+# * url
+# * source()
+# * up2date
+###
 url="http://enlightenment.freedesktop.org/"
 source=($url/files/$pkgname-$pkgver.tar.gz)
 up2date="lynx -dump $url |grep =$pkgname |sed -e 's/.*$pkgname-\(.*\).t.*/\1/' |Fsort |sed -n '$ p'"

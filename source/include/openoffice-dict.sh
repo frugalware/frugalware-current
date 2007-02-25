3f30acbbc876f0eaa03f8055bbee48fca5e5c217
VMiklos <vmiklos@frugalware.org>
2007-02-25 19:55:26 +0000

    include/openoffice-dict.sh: added asciidoc documentation

--- source/include/openoffice-dict.sh
+++ source/include/openoffice-dict.sh
@@ -1,18 +1,50 @@
 #!/bin/sh
 
-# (c) 2006 Miklos Vajna <vmiklos@frugalware.org>
-# openoffice-dict.sh for Frugalware
-# distributed under GPL License
+###
+# = openoffice-dict.sh(3)
+# Miklos Vajna <vmiklos@frugalware.org>
+#
+# == NAME
+# openoffice-dict.sh - for Frugalware
+#
+# == SYNOPSIS
+# Common schema for OpenOffice.org dictionary packages.
+#
+# == EXAMPLE
+# --------------------------------------------------
+# pkgname=openoffice.org-dict-de
+# _F_openoffice_name=de_DE
+# dictoptions=('nodict' 'nohyph')
+# pkgver=20031107
+# pkgrel=2
+# pkgdesc="German dictionary for OpenOffice.org."
+# rodepends=('openoffice.org')
+# groups=('locale-extra')
+# archs=('i686' 'x86_64')
+# Finclude openoffice-dict
+# sha1sums=('31a96c0de139056cf3a0a525c68d6bdf2ce79bae')
+# --------------------------------------------------
+#
+# == OPTIONS
+# * _F_openoffice_name: the code of the language in some xx_YY format
+# * dictoptions(): possible values: nodict, nohyph, noth for disabling the
+# installation of the dictionary, the hyphenation and/or the thesaurus files.
+###
 
-# common url up2date, source(), build() and .install for openoffice.org dictionaries
-
+###
+# == OVERWRITTEN VARIABLES
+# * url
+# * up2date
+# * source()
+# * install
+###
 url="ftp://ftp.services.openoffice.org/pub/OpenOffice.org/contrib/dictionaries/"
 # no way to determine pkgver automatically :-/
 up2date="$pkgver"
 source=(ftp://ftp.services.openoffice.org/pub/OpenOffice.org/contrib/dictionaries/$_F_openoffice_name-pack.zip)
-
 install=$Fincdir/openoffice-dict.install
 
+# don't document this, should not be used from FrugalBuilds
 _F_openoffice_option() {
 	local i
 	for i in ${dictoptions[@]}; do
@@ -25,6 +57,10 @@ _F_openoffice_option() {
 	done
 }
 
+###
+# == PROVIDED FUNCTIONS
+# * build()
+###
 build()
 {
 	for i in *.zip
@@ -54,4 +90,3 @@ build()
 		fi
 	fi
 }
-

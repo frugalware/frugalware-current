643dbfd656255595b6054eb48441d537956ad0f7
VMiklos <vmiklos@frugalware.org>
2007-02-26 01:09:25 +0000

    include/rox.sh: added asciidoc documentation

--- source/include/rox.sh
+++ source/include/rox.sh
@@ -1,13 +1,50 @@
 #!/bin/sh
 
-# (c) 2006 Marcus Habermehl <bmh1980@frugalware.org>
-# rox.sh for Frugalware
-# distributed under GPL License
+###
+# = rox.sh(3)
+# Marcus Habermehl <bmh1980@frugalware.org>
+#
+# == NAME
+# rox.sh - for Frugalware
+#
+# == SYNOPSIS
+# Common schema for ROX packages.
+#
+# == EXAMPLE
+# --------------------------------------------------
+# pkgname=rox-load
+# pkgver=2.1.4
+# pkgrel=1
+# pkgdesc="This applet displays the load average of your system."
+# url="http://www.kerofin.demon.co.uk/rox/load.html"
+# license="GPL2"
+# up2date="lynx -dump $url|grep 'Load [0-9\.]\+[0-9]'|tail -n 1|cut -d ' ' -f 8"
+# source=(http://www.kerofin.demon.co.uk/rox/Load-$pkgver.tar.gz $pkgname)
+# sha1sums=('1be4ef2394dbdebaab9ebf7240aae93ee3a13177' \
+#           'b181a0765fb3323072aba1dfb4ac7abd6907c9fd')
+# groups=('rox-extra')
+# archs=('i686')
+# depends=('libgtop' 'rox-clib')
+# _F_app_dir="Load"
+# Finclude rox
+# --------------------------------------------------
+#
+# == OPTIONS
+# * _F_app_dir: the name of the rox applet
+###
 
 [ -z "$_F_app_dir" ] && Fdie
 
+###
+# == OVERWRITTEN VARIABLES
+# * options
+###
 options=('nobuild')
 
+###
+# == PROVIDED FUNCTIONS
+# * Froxbuild()
+###
 Froxbuild()
 {
 	Fcd $_F_app_dir
@@ -27,6 +64,9 @@ Froxbuild()
 	fi
 }
 
+###
+# * build() just calls Froxbuild()
+###
 build()
 {
 	Froxbuild

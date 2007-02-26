cd1802be2cb940b15fed5962d6c5010921c88a5f
VMiklos <vmiklos@frugalware.org>
2007-02-26 20:42:18 +0000

    include/thunderbird-i18n.sh: added asciidoc documentation

--- source/include/thunderbird-i18n.sh
+++ source/include/thunderbird-i18n.sh
@@ -1,11 +1,36 @@
 #!/bin/sh
 
-# (c) 2005-2006 Miklos Vajna <vmiklos@frugalware.org>
-# thunderbird-i18n.sh for Frugalware
-# distributed under GPL License
-
-# provides a common up2date, source and build for thunderbird i18n packages
-
+###
+# = thunderbird-i18n.sh(3)
+# Miklos Vajna <vmiklos@frugalware.org>
+#
+# == NAME
+# thunderbird-i18n.sh - for Frugalware
+#
+# == SYNOPSIS
+# Common schema for Thunderbird language packages.
+#
+# == EXAMPLE
+# --------------------------------------------------
+# _F_thunderbird_lang=fi
+# pkgname=thunderbird-$_F_thunderbird_lang
+# pkgver=1.5.0.7
+# pkgrel=1
+# pkgdesc="Finnish language support for Thunderbird"
+# Finclude thunderbird-i18n
+# sha1sums=('ed10b93d8fc4bd9ea7a0fe698fed82e519e7dc5a')
+# --------------------------------------------------
+#
+# == OVERWRITTEN VARIABLES
+# * up2date
+# * url
+# * source()
+# * options()
+# * depends()
+# * makedepends()
+# * groups()
+# * archs()
+###
 up2date="1.5.0.7"
 url="http://www.mozilla.org/projects/l10n/mlp.html"
 source=(http://ftp.mozilla.org/pub/mozilla.org/thunderbird/releases/$pkgver/linux-i686/xpi/$_F_thunderbird_lang.xpi)
@@ -15,6 +40,10 @@ makedepends=('unzip')
 groups=('locale-extra')
 archs=('i686' 'x86_64')
 
+###
+# == PROVIDED FUNCTIONS
+# * build()
+###
 build()
 {
 	unzip -qqo $_F_thunderbird_lang.xpi

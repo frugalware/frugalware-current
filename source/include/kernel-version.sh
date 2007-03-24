ebf3eea688b2e7cc9aacdb17ea2124c160fe959e
VMiklos <vmiklos@frugalware.org>
2007-03-24 12:39:12 +0000

    include/kernel-version.sh: new variable: _F_kernelver_stable

--- source/include/kernel-version.sh
+++ source/include/kernel-version.sh
@@ -13,6 +13,8 @@
 # == OVERWRITTEN VARIABLES
 # * _F_kernelver_ver: the kernel version
 # * _F_kernelver_rel: the kernel release
+# * _F_kernelver_stable: the number of the -stable patch to use (if any)
 ###
 _F_kernelver_ver=2.6.20
 _F_kernelver_rel=4
+_F_kernelver_stable=3

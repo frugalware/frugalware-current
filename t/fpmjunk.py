40553ad19ae340b4cb3cac6124e26c0b703d27e6
VMiklos <vmiklos@frugalware.org>
2007-04-06 23:08:40 +0000

    t/*: use libpacman

--- t/fpmjunk.py
+++ t/fpmjunk.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import alpm, os, tempfile, shutil, sys, re
+import pacman, os, tempfile, shutil, sys, re
 
 remove = False
 if len(sys.argv) > 1:
@@ -16,23 +16,23 @@ if len(sys.argv) > 1:
 for i in ['frugalware-%s' % arch]:
 	arch = i[11:]
 	root = tempfile.mkdtemp()
-	alpm.initialize(root)
+	pacman.initialize(root)
 	if os.getcwd().split('/')[-2] == "frugalware-current":
 		treename = "frugalware-current"
 	else:
 		treename = "frugalware"
-	db = alpm.db_register(treename)
-	alpm.db_setserver(db, "file://" + os.getcwd() + "/../frugalware-" + arch)
-	alpm.db_update(1, db)
+	db = pacman.db_register(treename)
+	pacman.db_setserver(db, "file://" + os.getcwd() + "/../frugalware-" + arch)
+	pacman.db_update(1, db)
 	fdb = []
-	j = alpm.db_getpkgcache(db)
+	j = pacman.db_getpkgcache(db)
 	while j:
-		pkg = alpm.void_to_PM_PKG(alpm.list_getdata(j))
-		pkgname = alpm.void_to_char(alpm.pkg_getinfo(pkg, alpm.PKG_NAME))
-		pkgver = alpm.void_to_char(alpm.pkg_getinfo(pkg, alpm.PKG_VERSION))
+		pkg = pacman.void_to_PM_PKG(pacman.list_getdata(j))
+		pkgname = pacman.void_to_char(pacman.pkg_getinfo(pkg, pacman.PKG_NAME))
+		pkgver = pacman.void_to_char(pacman.pkg_getinfo(pkg, pacman.PKG_VERSION))
 		fdb.append("%s-%s-%s.fpm" % (pkgname, pkgver, arch))
-		j = alpm.list_next(j)
-	alpm.release()
+		j = pacman.list_next(j)
+	pacman.release()
 	shutil.rmtree(root)
 	for j in os.listdir(os.getcwd() + "/../frugalware-" + arch):
 		if j not in fdb and j != treename + ".fdb":

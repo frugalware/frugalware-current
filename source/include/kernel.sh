b9de7420952753c0dd0eefe565c66b17eba4d859
VMiklos <vmiklos@frugalware.org>
2007-04-26 17:32:58 +0000

    include/kernel.sh: add alsa-firmware to rodepends for _F_kernel_ver >= 2.6.21

--- source/include/kernel.sh
+++ source/include/kernel.sh
@@ -143,6 +143,9 @@ fi
 ###
 url="http://www.kernel.org"
 rodepends=('module-init-tools' 'sed')
+if [ "`vercmp 2.6.21 $_F_kernel_ver`" -le 0 ]; then
+	rodepends=(${rodepends[@]} 'alsa-firmware')
+fi
 if [ -z "$_F_kernel_name" ]; then
 	makedepends=('unifdef')
 fi

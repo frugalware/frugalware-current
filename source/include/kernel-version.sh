93dfcbcf2ef37eefdda8e3bc5551acae6fae2f4a
VMiklos <vmiklos@frugalware.org>
2007-06-20 12:13:29 +0000

    kernel-2.6.21-3-x86_64 bump to 2.6.21.5 closes #2160 also rebuild external modules

--- source/include/kernel-version.sh
+++ source/include/kernel-version.sh
@@ -16,5 +16,5 @@
 # * _F_kernelver_stable: the number of the -stable patch to use (if any)
 ###
 _F_kernelver_ver=2.6.21
-_F_kernelver_rel=2
-_F_kernelver_stable=3
+_F_kernelver_rel=3
+_F_kernelver_stable=5

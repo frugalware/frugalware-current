9c25fe3d71051011885b2dacfa619e877ad1c062
VMiklos <vmiklos@frugalware.org>
2007-09-08 19:36:35 +0200

    include/xorg.sh: 7.2 -> 7.3

--- source/include/xorg.sh
+++ source/include/xorg.sh
@@ -21,7 +21,6 @@
 # archs=('i686' 'x86_64')
 # depends=('xorg-server')
 # makedepends=('xproto' 'randrproto' 'renderproto')
-# _F_xorg_nr=1
 # Finclude xorg
 # sha1sums=('349572d92ca6cd2c8f370105c4bffc70b7034bf3')
 # --------------------------------------------------
@@ -30,9 +29,6 @@
 # * _F_xorg_name (defaults to $pkgname): if you want to use a custom package
 # name (for example the upstream name contains uppercase letters) then use this
 # to declare the real name
-# * _F_xorg_nr: X, where the xorg version of the package is 7.X (choose the
-# largest possible, sometimes packages are not available in the latest
-# directory)
 ###
 if [ -z "$_F_xorg_name" ]; then
 	_F_xorg_name=$pkgname
@@ -63,26 +59,14 @@ if [[ $pkgname =~ ^xf86-input- ]]; then
 	makedepends=('inputproto' 'randrproto')
 fi
 url="http://xorg.freedesktop.org"
-if [ -n "$_F_xorg_nr" ]; then
-	_F_xorg_release_dir="X11R7.2/src"
-else
-	_F_xorg_release_dir="individual"
-fi
+_F_xorg_release_dir="X11R7.3/src"
 _F_xorg_dir=`echo ${groups[$((${#groups[@]}-1))]}|sed 's/xorg-\(.*\)/\1/;s/s$//'`
 _F_xorg_version="X11R7."
 [ "$_F_xorg_name" = "xorg-server" ] && _F_xorg_dir="xserver"
 _F_xorg_url="$url/releases/$_F_xorg_release_dir/$_F_xorg_dir/"
 license="GPL2"
-if [ -n "$_F_xorg_nr" ]; then
-	up2date="lynx -dump $_F_xorg_url | grep '$_F_xorg_name-${_F_xorg_version}$_F_xorg_nr-\(.*\).tar.bz2'|sed -n 's/.*$_F_xorg_name-$_F_xorg_version$_F_xorg_nr-\(.*\)\.t.*/\1/;$ p'"
-	source=($_F_xorg_url/$_F_xorg_name-$_F_xorg_version$_F_xorg_nr-$pkgver.tar.bz2)
-	if [ -z "$_F_cd_path" ]; then
-		_F_cd_path="$_F_xorg_name-$_F_xorg_version$_F_xorg_nr-$pkgver"
-	fi
-else
-	up2date="lynx -dump $_F_xorg_url | grep '$_F_xorg_name-\(.*\).tar.bz2'|sed -n 's/.*$_F_xorg_name-\(.*\)\.t.*/\1/;$ p'"
-	source=($_F_xorg_url/$_F_xorg_name-$pkgver.tar.bz2)
-	if [ -z "$_F_cd_path" ]; then
-		_F_cd_path="$_F_xorg_name-$pkgver"
-	fi
+up2date="lynx -dump $_F_xorg_url | grep '$_F_xorg_name-\(.*\).tar.bz2'|sed -n 's/.*$_F_xorg_name-\(.*\)\.t.*/\1/;$ p'"
+source=($_F_xorg_url/$_F_xorg_name-$pkgver.tar.bz2)
+if [ -z "$_F_cd_path" ]; then
+	_F_cd_path="$_F_xorg_name-$pkgver"
 fi

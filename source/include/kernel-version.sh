66350cea6763ba9496d9341f91eb1a6cf7d945ec
VMiklos <vmiklos@frugalware.org>
2007-10-09 18:11:49 +0200

    kernel-2.6.22-6-i686 updates for -stable

--- source/include/kernel-version.sh
+++ source/include/kernel-version.sh
@@ -15,6 +15,7 @@
 # * _F_kernelver_rel: the kernel release
 # * _F_kernelver_stable: the number of the -stable patch to use (if any)
 ###
+# don't touch these!
 _F_kernelver_ver=2.6.22
 _F_kernelver_rel=6
 _F_kernelver_stable=9

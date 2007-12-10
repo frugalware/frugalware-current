2c295d90156729ea10676df22699670703267501
Krisztian VASAS <iron@frugalware.org>
2007-12-10 14:48:12 +0100

    xfce4-4.4.2-noarch - changed dlurl: started building 4.4.2

--- source/include/xfce4.sh
+++ source/include/xfce4.sh
@@ -58,7 +58,7 @@ if echo ${groups[*]} | grep -q goodies ; then
 else
 	url="http://www.xfce.org/"
 	preup2date="lynx -dump http://www.xfce.org/archive/ | grep xfce- | tail -n1 | sed 's/.*-\(.*\)\/.*/\1/'"
-	dlurl="$url/archive/xfce-4.4.1/src"
+	dlurl="$url/archive/xfce-4.4.2/src"
 	up2date="lynx -dump $url/archive/xfce-\$($preup2date)/src/ | grep $_F_xfce_name | Flasttarbz2"
 	source=($dlurl/$_F_xfce_name-$pkgver.tar.bz2)
 fi

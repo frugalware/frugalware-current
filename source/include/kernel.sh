a316e2ef7f6d91573457675eab94043d28f1e918
Miklos Vajna <vmiklos@frugalware.org>
2008-02-12 10:44:59 +0100

    kernel.sh: use config.$arch only for >=2.6.24

--- source/include/kernel.sh
+++ source/include/kernel.sh
@@ -158,7 +158,11 @@ groups=('base')
 archs=('i686' 'x86_64')
 options=('nodocs' 'genscriptlet')
 up2date="lynx -dump $url/kdist/finger_banner |sed -n 's/.* \([0-9]*\.[0-9]*\.[0-9]*\).*/\1/;1 p'"
-source=(ftp://ftp.kernel.org/pub/linux/kernel/v2.6/linux-$_F_kernel_ver.tar.bz2 config.i686 config.x86_64)
+if [ "`vercmp 2.6.24 $_F_kernel_ver`" -le 0 ]; then
+	source=(ftp://ftp.kernel.org/pub/linux/kernel/v2.6/linux-$_F_kernel_ver.tar.bz2 config.i686 config.x86_64)
+else
+	source=(ftp://ftp.kernel.org/pub/linux/kernel/v2.6/linux-$_F_kernel_ver.tar.bz2 config)
+fi
 signatures=("${source[0]}.sign" '' '')
 install="src/kernel.install"
 

5b59ee7c57c8e3b614e66d9c8b21328a8a85b4c5
crazy <crazy@frugalware.org>
2008-02-17 15:27:18 +0100

    perl.sh * perl -> perl>=5.10.0-2

--- source/include/perl.sh
+++ source/include/perl.sh
@@ -64,7 +64,7 @@
 [ -z "$_F_cd_path" ] && _F_cd_path="$_F_perl_sourcename-$pkgver"
 pkgname="perl-`echo $_F_perl_name|tr [A-Z] [a-z]`"
 [ -z "$pkgrel" ] && pkgrel=1
-depends=(${depends[@]} 'perl')
+depends=(${depends[@]} 'perl>=5.10.0-2')
 makedepends=(${makedepends[@]} 'perl-libwww')
 groups=('devel-extra')
 archs=('i686')

frugalware/frugalware-current

commit 487fbc410356f702e1d40db6345ee179a2de3c69

sourceforge.sh * only use s/_/-/ in source when _F_sourceforge_pkgver = $pkgver * for custom _F_sourceforge_pkgver that is not needed since $pkgver is right already * and the custom _F_sourceforge_pkgver may be 1_2_3 or alike. * reported by bouleetbil

source/include/sourceforge.sh
@@ -95,4 +95,10 @@ if [ $_F_sourceforge_broken_up2date -eq 0 ]; then
 else
  	up2date="lynx -dump http://sourceforge.net/project/showfiles.php?group_id=\$(lynx -dump $url|grep showfiles|sed 's/.*=\(.*\)/\1/;q')|grep -m1 '$_F_sourceforge_name\(.*\)$_F_sourceforge_ext'|sed 's/.*$_F_sourceforge_name$_F_sourceforge_prefix\(.*\)$_F_sourceforge_ext.*/\1/;s/-/_/g;s/_//1'"
 fi
-source=(http://${_F_sourceforge_mirror}.dl.sourceforge.net/sourceforge/${_F_sourceforge_dirname}/${_F_sourceforge_name}${_F_sourceforge_sep}${_F_sourceforge_pkgver//_/-}${_F_sourceforge_ext})
+## we have now regular $pkgver so use s/_/-/ but in case we have a custom _F_sourceforge_pkgver
+## which is for 'source only' we don't need that since $pkgver is right already
+if [ -z $_F_sourceforge_pkgver ]; then
+	source=(http://${_F_sourceforge_mirror}.dl.sourceforge.net/sourceforge/${_F_sourceforge_dirname}/${_F_sourceforge_name}${_F_sourceforge_sep}${_F_sourceforge_pkgver//_/-}${_F_sourceforge_ext})
+else
+	source=(http://${_F_sourceforge_mirror}.dl.sourceforge.net/sourceforge/${_F_sourceforge_dirname}/${_F_sourceforge_name}${_F_sourceforge_sep}${_F_sourceforge_pkgver}${_F_sourceforge_ext})
+fi

commit 43c2c520f222e3fa9df9263187bedf09fa2c657f

sourceforge.sh * I said I was to tired but still pushed that stuff =) * fix breakage introduced by 487fbc4 , -z check is buggy since always true * compare to $pkgver to see if we use a custom _F_sourceforge_pkgver or not

source/include/sourceforge.sh
@@ -97,7 +97,7 @@ else
 fi
 ## we have now regular $pkgver so use s/_/-/ but in case we have a custom _F_sourceforge_pkgver
 ## which is for 'source only' we don't need that since $pkgver is right already
-if [ -z $_F_sourceforge_pkgver ]; then
+if [ "$_F_sourceforge_pkgver" == "$pkgver" ]; then
 	source=(http://${_F_sourceforge_mirror}.dl.sourceforge.net/sourceforge/${_F_sourceforge_dirname}/${_F_sourceforge_name}${_F_sourceforge_sep}${_F_sourceforge_pkgver//_/-}${_F_sourceforge_ext})
 else
 	source=(http://${_F_sourceforge_mirror}.dl.sourceforge.net/sourceforge/${_F_sourceforge_dirname}/${_F_sourceforge_name}${_F_sourceforge_sep}${_F_sourceforge_pkgver}${_F_sourceforge_ext})
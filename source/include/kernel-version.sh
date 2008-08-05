e3d13a4b75ed7eea2596cc53a75d42925277c77a
Miklos Vajna <vmiklos@frugalware.org>
2008-08-06 00:51:38 +0200

    kernel-2.6.26-1-i686 - version bump

--- source/include/kernel-version.sh
+++ source/include/kernel-version.sh
@@ -15,6 +15,6 @@
 # * _F_kernelver_rel: the kernel release
 # * _F_kernelver_stable: the number of the -stable patch to use (if any)
 ###
-_F_kernelver_ver=2.6.25
-_F_kernelver_rel=7
-_F_kernelver_stable=9
+_F_kernelver_ver=2.6.26
+_F_kernelver_rel=1
+_F_kernelver_stable=2

ee41d9549fd7b8f12ee9154b274958b35306366f
Priyank <priyank@frugalware.org>
2008-11-16 01:02:26 +0530

    Fix wrong up2date when _F_googlecode_dirname is set

--- source/include/googlecode.sh
+++ source/include/googlecode.sh
@@ -66,5 +66,5 @@ fi
 # * source()
 ###
 url="http://code.google.com/p/$_F_googlecode_dirname"
-up2date="lynx -dump http://code.google.com/p/$_F_googlecode_dirname/downloads/list|grep -m1 '$_F_googlecode_name\(.*\)$_F_googlecode_ext'|sed 's/.*$_F_googlecode_name\(.*\)$_F_googlecode_ext.*/\1/;s/-/_/g;s/_//1'"
+up2date="lynx -dump http://code.google.com/p/$_F_googlecode_name/downloads/list|grep -m1 '$_F_googlecode_dirname\(.*\)$_F_googlecode_ext'|sed 's/.*$_F_googlecode_dirname\(.*\)$_F_googlecode_ext.*/\1/;s/-/_/g;s/_//1'"
 source=(http://${_F_googlecode_dirname}.googlecode.com/files/${_F_googlecode_name}${_F_googlecode_sep}${pkgver//_/-}${_F_googlecode_ext})

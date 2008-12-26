ecde86f6666fbffbafaa9eda3a2a6e7b16367ee7
crazy <crazy@frugalware.org>
2008-12-26 17:50:26 +0100

    amsn.sh * added up2date for all these plugins

--- source/include/amsn.sh
+++ source/include/amsn.sh
@@ -42,7 +42,10 @@ fi
 ###
 # == OVERWRITTEN VARIABLES
 # * groups
+# * up2date
 ###
+
+up2date="lynx -dump 'http://sourceforge.net/project/showfiles.php?group_id=54091&package_id=134844' | grep -om1 '$_F_amsn_name-\(.*\).zip' | sed 's/.*-\(.*\).zip.*/\1/'"
 groups=('xapps-extra' 'amsn-plugins')
 
 up2date="lynx -dump 'http://sourceforge.net/project/showfiles.php?group_id=54091&package_id=134844' | grep $_F_amsn_name | Flastarchive .zip"

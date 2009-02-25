29ef7478be855c148395c4e717395c9bcb7c2f90
crazy <crazy@frugalware.org>
2009-02-26 00:13:34 +0100

    kde.sh * Version bump to 3.5.10

--- source/include/kde.sh
+++ source/include/kde.sh
@@ -66,7 +66,7 @@ fi
 ## TODO: add mirror option
 if [ "$_F_kde_defaults" -eq 1 ]; then
 	url="http://www.kde.org"
-	_F_kde_ver=3.5.9
+	_F_kde_ver=3.5.10
 	pkgurl="ftp://ftp.solnet.ch/mirror/KDE/stable/$_F_kde_ver/src"
 	#up2date="lynx -dump http://www.kde.org/download/|grep '$_F_kde_name'|sed -n '1 p'|sed 's/.*-\([^ ]*\) .*/\1/'"
 	up2date=$_F_kde_ver

b236342bc089fc42af4f570a082292b3d00769b8
Priyank <priyank@frugalware.org>
2009-03-10 00:29:21 +0530

    xfce4.sh: Don't hardwire the version in dlurl * Update version to 4.6.0

--- source/include/xfce4.sh
+++ source/include/xfce4.sh
@@ -48,6 +48,12 @@ if [ -z $_F_archive_name ] ; then
 	_F_archive_name=$_F_xfce_name
 fi
 
+_F_xfce_ver="4.6.0"
+
+if [ -z $pkgver ]; then
+	pkgver=${_F_xfce_ver}
+fi
+
 ###
 # == OVERWRITTEN VARIABLES
 # * url
@@ -63,7 +69,7 @@ if echo ${groups[*]} | grep -q goodies ; then
 else
 	url="http://www.xfce.org/"
 	preup2date="lynx -dump http://www.xfce.org/archive/ | grep xfce- | tail -n1 | sed 's/.*-\(.*\)\/.*/\1/'"
-	dlurl="$url/archive/xfce-4.5.93/src"
+	dlurl="$url/archive/${_F_xfce_ver}/src"
 	up2date="lynx -dump $url/archive/xfce-\$($preup2date)/src/ | Flasttar"
 	source=($dlurl/$_F_xfce_name-$pkgver.tar.bz2)
 fi
@@ -73,3 +79,4 @@ fi
 # * scriptlet to options()
 ###
 options=(${options[@]} 'scriptlet')
+

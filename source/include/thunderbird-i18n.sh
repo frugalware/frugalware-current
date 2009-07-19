40140246e9ec2dabb0bbd12d04dcf0dd69730f2b
Michel Hermier <hermier@frugalware.org>
2009-07-19 23:33:35 +0200

    include/thunderbird-i18n.sh
    * Use eval instead of $(...) in up2date to try to please the mail check.
    * Use noversrc, i18n .xpi have no version.

--- source/include/thunderbird-i18n.sh
+++ source/include/thunderbird-i18n.sh
@@ -31,10 +31,10 @@
 # * groups()
 # * archs()
 ###
-up2date="$(_F_archive_name="thunderbird"; Flastarchive http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/latest/source '-source\.tar\.bz2')"
+up2date="eval \"_F_archive_name=thunderbird; Flastarchive http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/latest/source '-source\.tar\.bz2'\""
 url="http://www.mozilla.org/projects/l10n/mlp.html"
 source=(http://ftp.mozilla.org/pub/mozilla.org/thunderbird/releases/$pkgver/linux-i686/xpi/$_F_thunderbird_lang.xpi)
-options=('scriptlet' 'genscriptlet')
+options=('scriptlet' 'genscriptlet' 'noversrc')
 install=${Fsrcdir%/src}/thunderbird-i18n.install
 rodepends=("thunderbird>=$pkgver")
 makedepends=('unzip')

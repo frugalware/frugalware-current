frugalware/frugalware-current

commit 9bef1c0eab7fa3524dcabf2165b4f20f94bb80eb

kernel.sh *moved some subdepends() to rodepends() *now makepkg install the subdepends() *we can't build new kernel without

source/include/kernel.sh
@@ -184,7 +184,8 @@ fi
 # * subdescs()
 ###
 subpkgs=("kernel$_F_kernel_name-source" "kernel$_F_kernel_name-docs")
-subdepends=("make gcc kernel-headers kernel$_F_kernel_name-docs" "kernel$_F_kernel_name")
+subdepends=("make gcc kernel-headers" "")
+subrodepends=("kernel$_F_kernel_name-docs" "kernel$_F_kernel_name")
 subarchs=('i686 x86_64 ppc' 'i686 x86_64 ppc')
 subinstall=('src/kernel-source.install' '')
 suboptions=('nodocs' '')
@@ -235,6 +236,7 @@ Fbuildkernel()
 	done
 	# remove unneded localversions
 	rm -f localversion-*
+	rm -f ../*.{gz,bz2,sign}
 	make silentoldconfig || Fdie
 
 	if [ $_F_kernel_dontfakeversion -eq 0 ]; then

commit 6fd4e6b7a97c8694b153004024893e6322b01bce

kernel.sh: fix typo introduced by 9bef1c0

source/include/kernel.sh
@@ -192,6 +192,7 @@ suboptions=('nodocs' '')
 if [ -z "$_F_kernel_name" ]; then
 	subpkgs=("${subpkgs[@]}" 'kernel-headers')
 	subdepends=("${subdepends[@]}" '')
+	subrodepends=("${subrodepends[@]}" '')
 	subgroups=('devel' 'apps' 'devel devel-core')
 	subdescs=('Linux kernel source' 'Linux kernel documentation' 'Linux kernel include files')
 	subarchs=("${subarchs[@]}" 'i686 x86_64 ppc')
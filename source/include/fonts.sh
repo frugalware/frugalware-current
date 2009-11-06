64ed67d51936da4f211c2aa8eae97c3c5a7e3c9e
James Buren <ryuo@frugalware.org>
2009-11-06 03:12:25 -0600

    fonts.sh * initial draft

---
+++ source/include/fonts.sh
@@ -0,0 +1,80 @@
+#!/bin/sh
+
+###
+# = fonts.sh(3)
+# James Buren <ryuo@frugalware.org>
+#
+# == NAME
+# fonts.sh - for Frugalware
+#
+# == SYNOPSIS
+# Common schema for font packages.
+#
+# == EXAMPLE
+# --------------------------------------------------
+# pkgname=artwiz-fonts
+# pkgver=1.3
+# pkgrel=3
+# pkgdesc="A set of fonts known as artwiz."
+# _F_sourceforge_name=artwiz-aleczapka-en
+# _F_sourceforge_dirname=artwizaleczapka
+# _F_sourceforge_ext=.tar.bz2
+# _F_sourceforge_realname=iso-8859-1
+# _F_fonts_dir="/usr/lib/X11/fonts/TTF"
+# Finclude sourceforge fonts
+# groups=('xlib-extra')
+# archs=('i686' 'x86_64')
+# sha1sums=('81e711b5f00816c57e205c9e60f69237c709679d')
+# --------------------------------------------------
+#
+# == OPTIONS
+# * _F_fonts_dir (required): specifies the directory to install fonts to
+###
+if [ -z "$_F_fonts_dir" ]; then
+	error '$_F_fonts_dir is not defined.'
+  Fdie
+fi
+
+###
+# == OVERWRITTEN VARIABLES
+# * install
+###
+install="src/fonts.install"
+
+###
+# == APPENDED VARIABLES
+# * rodepends
+# * options
+###
+rodepends=(${rodepends[@]} 'mkfontscale' 'mkfontdir' 'fontconfig')
+options=(${options[@]} 'genscriptlet')
+
+###
+# == PROVIDED FUNCTIONS
+# * Fbuild_fonts
+###
+Fbuild_fonts() {
+
+  # find and install all font extensions we support
+  for i in `find -iregex '.*\.\(ttf\|otf\|pcf\|pcf.gz\)'`; do
+    Ffile "$i" "$_F_fonts_dir/`basename $i`"
+  done
+
+  # find any uncompressed PCF fonts and compress them
+  for i in `find "$Fdestdir" -name "*.pcf"`; do
+    Fmessage "Compressing PCF font: $i"
+    gzip -9 "$i" || Fdie
+  done
+
+  # generate the install script
+  cp "$Fincdir/fonts.install" "$Fsrcdir" || Fdie
+  Fsed '$_F_fonts_dir' "$_F_fonts_dir" "$Fsrcdir/fonts.install"
+
+}
+
+###
+# * build() just calls Fbuild_fonts
+###
+build() {
+	Fbuild_fonts
+}

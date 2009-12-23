frugalware/frugalware-current

commit c196841144a219dfd3324de7a1330dfd5b9acecb

include/mozilla-i18n.sh: add all archs to subarchs, not just the first one

source/include/mozilla-i18n.sh
@@ -68,7 +68,7 @@ mozilla_i18n_lang_add() {
 	subdescs=("${subdescs[@]}" "${1} language support for ${_F_mozilla_i18n_name^}") # Requires a locale to name function.
 	subrodepends=("${subrodepends[@]}" "$_F_mozilla_i18n_name>=$pkgver")
 	subgroups=("${subgroups[@]}" "${groups[*]}")
-	subarchs=("${subarchs[@]}" "$archs")
+	subarchs=("${subarchs[@]}" "${archs[@]}")
 	sha1sums=("${sha1sums[@]}" "$2")
 }
 

commit 907998a31e3af0004f62ccd7ba6dc97f3f49582f

include/mozilla-i18n.sh: fix typo introduced in c196841

source/include/mozilla-i18n.sh
@@ -68,7 +68,7 @@ mozilla_i18n_lang_add() {
 	subdescs=("${subdescs[@]}" "${1} language support for ${_F_mozilla_i18n_name^}") # Requires a locale to name function.
 	subrodepends=("${subrodepends[@]}" "$_F_mozilla_i18n_name>=$pkgver")
 	subgroups=("${subgroups[@]}" "${groups[*]}")
-	subarchs=("${subarchs[@]}" "${archs[@]}")
+	subarchs=("${subarchs[@]}" "${archs[*]}")
 	sha1sums=("${sha1sums[@]}" "$2")
 }
 
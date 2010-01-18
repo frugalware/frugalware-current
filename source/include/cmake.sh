9ca937faef873519f4b28bf2daf312a8d31082ea
Michel Hermier <hermier@frugalware.org>
2010-01-18 22:14:04 +0100

    include/cmake.sh
    * Automagically add nostrip option if _F_cmake_type matches Debug*.

--- source/include/cmake.sh
+++ source/include/cmake.sh
@@ -36,8 +36,14 @@ fi
 ###
 # == APPENDED VARIABLES
 # * makedepends(): add cmake and pkgconfig
+# * options(): add nostrip if _F_cmake_type is Debug*
 ###
 makedepends=(${makedepends[@]} 'cmake' 'pkgconfig')
+case "$_F_cmake_type" in
+Debug*)
+	options=("${options[@]}" 'nostrip')
+	;;
+esac
 
 ###
 # == PROVIDED FUNCTIONS

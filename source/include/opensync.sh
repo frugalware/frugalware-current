2e4ac15975774c17bb697a14a9c730a58169ccbb
crazy <crazy@frugalware.org>
2010-02-14 15:03:51 +0100

    opensync.sh * move to cmake

--- source/include/opensync.sh
+++ source/include/opensync.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+Finclude cmake
 ###
 # = opensync.sh(3)
 # Gabriel Craciunescu <crazy@frugalware.org>
@@ -33,9 +34,7 @@
 ###
 
 url="http://www.opensync.org"
-# Note: there is no way to really get the up2date of the _next_ stable release
-# it will be 0.4x but this has to be monitored by the maintainers of these packages
-up2date="Flasttar http://opensync.org/wiki/releases/0.39/download"
+up2date="Flasttar http://opensync.org/download/releases/0.39/"
 source=($url/download/releases/$pkgver/$pkgname-$pkgver.tar.bz2)
 
 ###
@@ -52,13 +51,7 @@ options=(${options[@]} 'scriptlet')
 
 Fbuild_opensync()
 {
-	Fcd
-	Fpatchall
-        Fsed '-Werror' '' src/Makefile.am
-	Fsed 'libsoup-2.2' 'libsoup-2.4' configure.ac
-        Fautoreconf
-        Fmake
-	Fmakeinstall
+	CMake_build
 
 }
 

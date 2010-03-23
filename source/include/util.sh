7025eadf54c532cc729556daf89e68f4ff0b7922
Michel Hermier <hermier@frugalware.org>
2010-03-23 19:20:00 +0100

    util.sh
    * Add cosmetic " around some variable for potential filename with
      whitespace.

--- source/include/util.sh
+++ source/include/util.sh
@@ -543,7 +543,7 @@ Freplace() {
 Fdeststrip() {
 	local i
 	for i in "$@"; do
-		Fsed "$Fdestdir" "" $Fdestdir/$i
+		Fsed "$Fdestdir" "" "$Fdestdir/$i"
 	done
 }
 
@@ -654,7 +654,7 @@ Fpatch() {
 ###
 Fpatchall() {
 	local patch="" patcharch=""
-	for i in ${source[@]}; do
+	for i in "${source[@]}"; do
 		if [ -n "`echo "$i" | grep '\.patch[0-9]*$'`" -o -n "`echo "$i" | grep '\.diff$'`" -o -n "`echo "$i" | grep '\.\(patch[0-9]*\|diff\)\.\(gz\|bz2\)$'`" ]; then
 			patch=`strip_url "$i"`
 			patcharch=`echo $patch|sed 's/.*-\([^-]\+\)\.\(diff\|patch0\?\)$/\1/'`

acb409ce58acd811c3d06d26682ffa22ec759e48
Miklos Vajna <vmiklos@frugalware.org>
2010-06-27 14:15:57 +0200

    fuse-2.8.4-2-i686
    - drop fuse.modprobe.d, udev>=157 does this for us by default

--- source/apps-extra/fuse/fuse.modprobe.d
+++
@@ -1,4 +0,0 @@
-install fuse /sbin/modprobe --ignore-install fuse && \
-	{ if grep -q fusectl /proc/filesystems; then /bin/mount -t fusectl fusectl /sys/fs/fuse/connections; fi ; : ; }
-remove fuse { if grep -q " /sys/fs/fuse/connections " /proc/mounts; then /bin/umount /sys/fs/fuse/connections; fi } && \
-	/sbin/modprobe -r --ignore-remove fuse

d1397201629630abcd4f26e1face8d7c411dd759
bouleetbil <bouleetbil@frogdev.info>
2010-12-15 22:26:59 +0000

    lxde.sh *schema for lxde packages

---
+++ source/include/lxde.sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+
+###
+# = lxde.sh(3)
+# bouleetbil <bouleetbil@frogdev.info>
+#
+# == NAME
+# lxde.sh - for Frugalware
+#
+# == SYNOPSIS
+# Common schema for LXDE packages.
+#
+# == EXAMPLE
+# --------------------------------------------------
+# pkgname=menu-cache
+# pkgver=0.3.2
+pkgrel=1
+# pkgdesc="library creating and utilizing caches to speed up the manipulation for freedesktop.org defined application menus"
+# depends=('glib2')
+# makedepends=('intltool')
+# options=('scriptlet')
+# groups=('xlib-extra')
+# archs=('i686' 'x86_64' 'ppc')
+# _F_lxde_sep="%20"
+# Finclude lxde
+# sha1sums=('1c92ae19326a18ca9ce588704a5d8e746a8ec244')
+# --------------------------------------------------
+#
+# == OPTIONS
+# * _F_lxde_name (defaults to $pkgname): if you want to use a custom package
+# name (for example the upstream name contains uppercase letters) then use this
+# to declare the real name
+###
+
+if [ -z "$_F_lxde_name" ]; then
+	_F_lxde_name=$pkgname
+fi
+#for up2date
+if [ -z "$_F_lxde_sep" ]; then
+	_F_lxde_sep="-"
+fi
+
+###
+# == OVERWRITTEN VARIABLES
+# * up2date
+# * source()
+# * url
+###
+#for source
+_F_sourceforge_name=$_F_lxde_name
+_F_sourceforge_dirname="lxde"
+Finclude sourceforge
+url="http://lxde.org/"
+up2date="lynx -dump http://sourceforge.net/projects/lxde/files/$_F_lxde_name/ | grep 'http.*lxde/.*$_F_lxde_name/.*/$' |sed 's|.*/\(.*\)/|\1|;q' | sed 's|${_F_lxde_name}${_F_lxde_sep}||'"
+
+###
+# == APPENDED VARIABLES
+# * lxde-desktop to groups()
+###
+groups=(${groups[@]} 'lxde-desktop')

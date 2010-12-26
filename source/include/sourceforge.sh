0aeb04a7973678360c133af34505a480e88798ca
Miklos Vajna <vmiklos@frugalware.org>
2010-12-26 23:42:09 +0100

    include/sourceforge.sh: drop unneeded quote causing syntax errors

--- source/include/sourceforge.sh
+++ source/include/sourceforge.sh
@@ -94,7 +94,7 @@ fi
 _F_archive_name=$_F_sourceforge_name
 _F_archive_prefix=$_F_sourceforge_prefix
 Fpkgversep=$_F_sourceforge_sep
-up2date="lynx -dump '$_F_sourceforge_rss_url' | \
+up2date="lynx -dump $_F_sourceforge_rss_url | \
 	egrep '$_F_sourceforge_name$_F_sourceforge_sep.*$_F_sourceforge_ext' | \
 	sed -e 's|.*$_F_sourceforge_name$_F_sourceforge_sep||;s|$_F_sourceforge_ext.*||' | \
 	Fsort | tac | \

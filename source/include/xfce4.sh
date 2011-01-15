c7c5eef0c93cd07aaf73fb0e9236d0b691f40056
Devil505 <devil505linux@gmail.com>
2011-01-15 19:40:15 +0100

    * fixing up2date (special thx to Lukc)

--- source/include/xfce4.sh
+++ source/include/xfce4.sh
@@ -76,7 +76,7 @@ if echo ${groups[*]} | grep -q goodies ; then
 else
 	url="http://www.xfce.org/"
 	#preup2date="lynx -dump http://mocha.xfce.org/archive/ | grep xfce- | tail -n1 | sed 's/.*-\(.*\)\/.*/\1/'"
-	preup2date="lynx -dump 'http://mocha.xfce.org/archive/xfce/?C=N;O=A' | sed -ne 's/.*\/xfce\/\(.*\)\//\1/; $ p'"
+	preup2date="lynx -dump 'http://mocha.xfce.org/archive/xfce/?C=N;O=A' | grep 'archive\.xfce\.org' | sed -ne 's/.*\/xfce\/\(.*\)\//\1/; $ p'"
 	dlurl="http://mocha.xfce.org/archive/xfce/${_F_xfce_ver}/src"
 	up2date="lynx -dump http://mocha.xfce.org/archive/xfce/\$($preup2date)/src/ | Flasttar"
 	source=($dlurl/$_F_xfce_name-$pkgver.tar.bz2)

483ae4754cc85ce51fa928172441f592b978a5dd
Devil505 <devil505linux@gmail.com>
2011-02-08 10:25:42 +0100

    garcon-0.1.5-2-i686 * added script for xfce menu * fixing pkgdesc

---
+++ source/xfce4/garcon/garcon.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export XDG_CONFIG_DIRS=${XDG_CONFIG_DIRS}:/etc/xdg
+export XDG_DATA_HOME=${HOME}/.local/share
+export XDG_CONFIG_HOME=${HOME}/.config
+export XDG_DATA_DIRS=/usr/share
+export XDG_CACHE_HOME=${HOME}/.cache
+export GDK_USE_XFT=1
+
+if [ -f /etc/xdg/menus/xfce-applications.menu ] ; then
+	export XDG-MENU-PREFIX=xfce-
+fi

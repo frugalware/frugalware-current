33aa5c27f730e01bc1bc5dce3c56719832440da0
Devil505 <devil505linux@gmail.com>
2011-03-10 09:53:20 +0100

    man-db-2.5.9-1-i686 * new package (replacing man)

---
+++ source/base/man-db/man-colors.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# colored manpages in less
+
+export LESS_TERMCAP_mb=$'\E[01;32m'
+export LESS_TERMCAP_md=$'\E[01;32m'
+export LESS_TERMCAP_me=$'\E[0m'
+export LESS_TERMCAP_ue=$'\E[0m'
+export LESS_TERMCAP_us=$'\E[01;34m'

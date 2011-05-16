51236644653cf5874403c33e43421d924775a921
Miklos Vajna <vmiklos@frugalware.org>
2011-05-16 16:33:55 +0200

    tools/find-unmaintained-packages: fix initial dir

--- tools/find-unmaintained-packages.sh
+++ tools/find-unmaintained-packages.sh
@@ -3,7 +3,7 @@
 # the output of this script is published daily at
 # http://frugalware.org/~repo/stats/unmaintained.txt
 
-cd ../t
+cd `dirname $0`/../t
 activedevs=$(./chkacc |sed "s/.*'\(.*\)'/\1/g" |xargs echo|sed 's/ /|/g')
 cd ..
 

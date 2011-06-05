6b434436e29b2ffacec65bb215d9f6c582ca0b48
bouleetbil <bouleetbil@frogdev.info>
2011-06-05 04:15:06 +0200

    gnome-shell-extensions *new schema *all gnome-shell-extension should use this schema *more simple to update shell-version when gnome-shell will be bumped

---
+++ source/include/gnome-shell-extensions.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+###
+# = gnome-shell-extension.sh(3)
+# bouleetbil <bouleetbil@frogdev.info>
+#
+# == NAME
+# gnome-shell-extension.sh - for Frugalware
+#
+# == SYNOPSIS
+# Common schema for gnome-shell-extensions packages.
+
+###
+# == PROVIDED FUNCTIONS
+# * FGnomeShellVersion(): Change shell-version into metadata.json.
+###
+FGnomeShellVersion()
+{
+for i in `find $Fdestdir/usr/share/gnome-shell/extensions/ -name metadata.json`
+do
+	Fsed "shell-version\": \[" \
+		"shell-version\": \[ \"3.0\"," \
+		$i
+done
+}

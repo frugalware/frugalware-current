4433ab277ad96b690f8060ac18ca1e29dfdc5d8e
Michel Hermier <hermier@frugalware.org>
2011-07-24 19:00:25 +0200

    kernel.sh kernel-version.sh
    * Bump version to 3.0.
    * Addapt so it should build correctly with that version.
    * Simplify source/signatures assignations (remove duplicated entries
      with/without USE_DEVEL).

--- source/include/kernel-version.sh
+++ source/include/kernel-version.sh
@@ -15,9 +15,10 @@
 # * _F_kernelver_rel: the kernel release
 # * _F_kernelver_stable: the number of the -stable patch to use (if any)
 ###
-_F_kernelver_ver=2.6.39
-_F_kernelver_rel=2
-_F_kernelver_stable=3
+pkgver=3.0
+_F_kernelver_ver=3.0.0
+_F_kernelver_rel=1
+_F_kernelver_stable=0
 
 ###
 # == APPENDED VALUES

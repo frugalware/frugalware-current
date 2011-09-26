9d0b92567e91dd138b5aec59aadcbb19e6f96140
bouleetbil <bouleetbil@frogdev.info>
2011-09-26 21:43:06 +0200

    gnome-shell-extensions.sh *update to 3.2

--- source/include/gnome-shell-extensions.sh
+++ source/include/gnome-shell-extensions.sh
@@ -20,7 +20,7 @@ FGnomeShellVersion()
 for i in `find $Fdestdir/usr/share/gnome-shell/extensions/ -name metadata.json`
 do
 	Fsed "shell-version\": \[" \
-		"shell-version\": \[ \"3.1\"," \
+		"shell-version\": \[ \"3.2\"," \
 		$i
 done
 }

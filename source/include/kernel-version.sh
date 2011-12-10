ec3acfd51b78990921e625ea0ed8f963f9537cd4
James Buren <ryuo@frugalware.org>
2011-12-10 13:25:27 -0600

    kernel-3.1-4-x86_64 * bump to patch level 5

--- source/include/kernel-version.sh
+++ source/include/kernel-version.sh
@@ -16,8 +16,8 @@
 # * _F_kernelver_stable: the number of the -stable patch to use (if any)
 ###
 _F_kernelver_ver=3.1
-_F_kernelver_rel=3
-_F_kernelver_stable=4
+_F_kernelver_rel=4
+_F_kernelver_stable=5
 
 ###
 # == APPENDED VALUES

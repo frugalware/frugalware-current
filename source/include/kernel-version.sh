50d7d91b393ef20ef52220a03f054598471857a2
James Buren <ryuo@frugalware.org>
2012-07-22 22:51:35 -0500

    kernel-3.4-4-x86_64 * bump to patch level 6

--- source/include/kernel-version.sh
+++ source/include/kernel-version.sh
@@ -17,8 +17,8 @@
 # * _F_kernelver_nextver: the next kernel version
 ###
 _F_kernelver_ver=3.4
-_F_kernelver_rel=3
-_F_kernelver_stable=4
+_F_kernelver_rel=4
+_F_kernelver_stable=6
 _F_kernelver_nextver="3.$(( ${_F_kernelver_ver/3./} + 1 ))"
 
 ###

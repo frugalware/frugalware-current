6f75fd62695cb504eda745bd8194940072e21690
James Buren <ryuo@frugalware.org>
2012-12-11 11:53:30 -0600

    kernel-3.7-1-x86_64 * initial bump to 3.7 (no build yet)

--- source/include/kernel-version.sh
+++ source/include/kernel-version.sh
@@ -16,9 +16,9 @@
 # * _F_kernelver_stable: the number of the -stable patch to use (if any)
 # * _F_kernelver_nextver: the next kernel version
 ###
-_F_kernelver_ver=3.6
-_F_kernelver_rel=4
-_F_kernelver_stable=6
+_F_kernelver_ver=3.7
+_F_kernelver_rel=1
+_F_kernelver_stable=0
 _F_kernelver_nextver="3.$(( ${_F_kernelver_ver/3./} + 1 ))"
 
 ###

b0cca39ccf0049a3a2b567c98a3be4ed8582ca22
James Buren <ryuo@frugalware.org>
2014-01-31 14:58:22 -0600

    Revert "chromium-browser-32.0.1700.103-1-x86_64"
    This reverts commit 3c4dce14e6686ea325a40bd0f12c6952295aaf7e.

--- source/include/sourceforge.sh
+++ source/include/sourceforge.sh
@@ -113,5 +113,3 @@ up2date="lynx -dump $_F_sourceforge_rss_url | \
 
 source=("http://${_F_sourceforge_mirror}.sourceforge.net/${_F_sourceforge_dirname}/${_F_sourceforge_subdir}${_F_sourceforge_name}${_F_sourceforge_sep}${_F_sourceforge_pkgver}${_F_sourceforge_ext}")
 
-
-echo "$up2date"

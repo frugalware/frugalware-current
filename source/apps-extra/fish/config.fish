63ce9356188bc5a60d8c7644ae6b19e0cd309b01
DeX77 <dex77@frugalware.org>
2015-10-08 16:51:50 +0200

    fish-2.2.0-2-x86_64
    * fix colors

---
+++ source/apps-extra/fish/config.fish
@@ -0,0 +1,48 @@
+#
+# Init file for fish
+#
+
+#
+# Some things should only be done for login terminals
+#
+
+if status --is-login
+
+	#
+	# Set some value for LANG if nothing was set before, and this is a
+	# login shell.
+	#
+
+	if not set -q LANG >/dev/null
+		set -gx LANG en_US.UTF-8
+	end
+
+	# Check for i18n information in
+	# /etc/sysconfig/i18n
+
+	if test -f /etc/sysconfig/i18n
+		eval (cat /etc/sysconfig/i18n |sed -ne 's/^\([a-zA-Z]*\)=\(.*\)$/set -gx \1 \2;/p')
+	end
+
+	#
+	# Put linux consoles in unicode mode.
+	#
+
+	if test "$TERM" = linux
+		if expr "$LANG" : ".*\.[Uu][Tt][Ff].*" >/dev/null
+			if which unicode_start >/dev/null
+				unicode_start
+			end
+		end
+	end
+end
+
+
+## @Dex -- I know hackish but .. :)
+
+setenv LS_COLORS ':no=00:fi=00:di=01;34:ln=01;36:pi=40;33:so=01;35:bd=40;33;01:cd=40;33;01:or=40;31;01:ex=01;32:*.bat=01;32:*.BAT=01;32:*.btm=01;32:*.BTM=01;32:*.cmd=01;32:*.CMD=01;32:*.com=01;32:*.COM=01;32:*.dll=01;32:*.DLL=01;32:*.exe=01;32:*.EXE=01;32:*.arj=01;31:*.bz2=01;31:*.deb=01;31:*.gz=01;31:*.lzh=01;31:*.fpm=01;31:*.rpm=01;31:*.tar=01;31:*.taz=01;31:*.tb2=01;31:*.tbz2=01;31:*.tbz=01;31:*.tgz=01;31:*.tz2=01;31:*.z=01;31:*.Z=01;31:*.zip=01;31:*.ZIP=01;31:*.zoo=01;31:*.asf=01;35:*.ASF=01;35:*.avi=01;35:*.AVI=01;35:*.bmp=01;35:*.BMP=01;35:*.flac=01;35:*.FLAC=01;35:*.gif=01;35:*.GIF=01;35:*.jpg=01;35:*.JPG=01;35:*.jpeg=01;35:*.JPEG=01;35:*.m2a=01;35:*.M2a=01;35:*.m2v=01;35:*.M2V=01;35:*.mov=01;35:*.MOV=01;35:*.mp3=01;35:*.MP3=01;35:*.mpeg=01;35:*.MPEG=01;35:*.mpg=01;35:*.MPG=01;35:*.ogg=01;35:*.OGG=01;35:*.ppm=01;35:*.rm=01;35:*.RM=01;35:*.tga=01;35:*.TGA=01;35:*.tif=01;35:*.TIF=01;35:*.wav=01;35:*.WAV=01;35:*.wmv=01;35:*.WMV=01;35:*.xbm=01;35:*.xpm=01;35:'
+setenv LS_OPTIONS ' --color=auto -F -b -T 0'
+alias ls "/usr/bin/ls $LS_OPTIONS"
+alias dir "/usr/bin/ls $LS_OPTIONS --format=vertical"
+alias vdir "/usr/bin/ls $LS_OPTIONS --format=long"
+

5d6cc4de05bb4444fdd47dd73ab6e70a8031652f
crazy <crazy@frugalware.org>
2015-10-12 22:20:20 +0200

    pkgconfig-0.28-1-x86_64 * pkgrel++ * move to devel * remove X11R6 from $pgkname.sh

--- source/devel/pkgconfig/pkgconfig.sh
+++ source/devel/pkgconfig/pkgconfig.sh
@@ -10,8 +10,4 @@ if [ -d /usr/local/lib/pkgconfig ] ; then
 	PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/usr/local/lib/pkgconfig
 fi
 
-if [ -d /usr/X11R6/lib/pkgconfig ] ; then
-	PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/usr/X11R6/lib/pkgconfig
-fi
-
 export PKG_CONFIG_PATH

26888371dc6caa7978daa7e76a49e53e190d9331
James Buren <ryuo@frugalware.org>
2015-11-18 05:03:34 +0100

    urbanterror-4.1-2-x86_64 * remove package

--- source/games-extra/urbanterror/urbanterror.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-if [ ! -e "$HOME/.urbanterror" ]; then mkdir $HOME/.urbanterror; fi
-cd /usr/share/urbanterror/
-exec ./ioUrbanTerror +set cl_curllib /usr/lib/libcurl.so \
-     +set fs_basepath $HOME/.urbanterror +set fs_homepath $HOME/.urbanterror $*

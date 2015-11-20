eb58a0e23132c8f11a1f0f37cc3a6779bc43c2e6
James Buren <ryuo@frugalware.org>
2015-11-20 04:36:56 +0100

    lib32-0.2-1-x86_64 * add C program to wrap glibc dynamic loader (needed for DRI to work)

---
+++ source/lib32-extra/lib32/ld-wrapper.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <unistd.h>
+
+typedef struct {
+	char *key;
+	char *value;
+} table_pair_t;
+
+static void prepare_environment(void) {
+	static const table_pair_t table[] = {
+		{ "LIBGL_DRIVERS_PATH", "/usr/lib32/dri" },
+		{                 NULL,             NULL }
+	};
+	size_t i;
+
+	for( i = 0 ; table[i].key != NULL ; ++i ) {
+		setenv(table[i].key, table[i].value, 1);
+	}
+}
+
+static void launch_binary(char **argv) {
+	static const char ld[] = "/lib32/ld-linux.so.2";
+
+	execv(ld, argv);
+}
+
+int main(int argc, char **argv) {
+	(void) argc;
+
+	prepare_environment();
+
+	launch_binary(&argv[1]);
+
+	return 1;
+}

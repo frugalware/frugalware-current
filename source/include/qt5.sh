4bb205d8e09bbfd88c8208c210c32cfffbc7546b
crazy <crazy@frugalware.org>
2015-11-21 22:01:23 +0100

    qt5.sh - i -> local i

--- source/include/qt5.sh
+++ source/include/qt5.sh
@@ -35,6 +35,7 @@ build_qt5()
 	Fmake
 	make  INSTALL_ROOT=$Fdestdir install || Fdie
 	if [ -d "${Fdestdir}/usr/lib/qt5/bin" ]; then
+		local i
 		for i in ${Fdestdir}/usr/lib/qt5/bin/*; do
 			q5=`basename ${i}`
 			Fln /usr/lib/qt5/bin/${q5} /usr/bin/${q5}-qt5

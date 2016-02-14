a6e603eb57aa5eaa08a9ca1863b915ac26f21f7f
DeX77 <dex77@frugalware.org>
2016-02-14 14:32:24 +0100

    wine-1.8.1-1-x86_64
    * version bump: take maintainership

--- source/include/wine.sh
+++ source/include/wine.sh
@@ -19,20 +19,19 @@
 
 url="http://www.winehq.org"
 groups=('xapps-extra')
-depends=(
-	'libgl' 'gnutls' 'libtiff' 'libjpeg' 'libpng' 'libxinerama'
-	'libxrandr' 'libxcursor' 'libxcomposite' 'libmpg123' 'libglu'
-	'lcms2' 'fontconfig' 'openal' 'libxi'
-	)
+depends=('lcms2' 'openal' 'libglu' 'libldap' 'libpcap' 'libpulse' 'libmpg123' 'libgphoto2' 'gettext')
+makedepends=('x11-protos' 'cups')
 _F_cd_path="wine-$pkgver"
 options=('genscriptlet')
 archs=('i686' 'x86_64')
+
+Fconfopts+="	--libdir=/usr/lib"
 
 case "$pkgname" in
 
 wine)
 	pkgdesc="An Open Source implementation of the Windows API on top of X and Unix. (Stable)"
-	up2date="lynx -dump $url | sed -n 's|^.*Stable:.*Wine \([0-9a-zA-Z.]\+\).*\$|\1|p'"
+	up2date="Flasttar https://dl.winehq.org/wine/source/1.8/"
 	conflicts=('wine-devel')
 	;;
 
@@ -53,10 +52,7 @@ esac
 if [[ "$CARCH" == "x86_64" ]]
 then
 	Fconfopts="--enable-win64"
-	if [[ "$pkgname" == "wine" ]]
-	then
-		rodepends=('lib32-wine')
-	fi
+	rodepends=('lib32-wine')
 fi
 
 source=(http://ftp.winehq.org/pub/wine/source/${pkgver%.*}/wine-$pkgver.tar.bz2)
@@ -64,8 +60,9 @@ signatures=("${source[@]}.sign")
 
 build()
 {
-	# Disable optimizations to workaround GCC issues
-	CFLAGS="${CFLAGS//-O2/-O0}"
+	Fcd
+	Fsed 'lib64' 'lib' configure.ac
+	Fautoreconf
 	Fbuild
 	Fexec cp $Fincdir/wine.conf $Fsrcdir
 	Ffile /etc/binfmt.d/wine.conf

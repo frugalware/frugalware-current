d8a392f84610a993d6225ce128c04862ab4f8441
crazy <crazy@frugalware.org>
2016-08-02 22:16:27 +0200

    Version bump to 5.7.3

--- source/include/kf5-version.sh
+++ source/include/kf5-version.sh
@@ -20,6 +20,6 @@
 _F_kdever_frameworks=5.24
 _F_kdever_frameworks_revision=0
 _F_kf5_full="${_F_kdever_frameworks}.${_F_kdever_frameworks_revision}"
-_F_kdever_plasma=5.7.1
+_F_kdever_plasma=5.7.3
 _F_kdever_qt5=5.7.0
 _F_kdever_apps=16.04.3

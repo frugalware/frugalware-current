79965724af329314843c58129a8eff2a073b0a20
Marius Cirsta <mcirsta@frugalware.org>
2016-08-20 21:00:06 +0300

    chromium-dev-54.0.2832.2-1-x86_64
    * add devel version of Chromium, build with gn, with gtk3 instead of gtk2 and without gnome stuff

---
+++ source/xapps-extra/chromium-dev/chromium-dev.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+export CHROME_WRAPPER=/usr/lib/chromium-dev/chromium
+export CHROME_DESKTOP=chromium-dev.desktop
+
+exec /usr/lib/chromium-dev/chromium $CHROME_GLOBAL_FLAGS $CHROME_USER_FLAGS $@

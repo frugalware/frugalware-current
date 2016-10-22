b2beeea80d9f94aef4275c00b68a4eb60df9a0f0
crazy <crazy@frugalware.org>
2016-10-22 21:45:08 +0200

    we need ECM >= 5.27.0-2 now

--- source/include/kf5.sh
+++ source/include/kf5.sh
@@ -183,7 +183,7 @@ else
 fi
 
 if [ "$_F_kde_name" != 'extra-cmake-modules' ]; then
-	makedepends+=('extra-cmake-modules' 'qt5-tools')
+	makedepends+=('extra-cmake-modules>=5.27.0-2' 'qt5-tools')
 fi
 
 ## From gcc6 docs

f62aa738ac68ed6412564de5a633b710fa05dadf
DeX77 <dex77@frugalware.org>
2016-12-02 12:34:08 +0100

    * use github mirror for version check

--- source/include/kf5.sh
+++ source/include/kf5.sh
@@ -76,7 +76,6 @@ fi
 
 if [ -z "$_F_kde_mirror" ]; then
 	# set our preferred mirror
-	_F_kde_up2date_mirror="ftp://ftp5.gwdg.de/pub/linux/kde/"
 	#_F_kde_mirror="http://vesta.informatik.rwth-aachen.de/ftp/pub/mirror/kde/"
 	_F_kde_mirror="http://download.kde.org/"
 fi
@@ -147,7 +146,10 @@ fi
 
 if [ "$_F_kde_defaults" -eq 1 ]; then
 	if [ -z "$up2date" ]; then
-		up2date="Flastverdir http://download.kde.org/$_F_kde_folder"
+		_F_archive_grepv="\-rc"
+		_F_archive_name="archive"
+		Fpkgversep="/"
+		up2date="Flasttar https://github.com/KDE/${_F_kde_name}/releases/latest | sed 's/v//'"
 	fi
 
 	if [ ${#source[@]} -eq 0 ]; then

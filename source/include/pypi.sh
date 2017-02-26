bc1fb66800468cf5753dac783d00404328a1116c
crazy <crazy@frugalware.org>
2017-02-26 20:31:32 +0100

    pypi.sh : use _F_archive_name for source part

--- source/include/pypi.sh
+++ source/include/pypi.sh
@@ -39,5 +39,5 @@
 ###
 [ -z "$url" ] && url="http://pypi.python.org/pypi/$_F_pypi_name"
 up2date="Flastarchive https://pypi.python.org/pypi/${_F_pypi_name}/json $_F_pypi_ext"
-source=(https://files.pythonhosted.org/packages/source/${_F_pypi_name:0:1}/$_F_pypi_name/$_F_pypi_name-$pkgver$_F_pypi_ext)
+source=(https://files.pythonhosted.org/packages/source/${_F_pypi_name:0:1}/$_F_pypi_name/$_F_archive_name-$pkgver$_F_pypi_ext)
 

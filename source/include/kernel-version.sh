62b11e99b11e589fc75bbf258d302e039bc935a5
DeX77 <dex77@frugalware.org>
2017-07-12 19:51:24 +0200

    kernel-4.12.1-1-x86_64
    * kernel 4.12.1

--- source/include/kernel-version.sh
+++ source/include/kernel-version.sh
@@ -16,8 +16,8 @@
 # * _F_kernelver_stable: the number of the -stable patch to use (if any)
 # * _F_kernelver_nextver: the next kernel version
 ###
-_F_kernelver_ver=4.12
-_F_kernelver_rel=4
+_F_kernelver_ver=4.12.1
+_F_kernelver_rel=1
 
 ###
 # == APPENDED VALUES

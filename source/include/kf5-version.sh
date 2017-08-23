684e151893e0a68be9df7f10c24efeb2bdbbd59b
crazy <crazy@frugalware.org>
2017-08-23 13:24:19 +0200

    bump plasma version

--- source/include/kf5-version.sh
+++ source/include/kf5-version.sh
@@ -20,6 +20,6 @@
 _F_kdever_frameworks=5.37
 _F_kdever_frameworks_revision=0
 _F_kf5_full="${_F_kdever_frameworks}.${_F_kdever_frameworks_revision}"
-_F_kdever_plasma=5.10.4
+_F_kdever_plasma=5.10.5
 _F_kdever_qt5=5.9.1
 _F_kdever_apps=17.08.0

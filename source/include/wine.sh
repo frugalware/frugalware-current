7f2b1a5ecc7b0f75d3c6e757eaa93e5183b2d0a8
DeX77 <dex77@frugalware.org>
2018-02-15 13:36:59 +0100

    wine-devel-3.1-1-x86_64
    * version bump

--- source/include/wine.sh
+++ source/include/wine.sh
@@ -54,7 +54,7 @@ wine-devel)
 	conflicts=('wine' 'lib32-wine-devel')
 	provides=('wine' 'lib32-wine-devel')
 	replaces=('lib32-wine-devel')
-	source=(https://dl.winehq.org/wine/source/${pkgver}/wine-$pkgver.tar.xz \
+	source=(https://dl.winehq.org/wine/source/${pkgver%.*}.x/wine-$pkgver.tar.xz \
 		0001-programs-winhlp32-Use-noyywrap-for-macro.lex.l-and-p.patch )
 	;;
 

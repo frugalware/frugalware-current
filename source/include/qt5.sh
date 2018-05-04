d79184b8e542c4c77198ad884bdb17fc256b7d21
crazy <crazy@frugalware.org>
2018-05-04 13:08:33 +0200

    qt5.sh: lets make more use of it
    * change things to work more generic , also we need solve this problem too:
      qt5-base has forced $FLAGS in the configurations files .. $qmake-app
      always pull full flags in and our options=('some_flags_options') are not working.
    * _F_qt_nocore=yes is now used for !qt-xxxx core libs
      _F_qt_nobase_flags=yes forcea qtbase flags off and builds with our so options=() are working
    * added also helper functions: FQt_conf , FQt_make , FQt_install , FQt_symlink

--- source/include/qt5.sh
+++ source/include/qt5.sh
@@ -16,43 +16,109 @@
 # * build_qt5(): function to build qt5 source packages
 ###
 
-
-qtpkgname=${pkgname/5-/}
-qtpkgfilename=${qtpkgname}-everywhere-src-${pkgver}
-pkgdesc="The Qt5 toolkit, ${qtpkgname}"
-url="http://www.qt.io"
-if [ -z "$groups" ]; then
-	groups=('xlib')
-fi
-archs=('x86_64')
-options+=('nodocs')
-source=(http://download.qt.io/archive/qt/${pkgver%.*}/${pkgver}/submodules/${qtpkgfilename}.tar.xz)
-_F_archive_grepv="5.8"
-up2date="Flastverdir http://download.qt-project.org/official_releases/qt/\$(Flastverdir http://download.qt-project.org/official_releases/qt/)"
-_F_cd_path=${qtpkgfilename}
-makedepends+=('x11-protos' 'gperf')
-
-_qt_extra_cxx=" -Wno-deprecated -Wno-deprecated-declarations"
+## always use these
+_qt_extra_cxx+=" -Wno-deprecated -Wno-deprecated-declarations"
 
 if [ -n "$_F_qt_extra_cxx" ]; then
-	_qt_extra_cxx+=" ${_F_qt_extra_cxx[@]}"
+        _qt_extra_cxx+=" ${_F_qt_extra_cxx[@]}"
 fi
 
-build_qt5()
-{
-	Fcd
-	Fpatchall
-	qmake-qt5 QMAKE_CXXFLAGS+=" ${_qt_extra_cxx[@]}" || Fdie
+if [ -z "$_F_qt_nocore" ]; then
+	qtpkgname=${pkgname/5-/}
+	qtpkgfilename=${qtpkgname}-everywhere-src-${pkgver}
+	pkgdesc="The Qt5 toolkit, ${qtpkgname}"
+	url="http://www.qt.io"
+	if [ -z "$groups" ]; then
+		groups=('xlib')
+	fi
+	options+=('nodocs')
+	source=(http://download.qt.io/archive/qt/${pkgver%.*}/${pkgver}/submodules/${qtpkgfilename}.tar.xz)
+	_F_archive_grepv="5.8"
+	up2date="Flastverdir http://download.qt-project.org/official_releases/qt/\$(Flastverdir http://download.qt-project.org/official_releases/qt/)"
+	_F_cd_path=${qtpkgfilename}
+	makedepends+=('x11-protos' 'gperf')
+fi
+
+if [ -z "$archs" ]; then
+	archs=('x86_64')
+fi
+
+_qmake_conf() {
+
+	## always replace -isystem with -I..
+	## alyway drop in CXX/LD FLAGS
+	if [ -z "$_F_qt_nobase_flags" ]; then
+		## use whatever flags from qt-base configuration file
+		Fexec qmake-qt5 QMAKE_CXXFLAGS+=" ${_qt_extra_cxx[@]}"  QMAKE_LFLAGS+=" ${LDFLAGS}" QMAKE_CFLAGS_ISYSTEM=-I "$@"
+	else
+		## kill flags from qt-base and use = our ones so options=('flags_options')
+		## are working. That is for the case some app won't work with some CXX/LD flags
+		Fexec qmake-qt5 \
+			QMAKE_CXXFLAGS=" ${CXXFLAGS} ${_qt_extra_cxx[@]}"  \
+			QMAKE_LFLAGS=" ${LDFLAGS}" \
+			QMAKE_CFLAGS_ISYSTEM=-I \
+			QMAKE_CXXFLAGS_RELEASE="" \
+			QMAKE_CXXFLAGS_DEBUG="" "$@"
+	fi
+}
+
+_qmake_make() {
+
+	## there is reason for doing so
 	Fmake
-	make  INSTALL_ROOT=$Fdestdir install || Fdie
+}
+
+_qmake_install() {
+
+	Fexec make INSTALL_ROOT=$Fdestdir install || Fdie
+}
+
+_qt_symlink() {
+
+	local i
 	if [ -d "${Fdestdir}/usr/lib/qt5/bin" ]; then
-		local i
 		for i in ${Fdestdir}/usr/lib/qt5/bin/*; do
 			q5=`basename ${i}`
 			Fln /usr/lib/qt5/bin/${q5} /usr/bin/${q5}-qt5
 			Fln /usr/lib/qt5/bin/${q5} /usr/bin/${q5}
 		done
-        fi
+	fi
+}
+
+FQt_conf() {
+
+	_qmake_conf "$@" || Fdie
+}
+
+FQt_make() {
+
+	_qmake_make
+}
+
+FQt_install() {
+
+	_qmake_install
+}
+
+FQt_symlink() {
+
+	_qt_symlink
+}
+
+FQt_build()
+{
+	Fcd
+	Fpatchall
+	FQt_conf
+	FQt_make
+	FQt_install
+	FQt_symlink
+}
+
+## to be removed soon
+build_qt5() {
+
+	FQt_build
 }
 
 build()

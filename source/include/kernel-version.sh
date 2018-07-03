3f79113e3c55434e4c8219293b49bf5e1b3349a3
DeX77 <dex77@frugalware.org>
2018-07-03 15:02:10 +0200

    kernel-4.17.4-1-x86_64
    * kernel 4.17.4

--- source/include/kernel-version.sh
+++ source/include/kernel-version.sh
@@ -16,7 +16,7 @@
 # * _F_kernelver_stable: the number of the -stable patch to use (if any)
 # * _F_kernelver_nextver: the next kernel version
 ###
-_F_kernelver_ver=4.17.3
+_F_kernelver_ver=4.17.4
 _F_kernelver_rel=1
 
 ###

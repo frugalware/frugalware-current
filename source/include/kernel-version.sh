be02a070b1183767c9cf544fcc35f96c6a075735
DeX77 <dex77@frugalware.org>
2018-08-17 23:05:05 +0200

    kernel-4.18.2-1-x86_64
    * kernel 4.18.2

--- source/include/kernel-version.sh
+++ source/include/kernel-version.sh
@@ -16,7 +16,7 @@
 # * _F_kernelver_stable: the number of the -stable patch to use (if any)
 # * _F_kernelver_nextver: the next kernel version
 ###
-_F_kernelver_ver=4.18.1
+_F_kernelver_ver=4.18.2
 _F_kernelver_rel=1
 
 ###

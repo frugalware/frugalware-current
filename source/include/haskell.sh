c5e1539854544fda05a37d9ecbd1a17319728ceb
DeX77 <dex77@frugalware.org>
2018-10-11 16:10:19 +0200

    * raise ghc version in scheme

--- source/include/haskell.sh
+++ source/include/haskell.sh
@@ -82,7 +82,7 @@ install=$_F_haskell_install
 # * makedepends: ghc to makedepends=()
 ###
 options+=('scriptlet' 'genscriptlet')
-makedepends+=('ghc>=8.4.3' 'ghc-docs>=8.4.3')
+makedepends+=('ghc>=8.6.1' 'ghc-docs>=8.6.1')
 
 ###
 # == PROVIDED FUNCTIONS

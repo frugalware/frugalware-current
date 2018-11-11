65245344a8280ff4abcece0682f000c54ca05bf4
DeX77 <dex77@frugalware.org>
2018-11-11 19:23:19 +0100

    * kde apps 18.08.3

--- source/include/kf5-version.sh
+++ source/include/kf5-version.sh
@@ -22,4 +22,4 @@ _F_kdever_frameworks_revision=0
 _F_kf5_full="${_F_kdever_frameworks}.${_F_kdever_frameworks_revision}"
 _F_kdever_plasma=5.14.3
 _F_kdever_qt5=5.11.2
-_F_kdever_apps=18.08.2
+_F_kdever_apps=18.08.3

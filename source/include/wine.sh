950975b1ef8004016299599a998c0d6c9239d651
DeX77 <dex77@frugalware.org>
2019-01-27 12:49:07 +0100

    wine-4.0-1-x86_64
    * version bump

--- source/include/wine.sh
+++ source/include/wine.sh
@@ -20,10 +20,10 @@
 url="http://www.winehq.org"
 groups=('xapps-extra')
 depends=('lcms2' 'openal' 'libglu' 'libldap' 'libpcap' 'libpulse' 'libmpg123' 'libgphoto2' 'gettext' 'libxml2' \
-	'libxcursor' 'libxi' 'libxrandr' 'libxinerama' 'libxcomposite' 'sane-backends' 'v4l-utils' 'libxrender' 'libxslt')
+	'libxcursor' 'libxi' 'libxrandr' 'libxinerama' 'libxcomposite' 'sane-backends' 'v4l-utils' 'libxrender' 'libxslt' 'vkd3d')
 #32 bit
 depends+=('lib32-lcms2' 'lib32-libxcursor' 'lib32-libxi' 'lib32-libxrandr' 'lib32-libxinerama' 'lib32-libxcomposite' \
-	'lib32-libxrender' 'lib32-freetype2' 'lib32-libxml2' 'lib32-ncurses')
+	'lib32-libxrender' 'lib32-freetype2' 'lib32-libxml2' 'lib32-ncurses' 'lib32-vkd3d')
 makedepends=('x11-protos' 'cups')
 _F_cd_path="wine-$pkgver"
 options=('genscriptlet' 'nostrip' 'static')
@@ -43,7 +43,7 @@ wine)
 	conflicts=('wine-devel' 'lib32-wine-devel')
 	provides=('lib32-wine')
 	replaces=('lib32-wine')
-	source=(https://dl.winehq.org/wine/source/${pkgver%.*}/wine-$pkgver.tar.xz \
+	source=(https://dl.winehq.org/wine/source/${pkgver}/wine-$pkgver.tar.xz \
 		0001-programs-winhlp32-Use-noyywrap-for-macro.lex.l-and-p.patch )
 	;;
 
@@ -55,7 +55,7 @@ wine-devel)
 	provides=('wine' 'lib32-wine-devel')
 	replaces=('lib32-wine-devel')
 	depends+=('vulkan-icd-loader' 'lib32-vulkan-icd-loader')
-	source=(https://dl.winehq.org/wine/source/${pkgver%.*}.x/wine-$pkgver.tar.xz \
+	source=(https://dl.winehq.org/wine/source/${pkgver}.x/wine-$pkgver.tar.xz \
 		0001-programs-winhlp32-Use-noyywrap-for-macro.lex.l-and-p.patch )
 	;;
 

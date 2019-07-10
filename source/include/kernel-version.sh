655b8f39494b70b9bdea122ced45bf66bad3b7e1
DeX77 <dex77@frugalware.org>
2019-07-10 14:58:11 +0200

    kernel-5.1.17-1-x86_64
    * kernel 5.1.17

--- source/include/kernel-version.sh
+++ source/include/kernel-version.sh
@@ -16,7 +16,7 @@
 # * _F_kernelver_stable: the number of the -stable patch to use (if any)
 # * _F_kernelver_nextver: the next kernel version
 ###
-_F_kernelver_ver=5.1.16
+_F_kernelver_ver=5.1.17
 _F_kernelver_rel=1
 
 ###

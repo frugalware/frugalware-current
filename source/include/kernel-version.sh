b86bd1ae32bf1b786335ddde4f790820d415be45
dex77 <dex77@frugalware.org>
2019-12-05 10:08:19 +0100

    kernel-5.4.2-1-x86_64
    * kernel 5.4.2

--- source/include/kernel-version.sh
+++ source/include/kernel-version.sh
@@ -16,8 +16,8 @@
 # * _F_kernelver_stable: the number of the -stable patch to use (if any)
 # * _F_kernelver_nextver: the next kernel version
 ###
-_F_kernelver_ver=5.4.1
-_F_kernelver_rel=2
+_F_kernelver_ver=5.4.2
+_F_kernelver_rel=1
 
 ###
 # == APPENDED VALUES

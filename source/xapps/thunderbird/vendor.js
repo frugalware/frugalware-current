945125ad19f92ca3fb5270e599255a9b5f011af3
dex77 <dex77@frugalware.org>
2019-12-18 23:41:32 +0100

    thunderbird-68.3.1-2-x86_64
    * fix language packs

---
+++ source/xapps/thunderbird/vendor.js
@@ -0,0 +1,13 @@
+// Use LANG environment variable to choose locale
+pref("intl.locale.requested", "");
+
+// Use system-provided dictionaries
+pref("spellchecker.dictionary_path", "/usr/share/myspell/dicts");
+
+// Disable default mailer checking.
+pref("mail.shell.checkDefaultMail", false);
+
+// Don't disable our bundled extensions in the application directory
+pref("extensions.autoDisableScopes", 11);
+pref("extensions.shownSelectionUI", true)
+

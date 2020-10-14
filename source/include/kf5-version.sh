c17d133222df612730d38a89d68fe684c57b94a3
DeX77 <dex77@frugalware.org>
2020-10-14 11:11:46 +0200

    * plasma 5.20

--- source/include/kf5-version.sh
+++ source/include/kf5-version.sh
@@ -20,6 +20,6 @@
 _F_kdever_frameworks=5.75
 _F_kdever_frameworks_revision=0
 _F_kf5_full="${_F_kdever_frameworks}.${_F_kdever_frameworks_revision}"
-_F_kdever_plasma=5.19.5
+_F_kdever_plasma=5.20.0
 _F_kdever_qt5=5.15.1
 _F_kdever_apps=20.08.1

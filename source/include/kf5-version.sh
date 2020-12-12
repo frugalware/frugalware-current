dde9240b207632a4b8b88dc785e8a191d37ac5ac
DeX77 <dex77@frugalware.org>
2020-12-12 17:38:48 +0100

    * kf5 5.77

--- source/include/kf5-version.sh
+++ source/include/kf5-version.sh
@@ -17,7 +17,7 @@
 ###
 
 
-_F_kdever_frameworks=5.76
+_F_kdever_frameworks=5.77
 _F_kdever_frameworks_revision=0
 _F_kf5_full="${_F_kdever_frameworks}.${_F_kdever_frameworks_revision}"
 _F_kdever_plasma=5.20.3

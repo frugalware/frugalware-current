f27a7652ef585a3a426b860f6e336ea23d57a77f
dex77 <dex77@frugalware.org>
2020-12-23 10:50:36 +0100

    ghc-8.10.3-1-x86_64
    * version bump

--- source/include/haskell.sh
+++ source/include/haskell.sh
@@ -82,7 +82,7 @@ install=$_F_haskell_install
 # * makedepends: ghc to makedepends=()
 ###
 options+=('scriptlet' 'genscriptlet')
-makedepends+=('ghc>=8.10.2' 'ghc-docs>=8.10.2')
+makedepends+=('ghc>=8.10.3' 'ghc-docs>=8.10.3')
 
 ###
 # == PROVIDED FUNCTIONS

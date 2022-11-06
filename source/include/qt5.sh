30c300a7b5e7d02c3781e8334a96298eed6b1dda
DeX77 <dex77@frugalware.org>
2022-11-06 15:15:06 +0100

    * qt5 5.15.7

--- source/include/qt5.sh
+++ source/include/qt5.sh
@@ -52,11 +52,11 @@ fi
 if [[ "$pkgname" =~ "qt5-base" ]]; then
 	makedepends+=('x11-protos' 'gperf')
 else
-	makedepends+=('x11-protos' 'gperf' "qt5-base-static>=5.15.6")
+	makedepends+=('x11-protos' 'gperf' "qt5-base-static>=5.15.7")
 fi
 
 if [[ ! "$pkgname" =~ "qt5-base" ]] || [[ ! "$pkgname" =~ "qt5-declarative" ]]; then
-	makedepends+=('qt5-declarative-static>=5.15.6')
+	makedepends+=('qt5-declarative-static>=5.15.7')
 fi
 
 if [ -z "$archs" ]; then

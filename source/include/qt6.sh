8f45bb726edbb83d190fb8e70b84acd0d0f3e2cc
dex77 <dex77@frugalware.org>
2022-11-24 14:07:54 +0100

    qt6-base-6.4.1-1-x86_64
    * qt6

---
+++ source/include/qt6.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+###
+# = qt6.sh(3)
+# DeX77 <dex77@frugalware.org>
+#
+# == NAME
+# qt6.sh - for Frugalware
+#
+# == SYNOPSIS
+# Common schema for qt6 packages
+#
+# == OPTIONS
+# * _F_qt_nocore: when set to true it indicates is not a core
+#   qt6 package.
+###
+
+###
+# == PROVIDED FUNCTIONS
+# * build_qt6(): function to build qt6 source packages
+###
+
+if [ -z "$_F_qt_nocore" ]; then
+	qtpkgname=${pkgname/6-/}
+	qtpkgfilename=${qtpkgname}-everywhere-src-${pkgver}
+	pkgdesc="The Qt6 toolkit, ${qtpkgname}"
+	url="http://www.qt.io"
+	if [ -z "$groups" ]; then
+		groups=('xlib-extra')
+	fi
+	options+=('nodocs')
+	source=(http://download.qt.io/archive/qt/${pkgver%.*}/${pkgver}/submodules/${qtpkgfilename}.tar.xz)
+	up2date="Flastverdir http://download.qt-project.org/official_releases/qt/\$(Flastverdir http://download.qt-project.org/official_releases/qt/)"
+	_F_cd_path=${qtpkgname}-everywhere-src-${pkgver}
+fi
+
+if [[ "$pkgname" =~ "qt6-base" ]]; then
+	makedepends+=('x11-protos' 'gperf')
+else
+	makedepends+=('x11-protos' 'gperf' "qt6-base-static")
+fi
+
+if [[ ! "$pkgname" =~ "qt6-base" ]] && [[ ! "$pkgname" =~ "qt6-declarative" ]]; then
+	makedepends+=('qt6-declarative-static')
+fi
+
+if [ -z "$archs" ]; then
+	archs=('x86_64')
+fi
+
+_F_cmake_use_ninja=yes
+Finclude cmake

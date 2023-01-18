32fe34bc20248b8e2f1482a28504d4ab350de4c6
dex77 <dex77@frugalware.org>
2023-01-18 21:48:02 +0100

    openjdk-19.0.2-1-x86_64
    * version bump

--- source/devel-extra/openjdk/openjre.sh
+++ source/devel-extra/openjdk/openjre.sh
@@ -1,4 +1,4 @@
-export OPENJREDIR=/usr/lib/jvm/java-18-openjdk/
+export OPENJREDIR=/usr/lib/jvm/java-19-openjdk/
 export MANPATH=$MANPATH:$OPENJREDIR/man
 export CLASSPATH=$CLASSPATH:$OPENJREDIR/lib
 

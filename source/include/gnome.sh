d402ff83c157f5a9c38edd5f23d1c82774ce2d3d
dex77 <dex77@frugalware.org>
2023-03-13 11:13:59 +0100

    * update gnome.sh

--- source/include/gnome.sh
+++ source/include/gnome.sh
@@ -51,28 +51,6 @@ if [ -z "$_F_gnome_ext" ]; then
 	_F_gnome_ext=.tar.bz2
 fi
 
-# don't document this function, it's used only for source()
-_F_gnome_getver()
-{
-	local tmpver count
-
-	tmpver=`echo $pkgver | tr -d '[0-9][a-z]'`
-	count=`expr length "$tmpver"`
-
-	case $count in
-	    1)
-	    tmpver="$pkgver"
-	    ;;
-	    2)
-	    tmpver="${pkgver%.*}"
-	    ;;
-	    3)
-	    tmpver="${pkgver%.*.*}"
-	    ;;
-	esac
-	echo $tmpver
-}
-
 ###
 # == OVERWRITTEN VARIABLES
 # * _F_gnome_pygtkdefsdir
@@ -84,25 +62,15 @@ _F_gnome_getver()
 _F_gnome_pygtkdefsdir="usr/share/pygtk/2.0/defs"
 #_F_gnome_pkgurl="https://download.gnome.org/sources"
 _F_gnome_pkgurl="https://ftp.gnome.org/pub/GNOME/sources"
+_F_archive_name="$_F_gnome_name"
+
 if [ -z "$_F_gnome_up2date" ]; then
 	if [ -z "$_F_archive_grepv" ]; then
 		_F_archive_grepv="3.32.*"
 	fi
 fi
 
-if [[ "$_F_gnome_devel" == "y" ]]; then
-	if [ -n "$_F_archive_grepv" ]; then
-		up2date="lynx -read_timeout=280 -dump $_F_gnome_pkgurl/$_F_gnome_name/\$(lynx -dump -listonly $_F_gnome_pkgurl/$_F_gnome_name/?C=M\&O=D|grep -v "$_F_archive_grepv" | grep '[0-9]\.[0-9]*[0-9]/' |sed -ne 's|.*]\(.*\)/.*|\1|' -e '1 p')/|grep ]LA|sed 's/.*S-\([0-9\.]*\).*/\1/' | sort -r | head -n1"
-	else
-		up2date="lynx -read_timeout=280 -dump $_F_gnome_pkgurl/$_F_gnome_name/\$(lynx -dump -listonly $_F_gnome_pkgurl/$_F_gnome_name/?C=M\;O=D|  grep '[0-9]\.[0-9]*[0-9]/' |sed -ne 's|.*]\(.*\)/.*|\1|' -e '1 p')/|grep ]LA|sed 's/.*S-\([0-9\.]*\).*/\1/' | sort -r | head -n1"
-	fi
-else
-	if [ -n "$_F_archive_grepv" ]; then
-		up2date="lynx -read_timeout=280 -dump $_F_gnome_pkgurl/$_F_gnome_name/\$(lynx -dump -listonly $_F_gnome_pkgurl/$_F_gnome_name/?C=M\&O=D|grep -v "$_F_archive_grepv" |grep '[0-9]\.[0-9]*[02468]/'| grep -v ".9[0-9]." | sed -ne 's|.*]\(.*\)/.*|\1|' -e '1 p')/|grep ]LA|sed 's/.*S-\([0-9\.]*\).*/\1/' | sort -r | head -n1"
-	else
-		up2date="lynx -read_timeout=280 -dump $_F_gnome_pkgurl/$_F_gnome_name/\$(lynx -dump -listonly $_F_gnome_pkgurl/$_F_gnome_name/?C=M\&O=D|grep '[0-9]\.[0-9]*[02468]/'| grep -v ".9[0-9]." | sed -ne 's|.*]\(.*\)/.*|\1|' -e '1 p')/|grep ]LA|sed 's/.*S-\([0-9\.]*\).*/\1/' | sort -r | head -n1"
-	fi
-fi
+up2date="Flasttar $_F_gnome_pkgurl/$_F_gnome_name/\$(Flastverdir $_F_gnome_pkgurl/$_F_gnome_name )"
 
 if [ "$_F_gnome_git" != "n" ]; then
 	makedepends+=('git')
@@ -110,7 +78,7 @@ if [ "$_F_gnome_git" != "n" ]; then
 	_F_scm_type="git"
 	_F_scm_url="git://git.gnome.org/$_F_gnome_name"
 else
-	source=(https://download.gnome.org/sources/$_F_gnome_name/`_F_gnome_getver`/$_F_gnome_name-$pkgver$_F_gnome_ext)
+	source=(https://download.gnome.org/sources/$_F_gnome_name/${pkgver%%.*}/$_F_gnome_name-$pkgver$_F_gnome_ext)
 fi
 url="http://www.gnome.org/"
 _F_cd_path=$_F_gnome_name-$pkgver

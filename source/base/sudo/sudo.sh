a6c26bf48b463aad5f89254b3bbe49191a63a4ee
dex77 <dex77@frugalware.org>
2024-01-03 11:30:47 +0100

    sudo-1.9.15-2-x86_64
    * usr-merge

--- source/base/sudo/sudo.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# /etc/profile.d/sudo.sh
-
-# Adds /sbin and /usr/sbin to PATH when sudoing, so you don't have
-# to give full PATH for launching a program in those directories.
-
-alias sudo="PATH=$PATH:/sbin:/usr/sbin sudo"

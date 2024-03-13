4a5425a3737488558c063d2999512b882dcf970b
DeX77 <dex77@frugalware.org>
2024-03-13 13:38:28 +0100

    * we need still need old kf5

---
+++ source/include/kf5-version.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+###
+# = kf5-version.sh(3)
+# Michel Hermier <hermier@frugalware.org>
+#
+# == NAME
+# kf5-version.sh - for Frugalware
+#
+# == SYNOPSIS
+# Common schema for packages built against a given KDE version.
+#
+# == OVERWRITTEN VARIABLES
+# * _F_kdever_ver: the KDE realease
+# * _F_kdever_qt: the Qt release required for KDE
+# * _F_kdever_sha1sums: a hash of all the sha1sums for the various KDE projects
+###
+
+
+_F_kdever_frameworks=5.115
+_F_kdever_frameworks_revision=0
+_F_kf5_full="${_F_kdever_frameworks}.${_F_kdever_frameworks_revision}"
+_F_kdever_plasma=5.27.10
+_F_kdever_qt5=5.15.12
+_F_kdever_apps=23.08.5
+
+
+

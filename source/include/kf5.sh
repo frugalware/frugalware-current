ca68206af073695815f8098927950e13ae59960c
DeX77 <dex77@frugalware.org>
2024-03-13 15:37:20 +0100

    * special treatment for kf5

--- source/include/kf5.sh
+++ source/include/kf5.sh
@@ -136,6 +136,7 @@ if [ -z "$groups" ]; then
 		groups+=('plasma')
 	elif [ "$_F_kde_project" = "frameworks" ]; then
 		groups+=('kf5')
+		up2date='rsync -r ${_F_kde_up2date_mirror}/${_F_kde_folder} | grep "\-5\." | Flastarchive ${_F_kde_ext}'
 	elif [ "$_F_kde_project" = "release-service" ]; then
 		groups+=('kde5')
 	fi
@@ -144,7 +145,7 @@ fi
 if [ "$_F_kde_defaults" -eq 1 ]; then
 	if [ -z "$up2date" ]; then
 		makedepends+=('rsync')
-		up2date='rsync -r ${_F_kde_up2date_mirror}/${_F_kde_folder} | grep "\-5\." | Flastarchive ${_F_kde_ext}'
+		up2date='rsync -r ${_F_kde_up2date_mirror}/${_F_kde_folder} | Flastarchive ${_F_kde_ext}'
 	fi
 
 	if [ ${#source[@]} -eq 0 ]; then

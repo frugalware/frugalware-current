92cf96d74ef710108778012512a207fd1bf4b5d8
DeX77 <dex77@frugalware.org>
2024-07-31 09:41:01 +0200

    firefox-128.0.3-1-x86_64
    * version bump

--- source/xapps-extra/firefox/vendor.js
+++ source/xapps-extra/firefox/vendor.js
@@ -1,5 +1,8 @@
 // This file can be used to configure global preferences for Firefox
 pref("intl.locale.requested", "");
+pref("spellchecker.dictionary_path", "/usr/share/hunspell");
+pref("extensions.autoDisableScopes", 11);
 pref("browser.preferences.instantApply", true);
 pref("browser.shell.checkDefaultBrowser", false);
 pref("browser.display.use_system_colors", true);
+pref("browser.gnome-search-provider.enabled", true);

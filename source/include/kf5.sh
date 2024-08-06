925b5e1d7d0f5f0dceccc8e9a7008940a00375c4
DeX77 <dex77@frugalware.org>
2024-08-06 10:14:53 +0200

    Revert "* remove workaround"
    This reverts commit a0a9d1788ed8b3065caafb977b6c9e60839b77a5.

--- source/include/kf5.sh
+++ source/include/kf5.sh
@@ -145,7 +145,11 @@ fi
 if [ "$_F_kde_defaults" -eq 1 ]; then
 	if [ -z "$up2date" ]; then
 		makedepends+=('rsync')
-		up2date='rsync -r ${_F_kde_up2date_mirror}/${_F_kde_folder} | Flastarchive ${_F_kde_ext}'
+	        if [ "$_F_kde_project" = "frameworks" ]; then
+			up2date='rsync -r ${_F_kde_up2date_mirror}/${_F_kde_folder} | grep "\-5\." | Flastarchive ${_F_kde_ext}'
+		else
+			up2date='rsync -r ${_F_kde_up2date_mirror}/${_F_kde_folder} | Flastarchive ${_F_kde_ext}'
+		fi
 	fi
 
 	if [ ${#source[@]} -eq 0 ]; then

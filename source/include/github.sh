a9bd8f1fdfd1dd6b01bc0df882d612b6aef3228d
DeX77 <dex+fw@dragonslave.de>
2024-08-28 00:02:58 +0200

    * fix v prefix gh

--- source/include/github.sh
+++ source/include/github.sh
@@ -133,7 +133,7 @@ if [ -n "$_F_github_devel" ]; then
 	unset _F_github_source _F_github_tag _F_github_tag_v source
 else
 	if [[ -n "$_F_github_tag_v" ]] || [[ -n "$_F_github_tag" ]]; then
-		up2date="curl -s https://api.github.com/repos/${_F_github_author}/${_F_github_dirname}/git/matching-refs/tags | jq -r '.[].ref' | sed 's:refs/tags/::g' | tac $off $on $prefix | head -n1"
+		up2date="curl -s https://api.github.com/repos/${_F_github_author}/${_F_github_dirname}/git/matching-refs/tags | jq -r '.[].ref' | sed 's:refs/tags/::g' | grep -E "^v" | tac $off $on $prefix | head -n1"
 		_F_github_source="https://github.com/$_F_github_author/$_F_github_dirname/archive/refs/tags/${__F_github_full_archive_name}"
 
 	else

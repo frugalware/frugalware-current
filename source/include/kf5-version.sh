952c2c53d464586e704d43b15f12b5a8e53c1f30
DeX77 <dex+fw@dragonslave.de>
2024-09-13 13:57:28 +0200

    * kde apps 24.08.1

--- source/include/kf5-version.sh
+++ source/include/kf5-version.sh
@@ -22,7 +22,7 @@ _F_kdever_frameworks_revision=0
 _F_kf5_full="${_F_kdever_frameworks}.${_F_kdever_frameworks_revision}"
 _F_kdever_plasma=5.27.11
 _F_kdever_qt5=5.15.15
-_F_kdever_apps=24.08.0
+_F_kdever_apps=24.08.1
 
 
 

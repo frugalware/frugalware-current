974ac1f874020d5ad54f9dfeba4a2733cefd772c
DeX77 <dex+fw@dragonslave.de>
2024-09-16 21:14:46 +0200

    * fix genauthors

--- tools/genauthors.py
+++ tools/genauthors.py
@@ -1,17 +1,17 @@
 from xml.dom import minidom
 import sys, codecs
 
-def sort_authors(a, b):
-	return cmp(a.getElementsByTagName("name")[0].firstChild.toxml(), b.getElementsByTagName("name")[0].firstChild.toxml())
+def sort_authors(a):
+	return a.getElementsByTagName("name")[0].firstChild.toxml()
 
 xmldoc = minidom.parse(sys.argv[1])
 out = codecs.open(sys.argv[2], "w", "utf-8")
 authors = []
 for i in xmldoc.getElementsByTagName('author'):
 	authors.append(i)
-authors.sort(sort_authors)
+authors.sort(key=sort_authors)
 for i in authors:
-	if unicode("active") == i.getElementsByTagName("status")[0].firstChild.toxml():
+	if str("active") == i.getElementsByTagName("status")[0].firstChild.toxml():
 		out.write("%s " % i.getElementsByTagName("name")[0].firstChild.toxml())
 		out.write("(%s) " % i.getElementsByTagName("nick")[0].firstChild.toxml())
 		out.write("<%s>\n" % i.getElementsByTagName("email")[0].firstChild.toxml())

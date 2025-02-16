0bcaadd0df38535284a6b3cf3da09ca05689a022
DeX77 <dex+fw@dragonslave.de>
2025-02-16 12:29:32 +0100

    wine-devel-10.1-1-x86_64
    * version bump

--- source/include/wine.sh
+++ source/include/wine.sh
@@ -20,20 +20,28 @@
 url="http://www.winehq.org"
 groups=('xapps-extra')
 depends=('lcms2' 'openal' 'libglu' 'libldap>=2.5.4' 'libpcap' 'libpulse' 'libmpg123' 'libgphoto2' 'gettext' 'libxml2' 'ocl-icd' \
-	'libxcursor' 'libxi' 'libxrandr' 'libxinerama' 'libxcomposite' 'sane-backends' 'v4l-utils' 'libxrender' \
+	'libxcursor' 'libxi' 'libxrandr' 'libxinerama' 'libxcomposite' 'sane-backends' 'v4l-utils' 'libxrender' 'wayland' \
 	'libxslt' 'vkd3d' 'vulkan-icd-loader' 'faudio' 'libosmesa' 'pcsc-lite' 'gst1-plugins-base' 'libxkbcommon')
 #32 bit
 depends+=('lib32-lcms2' 'lib32-libxcursor' 'lib32-libxi' 'lib32-libxrandr' 'lib32-libxinerama' 'lib32-libxcomposite' \
-	'lib32-libxrender' 'lib32-freetype2' 'lib32-libxml2' 'lib32-ncurses' 'lib32-vkd3d' 'lib32-ocl-icd' \
-	'lib32-libldap>=2.5.4' 'lib32-vulkan-icd-loader' 'lib32-faudio' 'lib32-libosmesa')
-makedepends=('x11-protos' 'cups' 'bison' 'opencl-headers' 'kernel-headers' 'wayland' 'wayland-protocols')
+	'lib32-libxrender' 'lib32-freetype2' 'lib32-libxml2' 'lib32-ncurses' 'lib32-vkd3d' 'lib32-ocl-icd' 'lib32-libxkbcommon' \
+	'lib32-libldap>=2.5.4' 'lib32-vulkan-icd-loader' 'lib32-faudio' 'lib32-libosmesa' 'lib32-wayland')
+makedepends=('x11-protos' 'cups' 'bison' 'opencl-headers' 'kernel-headers' 'wayland-protocols')
 _F_cd_path="wine-$pkgver"
-options=('genscriptlet' 'static' 'plt' 'nolto' 'ldgold')
+options=('genscriptlet' 'static' 'nofortify' 'nolto' 'plt' 'ldbfd')
 archs=('x86_64')
 _F_conf_configure="../configure"
 _F_archive_grepv="\-rc"
-Fconfopts="	--without-oss"
-F32confopts+="	--libdir=/usr/lib32 --without-oss"
+Fconfopts="	--without-oss \
+		--with-x \
+		--with-wayland \
+		--with-gstreamer"
+F32confopts+="	--libdir=/usr/lib32 \
+		--without-oss \
+		--with-x \
+		--with-wayland \
+		--with-gstreamer"
+
 
 Finclude cross32
 
@@ -46,20 +54,17 @@ wine)
 	conflicts=('wine-devel' 'lib32-wine-devel')
 	provides=('lib32-wine')
 	replaces=('lib32-wine')
-	source=(https://dl.winehq.org/wine/source/${pkgver}/wine-$pkgver.tar.xz \
-		0001-programs-winhlp32-Use-noyywrap-for-macro.lex.l-and-p.patch )
+	source=(https://dl.winehq.org/wine/source/${pkgver}/wine-$pkgver.tar.xz)
 	;;
 
 wine-devel)
 	pkgdesc="An Open Source implementation of the Windows API on top of X and Unix. (Development)"
 	_F_archive_name="wine"
-	up2date="Flasttar https://dl.winehq.org/wine/source/9.x"
+	up2date="Flasttar https://dl.winehq.org/wine/source/10.x"
 	conflicts=('wine' 'lib32-wine-devel')
 	provides=('wine' 'lib32-wine-devel')
 	replaces=('lib32-wine-devel')
-	depends+=('gst1-plugins-base')
-	source=(https://dl.winehq.org/wine/source/9.x/wine-$pkgver.tar.xz \
-		0001-programs-winhlp32-Use-noyywrap-for-macro.lex.l-and-p.patch )
+	source=(https://dl.winehq.org/wine/source/10.x/wine-$pkgver.tar.xz)
 	;;
 
 default)
@@ -71,16 +76,17 @@ esac
 
 build()
 {
-
 	Fcd
 	Fpatchall
 	Fsed 'lib64' 'lib' configure.ac
 	Fautoreconf
+	Fexec rm -rf 64Bit_build || Fdie
 	Fexec mkdir 64Bit_build || Fdie
 	Fexec cd 64Bit_build || Fdie
 	Fmake --enable-win64
 
 	Fexec cd .. || Fdie
+	Fexec rm -rf 32Bit_build || Fdie
 	Fexec mkdir 32Bit_build || Fdie
         Fexec cd 32Bit_build || Fdie
         Fcross32_prepare

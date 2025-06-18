cb7d68dac6a0c4cf13fdfe29c661af2646e61530
DeX77 <dex+fw@dragonslave.de>
2025-06-18 14:02:35 +0200

    * use mingw

--- source/include/wine.sh
+++ source/include/wine.sh
@@ -26,7 +26,7 @@ depends=('lcms2' 'openal' 'libglu' 'libldap>=2.5.4' 'libpcap' 'libpulse' 'libmpg
 depends+=('lib32-lcms2' 'lib32-libxcursor' 'lib32-libxi' 'lib32-libxrandr' 'lib32-libxinerama' 'lib32-libxcomposite' \
 	'lib32-libxrender' 'lib32-freetype2' 'lib32-libxml2' 'lib32-ncurses' 'lib32-vkd3d' 'lib32-ocl-icd' 'lib32-libxkbcommon' \
 	'lib32-libldap>=2.5.4' 'lib32-vulkan-icd-loader' 'lib32-faudio' 'lib32-libosmesa' 'lib32-wayland')
-makedepends=('x11-protos' 'cups' 'bison' 'opencl-headers' 'kernel-headers' 'wayland-protocols')
+makedepends=('x11-protos' 'cups' 'bison' 'opencl-headers' 'kernel-headers' 'wayland-protocols' 'mingw-w64-gcc')
 _F_cd_path="wine-$pkgver"
 options=('genscriptlet' 'static' 'nofortify' 'nolto' 'plt' 'ldbfd')
 archs=('x86_64')
